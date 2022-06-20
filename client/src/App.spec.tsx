e64de3ac692cacecdf460ba05945315b76d96ef8
Jakob Öhman <jakob@29k.org>
2022-06-20 21:55:04 +0200

    Fix tests

--- client/src/App.spec.tsx
+++ client/src/App.spec.tsx
@@ -4,10 +4,12 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../src/App';
+import App from './App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+jest.useFakeTimers();
 
 it('renders correctly', () => {
   renderer.create(<App />);
