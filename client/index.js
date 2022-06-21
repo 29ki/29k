2124d01193338815ab0e4d952b1ed93ed2fad12a
ian <ian@vidales.se>
2022-06-21 21:57:10 +0200

    Hard code root component name and remove unused file

--- client/index.js
+++ client/index.js
@@ -2,10 +2,9 @@ import {AppRegistry} from 'react-native';
 import auth from '@react-native-firebase/auth';
 
 import App from './src/App';
-import {name as appName} from './app.json';
 
 if (__DEV__) {
   auth().useEmulator('http://localhost:9099');
 }
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('twentyninek', () => App);
