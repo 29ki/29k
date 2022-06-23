d9208543c81d87136743269d1c9ab323b29d7cfb
Kajsa Unge <hej@kajsaunge.se>
2022-06-23 16:43:56 +0200

    Add tab icons. Fix imports

---
+++ client/src/common/components/Icons/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
+import {COLORS} from '../../../constants/colors';
+
+export const HomeIcon = ({fill = COLORS.GREY100}) => (
+  <Svg width="100%" height="100%" viewBox="0 0 30 30" style={{aspectRatio: 1}}>
+    <Path
+      d="M4.558 11.68a.75.75 0 00-.308.606V23c0 .414.336.75.75.75h20a.75.75 0 00.75-.75V12.286a.75.75 0 00-.308-.606l-10-7.286a.75.75 0 00-.884 0l-10 7.286z"
+      stroke={fill}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M12 24v-7h6v7"
+      stroke={fill}
+      strokeWidth={1.5}
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
