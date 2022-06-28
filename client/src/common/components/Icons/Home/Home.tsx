c93abc77c43a86b70d6586bcb4d62e3a80ecdb6c
Emil Rondahl <emil@29k.org>
2022-06-28 10:15:40 +0200

    Add icon wrapper

--- client/src/common/components/Icons/Home/Home.tsx
+++ client/src/common/components/Icons/Home/Home.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import Svg, {Path} from 'react-native-svg';
+import {Path} from 'react-native-svg';
 import {COLORS} from '../../../constants/colors';
-
-const styles = StyleSheet.create({
-  home: {
-    aspectRatio: 1,
-  },
-});
+import Icon from '../Icon';
 
 export const HomeIcon = ({fill = COLORS.GREY100}) => (
-  <Svg width="100%" height="100%" viewBox="0 0 30 30" style={styles.home}>
+  <Icon>
     <Path
       d="M4.558 11.68a.75.75 0 00-.308.606V23c0 .414.336.75.75.75h20a.75.75 0 00.75-.75V12.286a.75.75 0 00-.308-.606l-10-7.286a.75.75 0 00-.884 0l-10 7.286z"
       stroke={fill}
@@ -24,5 +18,5 @@ export const HomeIcon = ({fill = COLORS.GREY100}) => (
       strokeWidth={1.5}
       strokeLinejoin="round"
     />
-  </Svg>
+  </Icon>
 );
--- client/src/common/components/Icons/Profile/Profile.tsx
+++ client/src/common/components/Icons/Profile/Profile.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import Svg, {Circle, Path} from 'react-native-svg';
+import {Circle, Path} from 'react-native-svg';
 import {COLORS} from '../../../constants/colors';
-
-const styles = StyleSheet.create({
-  profile: {
-    aspectRatio: 1,
-  },
-});
+import Icon from '../Icon';
 
 export const ProfileIcon = ({fill = COLORS.GREY100}) => (
-  <Svg width="100%" height="100%" viewBox="0 0 30 30" style={styles.profile}>
+  <Icon>
     <Circle cx={15} cy={10} r={3.75} stroke={fill} strokeWidth={1.5} />
     <Path
       d="M7.26 23.877a.75.75 0 00.74.873h14a.75.75 0 00.74-.873l-.014-.082a7.833 7.833 0 00-15.452 0l-.014.082z"
@@ -19,5 +13,5 @@ export const ProfileIcon = ({fill = COLORS.GREY100}) => (
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-  </Svg>
+  </Icon>
 );
