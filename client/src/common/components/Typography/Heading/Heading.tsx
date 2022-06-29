7296e1179bb0fed402b6d3040f031fb320edaaa9
Kajsa Unge <hej@kajsaunge.se>
2022-06-29 14:19:28 +0200

    Combined type styles. Typed heading component

--- client/src/common/components/Typography/Heading/Heading.tsx
+++ client/src/common/components/Typography/Heading/Heading.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import {StyleSheet, Text, TextStyle} from 'react-native';
-import {COLORS} from '../../../constants/colors';
+import {Text, TextStyle} from 'react-native';
 
 import typeStyles from '../styles';
 
-const styles = StyleSheet.create({
-  heading: {
-    ...typeStyles[`HEADING${level}`],
-    color: COLORS.GREY800,
-  },
-});
-
-type HeadingProps = {style: {} & TextStyle};
-const Heading: React.FC<HeadingProps> = ({style = {}, children}) => (
-  <Text allowFontScaling={false} style={[styles.heading, style]}>
+type HeadingBaseProps = {
+  style?: {} & TextStyle;
+  defaultStyles: TextStyle;
+};
+const Heading: React.FC<HeadingBaseProps> = ({
+  style = {},
+  defaultStyles,
+  children,
+}) => (
+  <Text allowFontScaling={false} style={[defaultStyles, style]}>
     {children}
   </Text>
 );
 
-export const Heading1 = props => <Heading {...props} level={1} />;
-export const Heading2 = props => <Heading {...props} level={2} />;
-export const Heading3 = props => <Heading {...props} level={3} />;
-export const Heading4 = props => <Heading {...props} level={4} />;
+type HeadingProps = {
+  style?: TextStyle;
+};
+export const Heading1: React.FC<HeadingProps> = props => (
+  <Heading {...props} defaultStyles={typeStyles.HEADING1} />
+);
+export const Heading2: React.FC<HeadingProps> = props => (
+  <Heading {...props} defaultStyles={typeStyles.HEADING2} />
+);
+export const Heading3: React.FC<HeadingProps> = props => (
+  <Heading {...props} defaultStyles={typeStyles.HEADING3} />
+);
+export const Heading4: React.FC<HeadingProps> = props => (
+  <Heading {...props} defaultStyles={typeStyles.HEADING4} />
+);
