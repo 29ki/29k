6d63e7fba81830d1728c6c5522e6fb7ed7f12948
Jakob Öhman <jakob@29k.org>
2022-07-04 09:11:31 +0200

    Add Gutters and Spacers

---
+++ client/src/common/components/Gutters/Gutters.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import {GUTTERS} from '../../constants/spacings';
+
+const Gutters = styled.View`
+  padding-horizontal: ${GUTTERS};
+`;
+
+export default Gutters;
