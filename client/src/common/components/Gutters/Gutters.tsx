bac21a8a7264dc9c2d512e7ef398fc6c7de9443e
Jakob Öhman <jakob@29k.org>
2022-07-04 16:23:49 +0200

    Reverted gutters to one

--- client/src/common/components/Gutters/Gutters.tsx
+++ client/src/common/components/Gutters/Gutters.tsx
@@ -1,12 +1,9 @@
 import styled from 'styled-components/native';
-import {SPACINGS} from '../../constants/spacings';
+import {GUTTERS} from '../../constants/spacings';
 
-export const TextGutter = styled.View`
-  padding-left: ${SPACINGS.TWENTYFOUR}px;
-  padding-right: ${SPACINGS.TWENTYFOUR}px;
+const Gutters = styled.View`
+  padding-left: ${GUTTERS}px;
+  padding-right: ${GUTTERS}px;
 `;
 
-export const GrapihcGutter = styled.View`
-  padding-left: ${SPACINGS.SIXTEEN}px;
-  padding-right: ${SPACINGS.SIXTEEN}px;
-`;
+export default Gutters;
