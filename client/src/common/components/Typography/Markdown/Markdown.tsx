f01ebc14ec53ff230f4b84610700e4faa0019a8a
Jakob Öhman <jakob@29k.org>
2022-07-04 16:54:13 +0200

    Markdown component

--- client/src/common/components/Typography/Markdown/Markdown.tsx
+++ client/src/common/components/Typography/Markdown/Markdown.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import MarkdownDisplay from 'react-native-markdown-display';
+import rules from './rules';
+import styles from './styles';
 
 const Markdown: React.FC = ({children}) => (
-  <MarkdownDisplay>{children}</MarkdownDisplay>
+  <MarkdownDisplay style={styles} rules={rules}>
+    {children}
+  </MarkdownDisplay>
 );
 
 export default Markdown;
