7ecf05c7b2bdc62a9566afc90359388338095727
Jakob Öhman <jakob@29k.org>
2022-07-04 16:36:01 +0200

    Add Gutters to ScreenWrapper

--- client/src/lib/uiLib/decorators/ScreenWrapper.tsx
+++ client/src/lib/uiLib/decorators/ScreenWrapper.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {ScrollView, View, StyleSheet} from 'react-native';
+import {ScrollView} from 'react-native';
 
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {SPACINGS} from '../../../common/constants/spacings';
-
-const styles = StyleSheet.create({
-  wrapper: {flex: 1, padding: SPACINGS.SIXTEEN},
-});
+import Gutters from '../../../common/components/Gutters/Gutters';
+import {
+  BottomSafeArea,
+  TopSafeArea,
+} from '../../../common/components/Spacers/Spacer';
 
 const ScreenWrapper: React.FunctionComponent = ({children}) => (
   <ScrollView>
-    <SafeAreaView>
-      <View style={styles.wrapper}>{children}</View>
-    </SafeAreaView>
+    <TopSafeArea />
+    <Gutters>{children}</Gutters>
+    <BottomSafeArea />
   </ScrollView>
 );
 
