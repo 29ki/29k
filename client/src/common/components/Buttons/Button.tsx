f360a99222491b68863fe177952aa8cfb36a8b8e
Jakob Öhman <jakob@29k.org>
2022-07-04 22:19:05 +0200

    Button component

--- client/src/common/components/Buttons/Button.tsx
+++ client/src/common/components/Buttons/Button.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import {Button as RNButton} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+import styled from 'styled-components/native';
+import {COLORS} from '../../constants/colors';
+import {B1} from '../Typography/Text/Text';
+
+const ButtonComponent = styled.View`
+  background-color: ${COLORS.SUCCESS_GREEN};
+  padding: 10px 20px;
+  border-radius: 30px;
+  align-items: center;
+`;
+
+const ButtonText = styled(B1)`
+  color: ${COLORS.WHITE};
+`;
 
 const Button: React.FC<{
-  title: string;
   onPress: () => void;
-}> = ({title, onPress}) => <RNButton title={title} onPress={onPress} />;
+}> = ({children, onPress}) => (
+  <TouchableOpacity onPress={onPress}>
+    <ButtonComponent>
+      <ButtonText>{children}</ButtonText>
+    </ButtonComponent>
+  </TouchableOpacity>
+);
 
 export default Button;
