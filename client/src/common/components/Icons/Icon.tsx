69717eb618a62ff2b80948677d9f789994c7e9bb
Jakob Öhman <jakob@29k.org>
2022-07-05 18:01:49 +0200

    aspect-ratio only works in styled template litterals

--- client/src/common/components/Icons/Icon.tsx
+++ client/src/common/components/Icons/Icon.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Svg, {SvgProps} from 'react-native-svg';
 import styled from 'styled-components/native';
 
-const SvgIcon = styled(Svg)({
-  aspectRatio: 1,
-});
+const SvgIcon = styled(Svg)`
+  aspect-ratio: 1;
+`;
 
 const Icon: React.FC<SvgProps> = ({children, style}) => (
   <SvgIcon width="100%" height="100%" viewBox="0 0 30 30" style={style}>
