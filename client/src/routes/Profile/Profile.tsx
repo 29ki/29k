32552220ed52818470044535848b9a377c72d841
Jakob Öhman <jakob@29k.org>
2022-07-05 23:15:46 +0200

    Show user id on Profile tab

--- client/src/routes/Profile/Profile.tsx
+++ client/src/routes/Profile/Profile.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {useRecoilValue} from 'recoil';
 import styled from 'styled-components/native';
 import Button from '../../common/components/Buttons/Button';
 import Gutters from '../../common/components/Gutters/Gutters';
 import {Spacer16} from '../../common/components/Spacers/Spacer';
+import {B1} from '../../common/components/Typography/Text/Text';
 import useCheckForUpdate from '../../lib/codePush/hooks/useCheckForUpdate';
 import useClearUpdates from '../../lib/codePush/hooks/useClearUpdates';
 import {useUiLib} from '../../lib/uiLib/hooks/useUiLib';
+import {userAtom} from '../../lib/user/state/state';
 
 const Wrapper = styled.View({
   flex: 1,
@@ -14,12 +17,19 @@ const Wrapper = styled.View({
 });
 
 const Profile = () => {
+  const user = useRecoilValue(userAtom);
   const {toggle: toggleUiLib} = useUiLib();
   const clearUpdates = useClearUpdates();
   const checkForUpdate = useCheckForUpdate();
   return (
     <Wrapper>
       <Gutters>
+        {user && (
+          <>
+            <B1>User ID: {user.uid}</B1>
+            <Spacer16 />
+          </>
+        )}
         <Button onPress={toggleUiLib}>UI lib</Button>
         <Spacer16 />
         <Button onPress={clearUpdates}>Clear update</Button>
