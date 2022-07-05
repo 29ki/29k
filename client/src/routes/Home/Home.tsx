910a8ef698dd5820eaf71428ce840beedff2ed87
Kajsa Unge <hej@kajsaunge.se>
2022-07-05 13:32:25 +0200

    Add Logo Icon

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import {View} from 'react-native';
 
 import styled from 'styled-components/native';
 import Gutters from '../../common/components/Gutters/Gutters';
+import {Logo} from '../../common/components/Icons';
+import {
+  BottomSafeArea,
+  Spacer12,
+  Spacer48,
+  TopSafeArea,
+} from '../../common/components/Spacers/Spacer';
+import {H1, H3} from '../../common/components/Typography/Heading/Heading';
+import {B1} from '../../common/components/Typography/Text/Text';
 
 const Wrapper = styled.View`
   flex: 1;
@@ -9,21 +19,38 @@ const Wrapper = styled.View`
   align-items: center;
 `;
 
-const Logotype = styled.Image`
-  flex-grow: 0;
-  width: 150px;
-  height: 150px;
-  border-radius: 20px;
+const Logotype = styled.View`
+  width: 50px;
+  height: 50px;
+`;
+
+const ContentWrapper = styled.View`
+  width: 80%;
+  align-items: center;
+  flex-direction: row;
 `;
 
 const Home = () => {
   return (
     <Wrapper>
       <Gutters>
-        <Logotype
-          source={require('../../assets/logotype.png')}
-          resizeMode="contain"
-        />
+        <TopSafeArea />
+        <ContentWrapper>
+          <Logotype>
+            <Logo />
+          </Logotype>
+          <Spacer12 />
+          <View>
+            <H3>Vulnerability</H3>
+            <B1>
+              To share beyond the layers of shoeshine and collateral sticks and
+              bones
+            </B1>
+          </View>
+        </ContentWrapper>
+        <Spacer48 />
+        <H1>Vulnerability is a birthday cake served with fireworks</H1>
+        <BottomSafeArea />
       </Gutters>
     </Wrapper>
   );
