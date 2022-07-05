52cf6325d4fbc94675513c4eea31a6575762a154
Kajsa Unge <hej@kajsaunge.se>
2022-07-05 08:40:36 +0200

    Colors annotations

--- client/src/common/constants/colors.ts
+++ client/src/common/constants/colors.ts
@@ -1,10 +1,14 @@
 export const COLORS = {
+  //
+  // Base
+  //
   BLACK: '#000000',
   BLACK_TRANSPARENT: 'rgba(0, 0, 0, 0.5)',
   WHITE: '#FFFFFF',
   WHITE_TRANSPARENT: 'rgba(255, 255, 255, 0.75)',
+
   //
-  // Grey -- typography
+  // Greyscale
   //
   GREY100: '#FFFDF5',
   GREY200: '#E6E4DC',
@@ -14,17 +18,15 @@ export const COLORS = {
   GREY600: '#807E7A',
   GREY800: '#595856',
 
-  //
-  // Peach -- primary CTAs
-  //
+  // Brand   -- Primary, Secondary, Accent
+
+  // Primary
   PEACH100: '#FF7259',
   PEACH200: '#E65D45',
   PEACH300: '#CC533D',
   PEACH600: '#803426',
 
-  //
-  // Plum -- Background shades
-  //
+  // Secondary
   PLUM100: '#493C4D',
   PLUM200: '#3D3240',
   PLUM300: '#312833',
@@ -33,14 +35,7 @@ export const COLORS = {
   PLUM600: '#0C0A0D',
 
   //
-  // Highlight alerts
-  //
-  ERROR_PINK: '#F85F7D',
-  SUCCESS_GREEN: '#2CAC76',
-  HANG_UP_RED: '#E65d45',
-
-  //
-  // Content types
+  // Accent -- Content types
   //
   DEFAULT_CONTENT: '#293744',
   EXERCISE: '#423C5A',
@@ -50,4 +45,11 @@ export const COLORS = {
   TEST: '#5D5A70',
   COURSE: '#293744',
   LESSON: '#354D48',
+
+  //
+  // Functional  --  Information, Success, Warning, Danger
+  //
+  ERROR_PINK: '#F85F7D',
+  SUCCESS_GREEN: '#2CAC76',
+  HANG_UP_RED: '#E65d45',
 };
