56418f6eeb3e5ced3bf931cb4a5c6b2b4f0f4d43
Jakob Öhman <jakob@29k.org>
2022-07-05 22:54:59 +0200

    Add utils to Profile

--- client/src/routes/Profile/Profile.tsx
+++ client/src/routes/Profile/Profile.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components/native';
 import Button from '../../common/components/Buttons/Button';
 import Gutters from '../../common/components/Gutters/Gutters';
+import {Spacer16} from '../../common/components/Spacers/Spacer';
+import useCheckForUpdate from '../../lib/codePush/hooks/useCheckForUpdate';
+import useClearUpdates from '../../lib/codePush/hooks/useClearUpdates';
 import {useUiLib} from '../../lib/uiLib/hooks/useUiLib';
 
 const Wrapper = styled.View({
@@ -12,10 +15,16 @@ const Wrapper = styled.View({
 
 const Profile = () => {
   const {toggle: toggleUiLib} = useUiLib();
+  const clearUpdates = useClearUpdates();
+  const checkForUpdate = useCheckForUpdate();
   return (
     <Wrapper>
       <Gutters>
         <Button onPress={toggleUiLib}>UI lib</Button>
+        <Spacer16 />
+        <Button onPress={clearUpdates}>Clear update</Button>
+        <Spacer16 />
+        <Button onPress={checkForUpdate}>Check for update</Button>
       </Gutters>
     </Wrapper>
   );
