afb36c88af9c6153850509787798afaf62e5195f
Jakob Öhman <jakob@29k.org>
2022-07-07 22:49:34 +0200

    Setup basic settings

--- shared/src/constants/i18n.ts
+++ shared/src/constants/i18n.ts
@@ -1,3 +1,18 @@
-export const LANGUAGE_TAGS = ['en'];
+// LANGUAGES defines the supported languages and their tags
+const LANGUAGES = {
+  en: 'English',
+  sv: 'Svenska',
+  pt: 'Português',
+};
 
+// LANGUAGE_TAGS defines the supported languages
+export const LANGUAGE_TAGS = ['en', 'sv', 'pt'];
+
+// DEFAULT_LANGUAGE defines the default language is used when a translation for
+// a specific language is missing and will also be the default for all users,
+// unless overridden.
 export const DEFAULT_LANGUAGE_TAG = 'en';
+
+// DEFAULT_TIME_LOCATION is the time location used as a fallback for users that
+// doesn't have one set.
+export const DEFAULT_TIME_LOCATION = 'UTC';
