eaacf0c4b06dba465e6bad08057cf39c9349038b
Jakob Öhman <jakob@29k.org>
2022-07-07 14:38:33 +0200

    A new home

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,52 +1,81 @@
-import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import AnimatedLottieView from 'lottie-react-native';
 import React from 'react';
+import {useTranslation} from 'react-i18next';
 import styled from 'styled-components/native';
-import Button from '../../common/components/Buttons/Button';
+import Button, {ButtonText} from '../../common/components/Buttons/Button';
 import Gutters from '../../common/components/Gutters/Gutters';
+import {ForwardIcon, LogoIcon} from '../../common/components/Icons';
 import {
   BottomSafeArea,
   Spacer16,
   TopSafeArea,
 } from '../../common/components/Spacers/Spacer';
-import {ROUTES} from '../../common/constants/routes';
-import {StackNavigatorScreens} from '../../lib/navigation/Stacks';
+import {H2} from '../../common/components/Typography/Heading/Heading';
+import {COLORS} from '../../common/constants/colors';
+import {ROUTES, ScreenProps} from '../../common/constants/routes';
+import NS from '../../lib/i18n/constants/namespaces';
 
 const Wrapper = styled.View({
   flex: 1,
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'space-around',
 });
 
-const Logotype = styled.View({
-  width: '50%',
+const Mandala = styled(AnimatedLottieView)({
+  width: '80%',
   height: undefined,
   aspectRatio: '1',
   borderRadius: 20,
   overflow: 'hidden',
 });
 
-const Image = styled.Image({
-  width: '100%',
-  height: '100%',
+const Logo = styled.View({
+  width: 80,
+  aspectRatio: '1',
 });
 
-type BreathinhScreenNavigationProp = StackNavigationProp<StackNavigatorScreens>;
+const Preamble = styled(H2)({
+  textAlign: 'center',
+});
+
+const ButtonIcon = styled.View({
+  width: 30,
+  height: 30,
+});
+
+type ScreenNavigationProps = StackNavigationProp<ScreenProps>;
 
 const Home = () => {
-  const {navigate} = useNavigation<BreathinhScreenNavigationProp>();
+  const {t} = useTranslation(NS.SCREEN.HOME);
+  const {navigate} = useNavigation<ScreenNavigationProps>();
   return (
-    <Wrapper>
-      <Gutters>
-        <TopSafeArea />
-        <Logotype>
-          <Image source={require('../../assets/logotype.png')} />
-        </Logotype>
+    <>
+      <TopSafeArea />
+      <Wrapper>
+        <Logo>
+          <LogoIcon />
+        </Logo>
+        <Gutters>
+          <Preamble>{t('heading')}</Preamble>
+        </Gutters>
+        <Mandala
+          source={require('../../assets/animations/mandala.json')}
+          autoPlay
+          loop
+        />
         <Spacer16 />
-        <Button onPress={() => navigate(ROUTES.BREATHING)}>Hej</Button>
-        <BottomSafeArea />
-      </Gutters>
-    </Wrapper>
+        <Button onPress={() => navigate(ROUTES.BREATHING)}>
+          <ButtonText>{t('button')}</ButtonText>
+          <Spacer16 />
+          <ButtonIcon>
+            <ForwardIcon fill={COLORS.YELLOW} />
+          </ButtonIcon>
+        </Button>
+      </Wrapper>
+      <BottomSafeArea />
+    </>
   );
 };
 
