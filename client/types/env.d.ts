39506eae5d770b303ba6fd1d5be204c27077ae1d
Emil Rondahl <emil@29k.org>
2022-07-07 08:57:20 +0200

    Fix typo in env variable

--- client/types/env.d.ts
+++ client/types/env.d.ts
@@ -3,5 +3,5 @@ declare module 'config' {
   export const ANDROID_CODE_PUSH_DEPLOYMENT_KEY: string;
   export const IOS_CODE_PUSH_DEPLOYMENT_KEY: string;
   export const KILL_SWITCH_ENDPOINT: string;
-  export const CALL_ENDOINT: string;
+  export const CALL_ENDPOINT: string;
 }
