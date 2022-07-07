ed70824fc0a16887e0071e2951694ee0cea6a7f4
Jakob Öhman <jakob@29k.org>
2022-07-07 08:17:32 +0200

    Cleaned-up home screen

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
-import {View} from 'react-native';
-
 import styled from 'styled-components/native';
 import Gutters from '../../common/components/Gutters/Gutters';
-import {Logo} from '../../common/components/Icons';
 import {
   BottomSafeArea,
-  Spacer12,
-  Spacer48,
   TopSafeArea,
 } from '../../common/components/Spacers/Spacer';
-import {H1, H3} from '../../common/components/Typography/Heading/Heading';
-import {B1} from '../../common/components/Typography/Text/Text';
 
 const Wrapper = styled.View({
   flex: 1,
-  justifyContent: 'center',
   alignItems: 'center',
+  justifyContent: 'center',
 });
 
 const Logotype = styled.View({
-  width: 50,
-  height: 50,
+  width: '50%',
+  height: undefined,
+  aspectRatio: '1',
+  borderRadius: 20,
+  overflow: 'hidden',
 });
 
-const ContentWrapper = styled.View({
-  width: '80%',
-  alignItems: 'center',
-  flexDirection: 'row',
+const Image = styled.Image({
+  width: '100%',
+  height: '100%',
 });
 
 const Home = () => {
@@ -35,21 +30,9 @@ const Home = () => {
     <Wrapper>
       <Gutters>
         <TopSafeArea />
-        <ContentWrapper>
-          <Logotype>
-            <Logo />
-          </Logotype>
-          <Spacer12 />
-          <View>
-            <H3>Vulnerability</H3>
-            <B1>
-              To share beyond the layers of shoeshine and collateral sticks and
-              bones
-            </B1>
-          </View>
-        </ContentWrapper>
-        <Spacer48 />
-        <H1>Vulnerability is a birthday cake served with fireworks</H1>
+        <Logotype>
+          <Image source={require('../../assets/logotype.png')} />
+        </Logotype>
         <BottomSafeArea />
       </Gutters>
     </Wrapper>
