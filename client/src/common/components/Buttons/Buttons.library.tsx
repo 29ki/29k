78ae786219fe2d7b4dff4ba4284daee09e1d1aef
Jakob Öhman <jakob@29k.org>
2022-07-07 22:50:35 +0200

    Regular and Primary button

--- client/src/common/components/Buttons/Buttons.library.tsx
+++ client/src/common/components/Buttons/Buttons.library.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import ScreenWrapper from '../../../lib/uiLib/decorators/ScreenWrapper';
+import {Spacer16} from '../Spacers/Spacer';
 
 import Button from './Button';
 
 export const AllButtons = () => (
   <ScreenWrapper>
-    <Button onPress={() => undefined}>Dummy button</Button>
+    <Button>Regular button</Button>
+    <Spacer16 />
+    <Button primary>Primary button</Button>
   </ScreenWrapper>
 );
