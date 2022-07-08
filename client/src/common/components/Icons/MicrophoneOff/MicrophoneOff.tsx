b738579613b44a97e215f9172d7042552e128f81
Emil Rondahl <emil@29k.org>
2022-07-08 10:12:42 +0200

    Adding video and audio icons

---
+++ client/src/common/components/Icons/MicrophoneOff/MicrophoneOff.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import Svg, {Path, Rect} from 'react-native-svg';
+import {COLORS} from '../../../constants/colors';
+
+const style = StyleSheet.create({icon: {aspectRatio: 1}});
+
+export const MicrophoneOffIcon = ({fill = COLORS.GREY200}) => (
+  <Svg width="100%" height="100%" viewBox="0 0 30 30" style={style.icon}>
+    <Rect
+      x={11.75}
+      y={4.75}
+      width={6.5}
+      height={13.5}
+      rx={3.25}
+      stroke={fill}
+      strokeWidth={1.5}
+    />
+    <Path
+      d="M15 26v-3.667M8 15v.333a7 7 0 007 7v0a7 7 0 007-7V15M24 5.5L5.5 24"
+      stroke={fill}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+    />
+  </Svg>
+);
