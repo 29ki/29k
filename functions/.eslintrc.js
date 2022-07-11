39bda644e9c106f2e73071dfa66a0b5cec51a706
Jakob Öhman <jakob@29k.org>
2022-07-11 08:08:08 +0200

    Removed google eslint rules

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'google',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
