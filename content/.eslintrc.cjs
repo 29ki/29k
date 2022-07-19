9cff346aeb1f5ec7627fb3947561fffa00ed35b5
Jakob Öhman <jakob@29k.org>
2022-07-19 08:45:54 +0200

    Eslint setup for content

---
+++ content/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
