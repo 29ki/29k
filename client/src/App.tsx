80911d1134ab9e2b08e4b78d39a5965722ac4679
Jakob Öhman <jakob@29k.org>
2022-08-05 15:57:06 +0200

    Wrap app in ErrorBoundary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,19 @@ import Navigation from './lib/navigation/Navigation';
 import codePush, {CodePushOverlay} from './lib/codePush';
 import {UiLibProvider} from './lib/uiLib/hooks/useUiLib';
 import Bootstrap from './Bootstrap';
+import {ErrorBoundary} from './lib/sentry';
 
 const App = () => (
-  <RecoilRoot>
-    <UiLibProvider>
-      <Bootstrap>
-        <Navigation />
-        <CodePushOverlay />
-      </Bootstrap>
-    </UiLibProvider>
-  </RecoilRoot>
+  <ErrorBoundary>
+    <RecoilRoot>
+      <UiLibProvider>
+        <Bootstrap>
+          <Navigation />
+          <CodePushOverlay />
+        </Bootstrap>
+      </UiLibProvider>
+    </RecoilRoot>
+  </ErrorBoundary>
 );
 
 export default codePush(App);
