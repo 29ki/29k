617fad75826c5f729b974d92ba23533fbde07bf6
Kajsa Unge <hej@kajsaunge.se>
2022-08-16 10:22:34 +0200

    Used styled for text components

--- client/src/common/components/Typography/Heading/Heading.tsx
+++ client/src/common/components/Typography/Heading/Heading.tsx
@@ -1,37 +1,14 @@
-import React from 'react';
-import {Text, TextStyle} from 'react-native';
+import styled from 'styled-components/native';
 
 import typeStyles from '../styles';
 
-type HeadingBaseProps = {
-  style?: {} & TextStyle;
-  defaultStyles: TextStyle;
+const BaseHeading = styled.Text({});
+BaseHeading.defaultProps = {
+  allowFontScaling: false,
 };
-const Heading: React.FC<HeadingBaseProps> = ({
-  style = {},
-  defaultStyles,
-  children,
-}) => (
-  <Text allowFontScaling={false} style={[defaultStyles, style]}>
-    {children}
-  </Text>
-);
 
-type HeadingProps = {
-  style?: TextStyle;
-};
-export const H1: React.FC<HeadingProps> = props => (
-  <Heading {...props} defaultStyles={typeStyles.H1} />
-);
-export const H2: React.FC<HeadingProps> = props => (
-  <Heading {...props} defaultStyles={typeStyles.H2} />
-);
-export const H3: React.FC<HeadingProps> = props => (
-  <Heading {...props} defaultStyles={typeStyles.H3} />
-);
-export const H4: React.FC<HeadingProps> = props => (
-  <Heading {...props} defaultStyles={typeStyles.H4} />
-);
-export const H5: React.FC<HeadingProps> = props => (
-  <Heading {...props} defaultStyles={typeStyles.H5} />
-);
+export const H1 = styled(BaseHeading)(typeStyles.H1);
+export const H2 = styled(BaseHeading)(typeStyles.H2);
+export const H3 = styled(BaseHeading)(typeStyles.H3);
+export const H4 = styled(BaseHeading)(typeStyles.H4);
+export const H5 = styled(BaseHeading)(typeStyles.H5);
