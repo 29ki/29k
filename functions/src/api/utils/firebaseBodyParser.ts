d8f0cff044d00b1064bcca64095db9e9760fc526
Jakob Öhman <jakob@29k.org>
2022-08-19 08:58:38 +0200

    More explenations

--- functions/src/api/utils/firebaseBodyParser.ts
+++ functions/src/api/utils/firebaseBodyParser.ts
@@ -1,12 +1,12 @@
-import {Request} from 'firebase-functions';
-import {ExtendableContext, Next} from 'koa';
+import {Request as FirebaseRequest} from 'firebase-functions';
+import {Context as KoaContext, Next} from 'koa';
 
 /*
 Firebase parses body for us, so no need for a body parser
 https://github.com/koajs/bodyparser/issues/127
 */
-export type FirebaseContext = ExtendableContext & {
-  req: Request;
+export type FirebaseContext = KoaContext & {
+  req: FirebaseRequest;
 };
 
 const firebaseBodyParser = () => async (ctx: FirebaseContext, next: Next) => {
