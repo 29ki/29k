d8cef155134447f4640234b209e9b862527de409
Jakob Öhman <jakob@29k.org>
2022-08-21 12:37:08 +0200

    Use preferred device language

--- client/src/lib/i18n/index.ts
+++ client/src/lib/i18n/index.ts
@@ -1,6 +1,7 @@
 import 'intl-pluralrules';
 import i18next from 'i18next';
 import {initReactI18next} from 'react-i18next';
+import {findBestAvailableLanguage} from 'react-native-localize';
 
 import resources from '../../../../content/content.json';
 import {
@@ -8,12 +9,13 @@ import {
   LANGUAGE_TAGS,
 } from '../../../../shared/src/constants/i18n';
 
-export const init = () =>
+export const init = async () => {
   i18next.use(initReactI18next).init({
-    lng: 'en',
+    lng: findBestAvailableLanguage(LANGUAGE_TAGS)?.languageTag,
     supportedLngs: LANGUAGE_TAGS,
     fallbackLng: DEFAULT_LANGUAGE_TAG,
     resources,
   });
+};
 
 export default i18next;
