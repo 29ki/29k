32a73be64860c2c5d26be5de58305fe0113e1d02
Jakob Öhman <jakob@29k.org>
2022-08-22 16:13:07 +0200

    Koa context mock

--- functions/src/api/lib/i18nResolver.spec.ts
+++ functions/src/api/lib/i18nResolver.spec.ts
@@ -1,10 +1,10 @@
 import i18nResolver from './i18nResolver';
 import i18n from '../../lib/i18n';
-import {Context} from 'koa';
+import {createMockContext} from '@shopify/jest-koa-mocks';
 
 jest.mock('../../lib/i18n', () => ({
   cloneInstance: jest.fn(),
-  LANGUAGE_TAGS: ['some-supported-language-tag'],
+  LANGUAGE_TAGS: ['some-query-language', 'some-header-language'],
   DEFAULT_LANGUAGE_TAG: 'some-default-language-tag',
 }));
 
@@ -15,55 +15,75 @@ afterEach(() => {
 describe('i18nResolver', () => {
   it('supports lng query param', async () => {
     const middleware = i18nResolver();
-    const ctx = {
-      query: {
-        lng: 'some-language',
-      },
-    };
+    const ctx = createMockContext({
+      url: '?lng=some-query-language',
+    });
 
     const next = jest.fn();
 
-    await middleware(ctx as unknown as Context, next);
+    await middleware(ctx, next);
 
     expect(i18n.cloneInstance).toHaveBeenCalledTimes(1);
-    expect(i18n.cloneInstance).toHaveBeenCalledWith({lng: 'some-language'});
+    expect(i18n.cloneInstance).toHaveBeenCalledWith({
+      lng: 'some-query-language',
+    });
   });
 
   it('supports Accept-Language header', async () => {
     const middleware = i18nResolver();
+    const ctx = createMockContext({
+      headers: {'Accept-Language': 'some-header-language'},
+    });
 
-    const ctx = {
-      query: {},
-      acceptsLanguages: jest.fn().mockReturnValue('some-language'),
-    };
+    const acceptsLanguagesSpy = jest.spyOn(ctx, 'acceptsLanguages');
 
     const next = jest.fn();
 
-    await middleware(ctx as unknown as Context, next);
+    await middleware(ctx, next);
 
-    expect(ctx.acceptsLanguages).toHaveBeenCalledWith([
-      'some-supported-language-tag',
+    expect(acceptsLanguagesSpy).toHaveBeenCalledWith([
+      'some-default-language-tag',
+      'some-query-language',
+      'some-header-language',
     ]);
-    expect(ctx.acceptsLanguages).toHaveBeenCalledTimes(1);
+    expect(acceptsLanguagesSpy).toHaveBeenCalledTimes(1);
 
     expect(i18n.cloneInstance).toHaveBeenCalledTimes(1);
-    expect(i18n.cloneInstance).toHaveBeenCalledWith({lng: 'some-language'});
+    expect(i18n.cloneInstance).toHaveBeenCalledWith({
+      lng: 'some-header-language',
+    });
+  });
+
+  it('sets defaults language if Accept-Language language is not supported', async () => {
+    const middleware = i18nResolver();
+    const ctx = createMockContext({
+      headers: {'Accept-Language': 'some-unsupported-header-language'},
+    });
+
+    const next = jest.fn();
+
+    await middleware(ctx, next);
+
+    expect(i18n.cloneInstance).toHaveBeenCalledTimes(1);
+    expect(i18n.cloneInstance).toHaveBeenCalledWith({
+      lng: 'some-default-language-tag',
+    });
   });
 
   it('priorities query param over Accept-Language header', async () => {
     const middleware = i18nResolver();
-    const ctx = {
-      query: {
-        lng: 'some-query-language',
-      },
-      acceptsLanguages: jest.fn().mockReturnValue('some-header-language'),
-    };
+    const ctx = createMockContext({
+      url: '?lng=some-query-language',
+      headers: {'Accept-Language': 'some-header-language'},
+    });
+
+    const acceptsLanguagesSpy = jest.spyOn(ctx, 'acceptsLanguages');
 
     const next = jest.fn();
 
-    await middleware(ctx as unknown as Context, next);
+    await middleware(ctx, next);
 
-    expect(ctx.acceptsLanguages).toHaveBeenCalledTimes(0);
+    expect(acceptsLanguagesSpy).toHaveBeenCalledTimes(0);
 
     expect(i18n.cloneInstance).toHaveBeenCalledTimes(1);
     expect(i18n.cloneInstance).toHaveBeenCalledWith({
@@ -74,19 +94,20 @@ describe('i18nResolver', () => {
   it('sets default language if no language is supplied', async () => {
     const middleware = i18nResolver();
 
-    const ctx = {
-      query: {},
-      acceptsLanguages: jest.fn(),
-    };
+    const ctx = createMockContext();
+
+    const acceptsLanguagesSpy = jest.spyOn(ctx, 'acceptsLanguages');
 
     const next = jest.fn();
 
-    await middleware(ctx as unknown as Context, next);
+    await middleware(ctx, next);
 
-    expect(ctx.acceptsLanguages).toHaveBeenCalledWith([
-      'some-supported-language-tag',
+    expect(acceptsLanguagesSpy).toHaveBeenCalledWith([
+      'some-default-language-tag',
+      'some-query-language',
+      'some-header-language',
     ]);
-    expect(ctx.acceptsLanguages).toHaveBeenCalledTimes(1);
+    expect(acceptsLanguagesSpy).toHaveBeenCalledTimes(1);
 
     expect(i18n.cloneInstance).toHaveBeenCalledTimes(1);
     expect(i18n.cloneInstance).toHaveBeenCalledWith({
