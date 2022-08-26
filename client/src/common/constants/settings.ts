fb5293c8e17c3771771929db37f9ebaa601840cd
Kajsa Unge <hej@kajsaunge.se>
2022-08-26 16:20:51 +0200

    Extract style settings

---
+++ client/src/common/constants/settings.ts
@@ -0,0 +1,18 @@
+import {SPACINGS} from './spacings';
+
+export const SETTINGS = {
+  BORDER_RADIUS: {
+    CARDS: SPACINGS.TWENTYFOUR,
+  },
+  BOXSHADOW: {
+    shadowOffset: {
+      width: 0,
+      height: SPACINGS.FOUR,
+    },
+    shadowOpacity: 0.16,
+    shadowRadius: 6,
+    elevation: 10,
+  },
+};
+
+export default SETTINGS;
