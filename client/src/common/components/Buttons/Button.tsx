3f0a948daca3d0eccdb12afb7b2cb71ab2283535
Kajsa Unge <hej@kajsaunge.se>
2022-08-26 11:51:36 +0200

    Update button styles

--- client/src/common/components/Buttons/Button.tsx
+++ client/src/common/components/Buttons/Button.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import {COLORS} from '../../constants/colors';
-import {B1} from '../Typography/Text/Text';
+import {SPACINGS} from '../../constants/spacings';
+import {B2} from '../Typography/Text/Text';
 
 type ButtonProps = {
   primary?: boolean;
+  style?: object;
 };
 const ButtonComponent = styled.TouchableOpacity<ButtonProps>(({primary}) => ({
-  minHeight: 58,
-  backgroundColor: primary ? COLORS.GREY : 'transparent',
+  minHeight: 32,
+  backgroundColor: primary ? COLORS.GREY200 : COLORS.BLACK_EASY,
   paddingHorizontal: 24,
-  borderWidth: 1.5,
-  borderColor: COLORS.GREY,
-  borderRadius: 24,
+  borderRadius: SPACINGS.SIXTEEN,
   alignItems: 'center',
   justifyContent: 'center',
   flexDirection: 'row',
 }));
 
-export const ButtonText = styled(B1)<ButtonProps>(({primary}) => ({
-  color: primary ? COLORS.YELLOW : COLORS.GREY,
+export const ButtonText = styled(B2)<ButtonProps>(({primary}) => ({
+  color: primary ? COLORS.BLACK_EASY : COLORS.GREY100,
+  fontWeight: 600,
+  margin: 12,
 }));
 
 const Button: React.FC<{
   onPress?: () => void;
   primary?: boolean;
-}> = ({children, onPress = () => {}, primary = false}) => (
-  <ButtonComponent onPress={onPress} primary={primary}>
+  style?: object;
+}> = ({children, onPress = () => {}, primary = false, style = {}}) => (
+  <ButtonComponent onPress={onPress} primary={primary} style={style}>
     {typeof children === 'string' ? (
       <ButtonText primary={primary}>{children}</ButtonText>
     ) : (
