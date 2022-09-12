3928232dd24306b5249c36dece289cb858b10744
Jakob Öhman <jakob@29k.org>
2022-09-12 22:13:06 +0200

    Minor tweaks

---
+++ client/src/routes/Temple/components/Slides/Slides/Blocks/Heading.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {Spacer12} from '../../../../../../common/components/Spacers/Spacer';
+import {H2} from '../../../../../../common/components/Typography/Heading/Heading';
+
+const StyledHeading = styled(H2)({
+  textAlign: 'center',
+});
+
+const Heading: React.FC = ({children}) => (
+  <>
+    <Spacer12 />
+    <StyledHeading>{children}</StyledHeading>
+    <Spacer12 />
+  </>
+);
+
+export default Heading;
--- client/src/routes/Temple/components/Slides/Slides/Content.tsx
+++ client/src/routes/Temple/components/Slides/Slides/Content.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import styled from 'styled-components/native';
 import {
   ContentSlide,
   ReflectionSlide,
   SharingSlide,
 } from '../../../../../../../shared/src/types/Content';
+import Heading from './Blocks/Heading';
 import Image from '../../../../../common/components/Image/Image';
-import {Spacer12} from '../../../../../common/components/Spacers/Spacer';
-import {H2} from '../../../../../common/components/Typography/Heading/Heading';
 import Video from './Blocks/Video';
-
-const Heading = styled(H2)({
-  textAlign: 'center',
-});
+import {Spacer12} from '../../../../../common/components/Spacers/Spacer';
 
 type ContentProps = {
   slide: ReflectionSlide | SharingSlide | ContentSlide;
@@ -20,13 +15,7 @@ type ContentProps = {
 };
 const Content: React.FC<ContentProps> = ({slide, active}) => (
   <>
-    {slide.content.heading && (
-      <>
-        <Spacer12 />
-        <Heading>{slide.content.heading}</Heading>
-        <Spacer12 />
-      </>
-    )}
+    {slide.content.heading && <Heading>{slide.content.heading}</Heading>}
     {slide.content.video ? (
       <Video source={{uri: slide.content.video.source}} active={active} />
     ) : slide.content.image ? (
