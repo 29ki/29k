b4938b37103c63ba8923ca7213f852ecb4c15f10
Jonas <jonasandrade.mp@gmail.com>
2022-09-15 10:42:31 +0200

    Remove lineHeights

--- client/src/common/components/Typography/styles.ts
+++ client/src/common/components/Typography/styles.ts
@@ -10,54 +10,45 @@ const textStyles = StyleSheet.create({
   H1: {
     color: COLORS.GREY,
     fontSize: 40,
-    lineHeight: 48,
     fontFamily: OrpheusProMedium,
   },
   H2: {
     color: COLORS.GREY,
     fontSize: 32,
-    lineHeight: 41,
     fontFamily: OrpheusProMedium,
   },
   H3: {
     color: COLORS.GREY,
     fontSize: 24,
-    lineHeight: 31,
     fontFamily: OrpheusProMedium,
   },
   H4: {
     color: COLORS.GREY,
     fontSize: 20,
-    lineHeight: 28,
     fontFamily: OrpheusProMedium,
   },
   H5: {
     color: COLORS.GREY,
     fontSize: 16,
-    lineHeight: 22,
     fontFamily: OrpheusProMedium,
   },
   B1: {
     color: COLORS.GREY,
     fontSize: 20,
-    lineHeight: 26,
     fontFamily: HKGroteskMedium,
   },
   B2: {
     color: COLORS.GREY,
     fontSize: 16,
-    lineHeight: 18,
     fontFamily: HKGroteskMedium,
   },
   B3: {
     color: COLORS.GREY,
     fontSize: 14,
-    lineHeight: 16,
     fontFamily: HKGroteskMedium,
   },
   ERROR_TEXT: {
     fontSize: 14,
-    lineHeight: 20,
     color: COLORS.ERROR_PINK,
   },
   HIGHLIGHT: {
