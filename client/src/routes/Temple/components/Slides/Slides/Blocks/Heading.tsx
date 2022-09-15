68e2c3f43d51c79dfe27cd735d00730eb854be7b
Jakob Öhman <jakob@29k.org>
2022-09-15 20:14:17 +0200

    Add some gutters to the heading

--- client/src/routes/Temple/components/Slides/Slides/Blocks/Heading.tsx
+++ client/src/routes/Temple/components/Slides/Slides/Blocks/Heading.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import Gutters from '../../../../../../common/components/Gutters/Gutters';
 import {Spacer12} from '../../../../../../common/components/Spacers/Spacer';
 import {H2} from '../../../../../../common/components/Typography/Heading/Heading';
 
@@ -8,11 +9,11 @@ const StyledHeading = styled(H2)({
 });
 
 const Heading: React.FC<{children: React.ReactNode}> = ({children}) => (
-  <>
+  <Gutters>
     <Spacer12 />
     <StyledHeading>{children}</StyledHeading>
     <Spacer12 />
-  </>
+  </Gutters>
 );
 
 export default Heading;
