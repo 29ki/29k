26862f9e8db912fe13c3c0e66ca0d7453ac3f61c
Emil Rondahl <emil@29k.org>
2022-09-15 14:36:31 +0200

    Do not mute when start playing on sharing slide

--- client/src/routes/Temple/hooks/useMuteAudioListener.ts
+++ client/src/routes/Temple/hooks/useMuteAudioListener.ts
@@ -3,16 +3,18 @@ import {useRecoilValue} from 'recoil';
 
 import {DailyContext} from '../DailyProvider';
 import {templeExerciseStateSelector} from '../state/state';
+import useTempleExercise from './useTempleExercise';
 
 const useSubscribeToExerciseState = () => {
   const {toggleAudio} = useContext(DailyContext);
   const exerciseState = useRecoilValue(templeExerciseStateSelector);
+  const excercise = useTempleExercise();
 
   useEffect(() => {
-    if (exerciseState?.playing) {
+    if (exerciseState?.playing && excercise?.slide.current.type !== 'sharing') {
       toggleAudio(false);
     }
-  }, [toggleAudio, exerciseState]);
+  }, [toggleAudio, exerciseState, excercise]);
 };
 
 export default useSubscribeToExerciseState;
