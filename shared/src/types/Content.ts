137441eb6f5635d39199d8aa7d98ab29acde3162
Emil Rondahl <emil@29k.org>
2022-09-16 11:06:22 +0200

    Use new image structure

--- shared/src/types/Content.ts
+++ shared/src/types/Content.ts
@@ -39,7 +39,7 @@ export type Exercise = {
   id: string;
   name: string;
   card?: {
-    image?: string;
+    image?: {source: string; description?: string};
     backgroundColor?: string;
   };
   slides: ExerciseSlide[];
