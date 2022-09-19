d43bf6e8967236a405d48ab6b7a16143d1cc44e0
Emil Rondahl <emil@29k.org>
2022-09-19 16:02:26 +0200

    Portal videos in cms

--- cms/src/fields/exercise.js
+++ cms/src/fields/exercise.js
@@ -1,4 +1,10 @@
-import {CARD_FIELD, ID_FIELD, NAME_FIELD, PUBLISHED_FIELD} from './common';
+import {
+  CARD_FIELD,
+  ID_FIELD,
+  VIDEO_FIELD,
+  NAME_FIELD,
+  PUBLISHED_FIELD,
+} from './common';
 import {
   CONTENT_SLIDE,
   PARTICIPANT_SPOTLIGHT_SLIDE,
@@ -6,11 +12,34 @@ import {
   SHARING_SLIDE,
 } from './slides';
 
+const PORTAL_CONTENT = {
+  label: 'Content',
+  name: 'content',
+  widget: 'object',
+  collapsed: false,
+  required: false,
+  fields: [
+    {...VIDEO_FIELD, label: 'Video Loop', name: 'videoLoop'},
+    {...VIDEO_FIELD, label: 'Video End', name: 'videoEnd'},
+  ],
+};
+
+const INTRO_PORTAL = {
+  label: 'Intro Portal',
+  name: 'introPortal',
+  widget: 'object',
+  collapsed: true,
+  required: false,
+  i18n: true,
+  fields: [PORTAL_CONTENT],
+};
+
 export default [
   ID_FIELD,
   NAME_FIELD,
   PUBLISHED_FIELD,
   CARD_FIELD,
+  INTRO_PORTAL,
   {
     label: 'Slides',
     name: 'slides',
