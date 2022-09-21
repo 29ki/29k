87e1e27e9e813ef7872a101f9c3cf2bd4d01adb9
Emil Rondahl <emil@29k.org>
2022-09-21 16:30:51 +0200

    Wait for user to be authenticated before fetching temples

--- client/src/routes/Temples/hooks/useTemples.spec.ts
+++ client/src/routes/Temples/hooks/useTemples.spec.ts
@@ -1,8 +1,10 @@
 import {act, renderHook} from '@testing-library/react-hooks';
 import {RecoilRoot, useRecoilValue} from 'recoil';
+import {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import useTemples from './useTemples';
 import fetchMock, {enableFetchMocks} from 'jest-fetch-mock';
 import {isLoadingAtom, templesAtom} from '../state/state';
+import {userAtom} from '../../../lib/user/state/state';
 
 enableFetchMocks();
 
@@ -20,6 +22,7 @@ describe('useTemples', () => {
 
       return {fetchTemples, temples, isLoading};
     };
+
     it('should fetch temples', async () => {
       fetchMock.mockResponseOnce(
         JSON.stringify([
@@ -27,7 +30,15 @@ describe('useTemples', () => {
         ]),
         {status: 200},
       );
-      const {result} = renderHook(() => useTestHook(), {wrapper: RecoilRoot});
+      const {result} = renderHook(() => useTestHook(), {
+        wrapper: RecoilRoot,
+        initialProps: {
+          initializeState: ({set}) => {
+            set(userAtom, {} as FirebaseAuthTypes.User);
+          },
+          children: null,
+        },
+      });
 
       await act(async () => {
         await result.current.fetchTemples();
@@ -39,6 +50,25 @@ describe('useTemples', () => {
       ]);
     });
 
+    it('should not fetch temples when user is not yet authenticated', async () => {
+      fetchMock.mockResponseOnce(
+        JSON.stringify([
+          {id: 'temple-id', url: '/temple-url', name: 'Temple Name'},
+        ]),
+        {status: 200},
+      );
+      const {result} = renderHook(() => useTestHook(), {
+        wrapper: RecoilRoot,
+      });
+
+      await act(async () => {
+        await result.current.fetchTemples();
+      });
+
+      expect(fetchMock).toHaveBeenCalledTimes(0);
+      expect(result.current.temples).toEqual(null);
+    });
+
     it('should update loading state', async () => {
       fetchMock.mockResponseOnce(
         JSON.stringify([
@@ -46,7 +76,15 @@ describe('useTemples', () => {
         ]),
         {status: 200},
       );
-      const {result} = renderHook(() => useTestHook(), {wrapper: RecoilRoot});
+      const {result} = renderHook(() => useTestHook(), {
+        wrapper: RecoilRoot,
+        initialProps: {
+          initializeState: ({set}) => {
+            set(userAtom, {} as FirebaseAuthTypes.User);
+          },
+          children: null,
+        },
+      });
 
       const fetchPromise = act(async () => {
         await result.current.fetchTemples();
@@ -68,7 +106,15 @@ describe('useTemples', () => {
         }),
         {status: 200},
       );
-      const {result} = renderHook(() => useTemples(), {wrapper: RecoilRoot});
+      const {result} = renderHook(() => useTemples(), {
+        wrapper: RecoilRoot,
+        initialProps: {
+          initializeState: ({set}) => {
+            set(userAtom, {} as FirebaseAuthTypes.User);
+          },
+          children: null,
+        },
+      });
 
       await act(async () => {
         await result.current.addTemple('Temple name');
@@ -89,12 +135,40 @@ describe('useTemples', () => {
         headers: {'Content-Type': 'application/json'},
       });
     });
+
+    it('should not add a temple and refetch if user is not authenticated', async () => {
+      fetchMock.mockResponseOnce(
+        JSON.stringify({
+          id: 'temple-id',
+          url: '/temple-url',
+          name: 'Temple Name',
+        }),
+        {status: 200},
+      );
+      const {result} = renderHook(() => useTemples(), {
+        wrapper: RecoilRoot,
+      });
+
+      await act(async () => {
+        await result.current.addTemple('Temple name');
+      });
+
+      expect(fetchMock).toHaveBeenCalledTimes(0);
+    });
   });
 
   describe('deleteTemple', () => {
     it('should add a temple and refetch', async () => {
       fetchMock.mockResponseOnce('Success', {status: 200});
-      const {result} = renderHook(() => useTemples(), {wrapper: RecoilRoot});
+      const {result} = renderHook(() => useTemples(), {
+        wrapper: RecoilRoot,
+        initialProps: {
+          initializeState: ({set}) => {
+            set(userAtom, {} as FirebaseAuthTypes.User);
+          },
+          children: null,
+        },
+      });
 
       await act(async () => {
         await result.current.deleteTemple('temple-id');
