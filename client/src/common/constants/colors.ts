23573ae582fdbdb0bab242a553b583596a0fd8df
Kajsa Unge <hej@kajsaunge.se>
2022-09-21 14:50:49 +0200

    Update color contants and values

--- client/src/common/constants/colors.ts
+++ client/src/common/constants/colors.ts
@@ -1,58 +1,24 @@
 export const COLORS = {
-  //
-  // Base
-  //
-  BLACK: '#000000',
-  BLACK_EASY: '#2E2E2E',
-  BLACK_TRANSPARENT: 'rgba(0, 0, 0, 0.5)',
-  WHITE: '#FFFFFF',
-  WHITE_EASY: '#F9F8F4',
-  WHITE_TRANSPARENT: 'rgba(255, 255, 255, 0.15)',
-
-  //
-  // Greyscale
-  //
-  GREY100: '#FFFDF5',
-  GREY200: '#E6E4DC',
-  GREY300: '#CCCAC4',
-  GREY400: '#BFBEB9',
-  GREY500: '#999893',
-  GREY600: '#807E7A',
-  GREY800: '#595856',
-
-  // Brand   -- Primary, Secondary, Accent
-
   // Primary
-  ROSE300: '#FDE4D8',
-  ROSE500: '#E18A6C',
-  ROSE700: '#EA8585',
+  PRIMARY: '#378D88',
+  BLACK: '#2E2E2E',
+  ACTIVE: '#E18A6C',
+  WHITE: '#F9F8F4',
+  CREAM: '#FBF5E9',
+  PURE_WHITE: '#FFFFFF',
+
+  // Greyscale
+  GREYLIGHTEST: '#F5F3EB',
+  GREYLIGHTER: '#F0EEE7',
+  GREYLIGHT: '#E0DED6',
+  GREYMEDIUM: '#BFBEB9',
+  GREYDARK: '#72706C',
 
   // Secondary
-  LEAF300: '#008A87',
-  LEAF500: '#005545',
-  LEAF700: '#022217',
+  ACTION: '#2AE2D7',
+  ERROR: '#FF6464',
 
-  // Accent
-  VIOLET300: '#DFD7F9',
-  VIOLET500: '#B2AFED',
-  VIOLET700: '#8D98DD',
-
-  CREAM500: '#ECE8DE',
-  CREAM700: '#D5CDC3',
-  BODY500: '#E4CCBD',
-  BODY700: '#CEABA1',
-
-  //
-  // Functional  --  Information, Success, Warning, Danger
-  //
-  ERROR_PINK: '#F85F7D',
-  SUCCESS_GREEN: '#2CAC76',
-  HANG_UP_RED: '#E65d45',
-
-  // New colors?
-  GREY: '#383838',
-  YELLOW: '#FFEEBE',
-  YELLOW_LIGHT: '#F4EBC4',
-  GREEN_LIGHT: '#2AE2D7',
-  GREEN: '#378D88',
+  // Transparent
+  BLACK_TRANSPARENT: 'rgba(0, 0, 0, 0.5)',
+  WHITE_TRANSPARENT: 'rgba(255, 255, 255, 0.15)',
 };
