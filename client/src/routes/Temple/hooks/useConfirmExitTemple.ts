72de954ec6df1cb916043d27b0b99c2e3c58895d
Emil Rondahl <emil@29k.org>
2022-09-22 07:36:19 +0200

    Handle leave meeting in confirm exit

--- client/src/routes/Temple/hooks/useConfirmExitTemple.ts
+++ client/src/routes/Temple/hooks/useConfirmExitTemple.ts
@@ -1,15 +1,17 @@
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {useEffect} from 'react';
+import {useContext, useEffect} from 'react';
 import {Alert} from 'react-native';
 import {useTranslation} from 'react-i18next';
 
 import NS from '../../../lib/i18n/constants/namespaces';
 import {TabNavigatorProps} from '../../../common/constants/routes';
+import {DailyContext} from '../DailyProvider';
 
 type ScreenNavigationProps = NativeStackNavigationProp<TabNavigatorProps>;
 
 const useConfirmExitTemple = () => {
+  const {leaveMeeting} = useContext(DailyContext);
   const navigation = useNavigation<ScreenNavigationProps>();
   const {t} = useTranslation(NS.COMPONENT.CONFIRM_EXIT_TEMPLE);
 
@@ -23,13 +25,14 @@ const useConfirmExitTemple = () => {
           text: t('buttons.confirm'),
           style: 'destructive',
 
-          onPress: () => {
+          onPress: async () => {
+            await leaveMeeting();
             navigation.navigate('Temples');
           },
         },
       ]);
     });
-  }, [navigation, t]);
+  }, [navigation, t, leaveMeeting]);
 };
 
 export default useConfirmExitTemple;
