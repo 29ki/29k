65299d4d383a3c899d496847c75d7fd63a952bb2
Kajsa Unge <hej@kajsaunge.se>
2022-09-22 10:11:57 +0200

    Use common component

--- client/src/routes/Temple/components/Buttons/SlideButton.tsx
+++ client/src/routes/Temple/components/Buttons/SlideButton.tsx
@@ -1,27 +1,16 @@
 import styled from 'styled-components/native';
 
-import {COLORS} from '../../../../common/constants/colors';
-import {IconType} from '../../../../common/components/Icons';
 import {SPACINGS} from '../../../../common/constants/spacings';
-import IconButton from '../../../../common/components/Buttons/IconButton/IconButton';
-
-type SlideButtonProps = {
-  onPress?: () => void;
-  Icon: IconType;
-  disabled?: boolean;
-  fill?: string;
-};
+import IconButton, {
+  BaseIconButtonProps,
+} from '../../../../common/components/Buttons/IconButton/IconButton';
 
 const SlideButton = styled(IconButton).attrs({
-  fill: COLORS.BLACK,
-})<SlideButtonProps>(({disabled}) => ({
-  backgroundColor: COLORS.GREYLIGHTEST,
-  color: COLORS.BLACK,
+  variant: 'tertiary',
+  elevated: true,
+})<BaseIconButtonProps>(() => ({
   paddingVertical: SPACINGS.EIGHT,
   paddingHorizontal: SPACINGS.TWELVE,
-  boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.16)',
-  elevation: 1,
-  opacity: disabled ? 0 : 1,
 }));
 
 export default SlideButton;
