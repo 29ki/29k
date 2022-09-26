2818b309b70ebd424c0a5e25e53dff30f22a2943
Kajsa Unge <hej@kajsaunge.se>
2022-09-26 14:09:42 +0200

    Remove not needed platform select

--- client/src/common/constants/settings.ts
+++ client/src/common/constants/settings.ts
@@ -1,19 +1,16 @@
-import {Platform} from 'react-native';
 import {SPACINGS} from './spacings';
 
 export const SETTINGS = {
   BORDER_RADIUS: {
     CARDS: SPACINGS.TWENTYFOUR,
   },
-  BOXSHADOW: Platform.select({
-    ios: {
-      shadowColor: '#000000',
-      shadowOffset: {width: 0, height: SPACINGS.FOUR},
-      shadowOpacity: 0.16,
-      shadowRadius: SPACINGS.TWELVE,
-    },
-    android: {elevation: SPACINGS.TWELVE},
-  }),
+  BOXSHADOW: {
+    shadowColor: '#000000',
+    shadowOffset: {width: 0, height: SPACINGS.FOUR},
+    shadowOpacity: 0.16,
+    shadowRadius: SPACINGS.TWELVE,
+    elevation: SPACINGS.TWELVE,
+  },
 };
 
 export default SETTINGS;
