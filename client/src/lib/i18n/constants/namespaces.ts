b3c8c1acbd0cd53bfe99e198a600efea13c14693
Emil Rondahl <emil@29k.org>
2022-09-28 14:40:18 +0200

    Add to calender

--- client/src/lib/i18n/constants/namespaces.ts
+++ client/src/lib/i18n/constants/namespaces.ts
@@ -1,21 +1,22 @@
 const NS = {
   COMPONENT: {
+    ADD_TO_CALENDAR: 'Component.AddToCalendar',
     CODE_PUSH_OVERLAY: 'Component.CodePushOverlay',
+    CONFIRM_EXIT_TEMPLE: 'Component.ConfirmExitTemple',
+    COUNTER: 'Component.Counter',
     CRASH_ERROR_MESSAGE: 'Component.CrashErrorMessage',
     TABS: 'Component.Tabs',
     TEMPLE_CARD: 'Component.TempleCard',
-    COUNTER: 'Component.Counter',
-    CONFIRM_EXIT_TEMPLE: 'Component.ConfirmExitTemple',
     TEMPLE_MODAL: 'Component.TempleModal',
   },
   SCREEN: {
     CHANGING_ROOM: 'Screen.ChangingRoom',
     HOME: 'Screen.Home',
     KILL_SWITCH: 'Screen.KillSwitch',
+    PORTAL: 'Screen.Portal',
     PROFILE: 'Screen.Profile',
     TEMPLE: 'Screen.Temple',
     TEMPLES: 'Screen.Temples',
-    PORTAL: 'Screen.Portal',
   },
   EXERCISES: 'exercises',
 };
