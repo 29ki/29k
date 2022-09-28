ed83df4d18ebfe28a5bfe11db70416f464faa6e4
Jakob Öhman <jakob@29k.org>
2022-09-28 21:00:01 +0200

    Session reminder hook

---
+++ client/src/routes/Temples/hooks/useSessionNotificationReminder.ts
@@ -0,0 +1,32 @@
+import {Notification} from '@notifee/react-native';
+import dayjs from 'dayjs';
+import {useTranslation} from 'react-i18next';
+import {Temple} from '../../../../../shared/src/types/Temple';
+import useExerciseById from '../../../lib/content/hooks/useExerciseById';
+import NS from '../../../lib/i18n/constants/namespaces';
+import useTriggerNotification from '../../../lib/notifications/hooks/useTriggerNotifications';
+
+const useSessionNotificationReminder = (
+  temple: Temple,
+): [Notification | undefined, (enabled: boolean) => Promise<string | void>] => {
+  const {name, contentId} = temple;
+
+  const {t} = useTranslation(NS.COMPONENT.SESSION_REMINDER);
+  const exercise = useExerciseById(contentId);
+
+  const [notification, setNotification, removeNotification] =
+    useTriggerNotification(temple.id);
+
+  const setReminder = async (enabled = true) =>
+    enabled
+      ? setNotification(
+          t('title', {name, exercise: exercise?.name}),
+          t('body'),
+          dayjs().add(10, 'seconds').valueOf(),
+        )
+      : removeNotification();
+
+  return [notification, setReminder];
+};
+
+export default useSessionNotificationReminder;
