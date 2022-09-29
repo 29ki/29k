4c7e6d315c43d5f05d78a829145f91b964c8b9ca
Emil Rondahl <emil@29k.org>
2022-09-29 15:00:54 +0200

    Listening to notification updates inside recoil state

--- client/src/lib/notifications/hooks/useTriggerNotification.spec.ts
+++ client/src/lib/notifications/hooks/useTriggerNotification.spec.ts
@@ -1,34 +1,45 @@
 import {act, renderHook} from '@testing-library/react-hooks';
-import notifee, {EventType, Event} from '@notifee/react-native';
+import notifee from '@notifee/react-native';
 
 import useTriggerNotification from './useTriggerNotification';
+import {RecoilRoot} from 'recoil';
 
 const mockCreateTriggerNotification =
   notifee.createTriggerNotification as jest.Mock;
 const mockGetTriggerNotifications =
   notifee.getTriggerNotifications as jest.Mock;
-const mockOnForegroundEvent = notifee.onForegroundEvent as jest.Mock;
 const mockRequestPermission = notifee.requestPermission as jest.Mock;
 const mockCancelTriggerNotification =
   notifee.cancelTriggerNotification as jest.Mock;
 
-afterEach(() => {
+afterEach(async () => {
   jest.resetAllMocks();
 });
 
 describe('useTriggerNotification', () => {
   it('sets a trigger notification', async () => {
-    mockCreateTriggerNotification.mockReturnValueOnce('some-id');
+    mockGetTriggerNotifications.mockResolvedValueOnce([
+      {notification: {id: 'some-id'}},
+    ]);
 
-    const {result} = renderHook(() => useTriggerNotification('some-id'));
-
-    const notificationId = await result.current.setTriggerNotification(
-      'Some title',
-      'Some body',
-      123456789,
+    const {result, waitForNextUpdate} = renderHook(
+      () => useTriggerNotification('some-id'),
+      {
+        wrapper: RecoilRoot,
+      },
     );
 
-    expect(notificationId).toBe('some-id');
+    await waitForNextUpdate();
+
+    await act(async () => {
+      await result.current.setTriggerNotification(
+        'Some title',
+        'Some body',
+        123456789,
+      );
+    });
+
+    expect(result.current.triggerNotification).toMatchObject({id: 'some-id'});
     expect(mockRequestPermission).toHaveBeenCalledTimes(1);
     expect(mockCreateTriggerNotification).toHaveBeenCalledTimes(1);
     expect(mockCreateTriggerNotification).toHaveBeenCalledWith(
@@ -46,148 +57,38 @@ describe('useTriggerNotification', () => {
       },
     );
 
-    expect(result.all.length).toBe(1);
-  });
-
-  it('gets any existing trigger notification on mount', async () => {
-    mockGetTriggerNotifications.mockReturnValueOnce([
-      {notification: {id: 'some-id'}},
-      {notification: {id: 'some-other-id'}},
-    ]);
-
-    const {result, waitForNextUpdate} = renderHook(() =>
-      useTriggerNotification('some-id'),
-    );
-
-    await waitForNextUpdate();
-
-    expect(result.current.triggerNotification).toEqual({id: 'some-id'});
-
-    expect(mockGetTriggerNotifications).toHaveBeenCalledTimes(1);
-
-    expect(result.all.length).toBe(2);
-  });
-
-  it('subscribes to TRIGGER_NOTIFICATION_CREATED events for the set id', async () => {
-    let eventCallback: (event: Event) => void = () => {};
-    mockOnForegroundEvent.mockImplementationOnce(callback => {
-      eventCallback = callback;
-    });
-
-    const {result} = renderHook(() => useTriggerNotification('some-id'));
-
-    expect(result.current.triggerNotification).toBe(undefined);
-
-    expect(mockOnForegroundEvent).toHaveBeenCalledTimes(1);
-
-    act(() => {
-      eventCallback({
-        type: EventType.TRIGGER_NOTIFICATION_CREATED,
-        detail: {notification: {id: 'some-id'}},
-      });
-    });
-
-    expect(result.current.triggerNotification).toEqual({id: 'some-id'});
-
-    expect(result.all.length).toBe(2);
-  });
-
-  it('subscribes to DELIVERED events for the set id', async () => {
-    mockGetTriggerNotifications.mockReturnValueOnce([
-      {notification: {id: 'some-id'}},
-    ]);
-
-    let eventCallback: (event: Event) => void = () => {};
-    mockOnForegroundEvent.mockImplementationOnce(callback => {
-      eventCallback = callback;
-    });
-
-    const {result, waitForNextUpdate} = renderHook(() =>
-      useTriggerNotification('some-id'),
-    );
-
-    await waitForNextUpdate();
-
-    expect(result.current.triggerNotification).toEqual({id: 'some-id'});
-
-    expect(mockOnForegroundEvent).toHaveBeenCalledTimes(1);
-
-    act(() => {
-      eventCallback({
-        type: EventType.DELIVERED,
-        detail: {notification: {id: 'some-id'}},
-      });
-    });
-
-    expect(result.current.triggerNotification).toBe(undefined);
-
-    expect(result.all.length).toBe(3);
-  });
-
-  it("doesn't require an id to be set", async () => {
-    let eventCallback: (event: Event) => void = () => {};
-    mockOnForegroundEvent.mockImplementationOnce(callback => {
-      eventCallback = callback;
-    });
-    mockCreateTriggerNotification.mockReturnValueOnce('some-generated-id');
-
-    const {result} = renderHook(() => useTriggerNotification());
-
-    expect(result.current.triggerNotification).toBe(undefined);
-
-    await result.current.setTriggerNotification(
-      'Some title',
-      'Some body',
-      123456789,
-    );
-
-    act(() => {
-      eventCallback({
-        type: EventType.TRIGGER_NOTIFICATION_CREATED,
-        detail: {notification: {id: 'some-generated-id'}},
-      });
-    });
-
-    expect(result.current.triggerNotification).toEqual({
-      id: 'some-generated-id',
-    });
-
     expect(result.all.length).toBe(2);
   });
 
   it('supports removing the notification', async () => {
-    mockGetTriggerNotifications.mockReturnValueOnce([
-      {notification: {id: 'some-id'}},
+    mockGetTriggerNotifications.mockResolvedValueOnce([
+      {notification: {id: 'some-other-id'}},
     ]);
 
-    const {result, waitForNextUpdate} = renderHook(() =>
-      useTriggerNotification('some-id'),
+    const {result, waitForNextUpdate} = renderHook(
+      /*
+        Recoil caches selectors between recoil roots - easiest to use a different ID here
+        https://recoiljs.org/docs/guides/testing/#clearing-all-selector-caches
+      */
+      () => useTriggerNotification('some-other-id'),
+      {
+        wrapper: RecoilRoot,
+      },
     );
 
     await waitForNextUpdate();
 
-    expect(result.current.triggerNotification).toEqual({id: 'some-id'});
+    expect(result.current.triggerNotification).toEqual({id: 'some-other-id'});
 
-    result.current.removeTriggerNotification();
-
-    await waitForNextUpdate();
+    await act(async () => {
+      await result.current.removeTriggerNotification();
+    });
 
     expect(result.current.triggerNotification).toBe(undefined);
 
     expect(mockCancelTriggerNotification).toHaveBeenCalledTimes(1);
-    expect(mockCancelTriggerNotification).toHaveBeenCalledWith('some-id');
+    expect(mockCancelTriggerNotification).toHaveBeenCalledWith('some-other-id');
 
-    expect(result.all.length).toBe(3);
-  });
-
-  it('unsubscribes from notification events on unmount', async () => {
-    const mockUnsubscribe = jest.fn();
-    mockOnForegroundEvent.mockImplementationOnce(() => mockUnsubscribe);
-
-    const {unmount} = renderHook(() => useTriggerNotification());
-
-    unmount();
-
-    expect(mockUnsubscribe).toHaveBeenCalledTimes(1);
+    expect(result.all.length).toBe(2);
   });
 });
--- client/src/lib/notifications/hooks/useTriggerNotification.ts
+++ client/src/lib/notifications/hooks/useTriggerNotification.ts
@@ -1,84 +1,43 @@
-import {useEffect, useRef, useState} from 'react';
-import notifee, {
-  EventType,
-  Notification,
-  TimestampTrigger,
-  TriggerType,
-} from '@notifee/react-native';
+import {useCallback} from 'react';
+import notifee, {TimestampTrigger, TriggerType} from '@notifee/react-native';
+import {useRecoilState} from 'recoil';
+import {notificationAtom} from '../state/state';
 
-const getTriggerNotificationById = async (notificationId: string) =>
-  (await notifee.getTriggerNotifications()).find(
-    ({notification}) => notification.id === notificationId,
+const useTriggerNotification = (id: string) => {
+  const [triggerNotification, setNotification] = useRecoilState(
+    notificationAtom(id),
   );
 
-const useTriggerNotification = (id?: string) => {
-  const notificationId = useRef<string | undefined>(id);
-  const [triggerNotification, setNotification] = useState<Notification>();
+  const setTriggerNotification = useCallback(
+    async (title: string, body: string, timestamp: number) => {
+      // TODO: handle declined permissions better
+      await notifee.requestPermission();
 
-  const getExisitingNotification = async () => {
-    if (notificationId.current) {
-      setNotification(
-        (await getTriggerNotificationById(notificationId.current))
-          ?.notification,
-      );
-    }
-  };
+      const trigger: TimestampTrigger = {
+        type: TriggerType.TIMESTAMP,
+        timestamp,
+      };
 
-  const subscribeToNotifications = () =>
-    notifee.onForegroundEvent(({type, detail}) => {
-      if (detail.notification?.id === notificationId.current) {
-        switch (type) {
-          case EventType.TRIGGER_NOTIFICATION_CREATED:
-            setNotification(detail.notification);
-            break;
-
-          case EventType.DELIVERED:
-            setNotification(undefined);
-            break;
-        }
-      }
-    });
-
-  useEffect(() => {
-    getExisitingNotification();
-    return subscribeToNotifications();
-  }, []);
-
-  const setTriggerNotification = async (
-    title: string,
-    body: string,
-    timestamp: number,
-  ) => {
-    // TODO: handle declined permissions better
-    await notifee.requestPermission();
-
-    const trigger: TimestampTrigger = {
-      type: TriggerType.TIMESTAMP,
-      timestamp,
-    };
-
-    // Create a trigger notification
-    notificationId.current = await notifee.createTriggerNotification(
-      {
-        id: notificationId.current,
+      // Create a trigger notification
+      const notification = {
+        id,
         title,
         body,
         android: {
           channelId: 'reminders',
         },
-      },
-      trigger,
-    );
+      };
 
-    return notificationId.current;
-  };
+      setNotification(notification);
+
+      await notifee.createTriggerNotification(notification, trigger);
+    },
+    [setNotification, id],
+  );
 
   const removeTriggerNotification = async () => {
-    if (notificationId.current) {
-      await notifee.cancelTriggerNotification(notificationId.current);
-      setNotification(undefined);
-      notificationId.current = id;
-    }
+    await notifee.cancelTriggerNotification(id);
+    setNotification(undefined);
   };
 
   return {
