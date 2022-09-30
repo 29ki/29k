42dc56ef0beda399f2b6898374b6a7f1ab12cf85
Emil Rondahl <emil@29k.org>
2022-09-30 11:06:20 +0200

    Update test to know initial state

--- client/src/lib/notifications/state/state.spec.ts
+++ client/src/lib/notifications/state/state.spec.ts
@@ -1,5 +1,5 @@
-import {snapshot_UNSTABLE} from 'recoil';
 import notifee, {EventType, Event} from '@notifee/react-native';
+import {snapshot_UNSTABLE} from 'recoil';
 import {notificationAtom} from './state';
 
 const mockGetTriggerNotifications =
@@ -7,6 +7,8 @@ const mockGetTriggerNotifications =
 const mockOnForegroundEvent = notifee.onForegroundEvent as jest.Mock;
 
 type EventCallback = (event?: Event) => Promise<void>;
+
+beforeEach(() => jest.resetModules());
 
 // Since recoil somehow keeps state between tests for atomFamiliy
 // each test have to create state with uniqe key between tests
@@ -25,22 +27,20 @@ describe('notifications - state', () => {
   });
 
   it('adds notification to state on TRIGGER_NOTIFICATION_CREATED', async () => {
-    const notification = {id: 'some-id-2'};
-    mockGetTriggerNotifications
-      .mockResolvedValueOnce([])
-      .mockResolvedValueOnce([{notification}]);
+    const notification = {id: 'some-id-1'};
+    mockGetTriggerNotifications.mockResolvedValue([{notification}]);
 
     let eventCallback: EventCallback = () => Promise.resolve();
     mockOnForegroundEvent.mockImplementationOnce(callback => {
       eventCallback = callback;
     });
 
-    const initialSnapshot = snapshot_UNSTABLE();
     const state = notificationAtom(notification.id);
+    const initialSnapshot = snapshot_UNSTABLE(({set}) => {
+      set(state, undefined);
+    });
 
-    expect(await initialSnapshot.getLoadable(state).promiseOrThrow()).toBe(
-      undefined,
-    ); // default is a promise, needs to be awaited
+    expect(initialSnapshot.getLoadable(state).valueOrThrow()).toBe(undefined);
 
     await eventCallback({
       type: EventType.TRIGGER_NOTIFICATION_CREATED,
@@ -49,26 +49,26 @@ describe('notifications - state', () => {
 
     expect(initialSnapshot.getLoadable(state).valueOrThrow()).toMatchObject(
       notification,
-    ); // next state is not taken from default, can't be awaited
+    );
   });
 
   it('removes notification from state on DELIVERED', async () => {
-    const notification = {id: 'some-id-3'};
-    mockGetTriggerNotifications
-      .mockResolvedValueOnce([{notification}])
-      .mockResolvedValueOnce([]);
+    const notification = {id: 'some-id-1'};
+    mockGetTriggerNotifications.mockResolvedValue([]);
 
     let eventCallback: EventCallback = () => Promise.resolve();
     mockOnForegroundEvent.mockImplementationOnce(callback => {
       eventCallback = callback;
     });
 
-    const initialSnapshot = snapshot_UNSTABLE();
     const state = notificationAtom(notification.id);
+    const initialSnapshot = snapshot_UNSTABLE(({set}) => {
+      set(state, notification);
+    });
 
-    expect(
-      await initialSnapshot.getLoadable(state).promiseOrThrow(),
-    ).toMatchObject(notification); // default is a promise, needs to be awaited
+    expect(initialSnapshot.getLoadable(state).valueOrThrow()).toMatchObject(
+      notification,
+    );
 
     await eventCallback({
       type: EventType.DELIVERED,
@@ -76,6 +76,5 @@ describe('notifications - state', () => {
     });
 
     expect(initialSnapshot.getLoadable(state).valueOrThrow()).toBe(undefined);
-    // next state is not taken from default, can't be awaited
   });
 });
