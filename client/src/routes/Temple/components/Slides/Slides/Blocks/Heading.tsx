c6c36696ef60ab5f827d10a8d32f0f5324a74d9c
Kajsa Unge <hej@kajsaunge.se>
2022-10-03 11:28:52 +0200

    Fix Heading size

--- client/src/routes/Temple/components/Slides/Slides/Blocks/Heading.tsx
+++ client/src/routes/Temple/components/Slides/Slides/Blocks/Heading.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components/native';
 import Gutters from '../../../../../../common/components/Gutters/Gutters';
 import {Spacer12} from '../../../../../../common/components/Spacers/Spacer';
-import {Display36} from '../../../../../../common/components/Typography/Display/Display';
+import {Display24} from '../../../../../../common/components/Typography/Display/Display';
 
-const StyledHeading = styled(Display36)({
+const StyledHeading = styled(Display24)({
   textAlign: 'center',
 });
 
