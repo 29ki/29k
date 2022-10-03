b4df17c45d78a160c13c8bcbb7444df6676a1c5b
Kajsa Unge <hej@kajsaunge.se>
2022-10-03 09:59:56 +0200

    WIP - components and icons

--- client/src/common/components/Icons/index.ts
+++ client/src/common/components/Icons/index.ts
@@ -8,6 +8,7 @@ export type IconType = React.FC<{
 export * from './ArrowLeft/ArrowLeft';
 export * from './Back/Back';
 export * from './Bell/Bell';
+export * from './BackwardCircle/BackwardCircle';
 export * from './ChevronLeft/ChevronLeft';
 export * from './ChevronRight/ChevronRight';
 export * from './Close/Close';
@@ -16,6 +17,7 @@ export * from './Ellipsis/Ellipsis';
 export * from './FilmCamera/FilmCamera';
 export * from './FilmCameraOff/FilmCameraOff';
 export * from './Forward/Forward';
+export * from './ForwardCircle/ForwardCircle';
 export * from './HangUp/HangUp';
 export * from './Home/Home';
 export * from './HomeFill/HomeFill';
