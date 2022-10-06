a6a950ccb0be7e498e3a2f77249fce57ecf8cccb
Kajsa Unge <hej@kajsaunge.se>
2022-10-06 15:58:14 +0200

    Animate content

--- client/src/routes/Temple/components/HostNotes/HostNotes.tsx
+++ client/src/routes/Temple/components/HostNotes/HostNotes.tsx
@@ -11,16 +11,20 @@ import {COLORS} from '../../../../../../shared/src/constants/colors';
 import {BackwardCircleIcon} from '../../../../common/components/Icons/BackwardCircle/BackwardCircle';
 import {ForwardCircleIcon} from '../../../../common/components/Icons/ForwardCircle/ForwardCircle';
 import {
+  Spacer28,
   Spacer8,
   TopSafeArea,
 } from '../../../../common/components/Spacers/Spacer';
 import Gutters from '../../../../common/components/Gutters/Gutters';
 import useTempleExercise from '../../hooks/useTempleExercise';
 import Animated, {
+  Easing,
+  FadeInUp,
   RotateInUpLeft,
   RotateInUpRight,
   RotateOutDownLeft,
   RotateOutDownRight,
+  SlideOutUp,
 } from 'react-native-reanimated';
 
 // Toggle open close
@@ -31,6 +35,7 @@ import Animated, {
 // Progressbar to replicate exercise slides?
 // Show note numbering based on?
 
+const BoxShadowWrapper = styled.View({...SETTINGS.BOXSHADOW});
 const Wrapper = styled.View({
   position: 'absolute',
   top: 0,
@@ -39,11 +44,12 @@ const Wrapper = styled.View({
   borderBottomLeftRadius: SETTINGS.BORDER_RADIUS.CARDS,
   borderBottomRightRadius: SETTINGS.BORDER_RADIUS.CARDS,
   backgroundColor: COLORS.WHITE,
-  ...SETTINGS.BOXSHADOW,
+  zIndex: 2,
 });
 const Row = styled.View({
   flexDirection: 'row',
   alignItems: 'center',
+  zIndex: 2,
 });
 const NavWrapper = styled.View({
   flexDirection: 'row',
@@ -73,16 +79,44 @@ const AnimatedMinusIcon = () => {
     </Animated.View>
   );
 };
+
+const NotesWrapper = styled(Animated.View).attrs({
+  entering: FadeInUp.duration(300).easing(Easing.bezier(0.34, 1.56, 0.64, 1)),
+  exiting: SlideOutUp.duration(600),
+})({
+  borderBottomLeftRadius: SETTINGS.BORDER_RADIUS.CARDS,
+  borderBottomRightRadius: SETTINGS.BORDER_RADIUS.CARDS,
+  backgroundColor: COLORS.WHITE,
+  zIndex: 1,
+  marginTop: -25,
+});
+const Navigation = styled.View({
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+const TextWrapper = styled.View({
+  overflow: 'hidden',
+});
+
+type HostNotesProps = {
+  exercise: object;
+};
+
+const HostNotes: React.FC<HostNotesProps> = () => {
   const [showNotes, setShowNotes] = useState(false);
+  let [activeIndex, setActiveIndex] = useState(0);
   const exercise = useTempleExercise();
 
   console.log(exercise);
   return (
+    <BoxShadowWrapper>
     <Wrapper>
       <TopSafeArea />
       <Gutters>
         <Spacer8 />
-        <Row>
+        <Gutters>
+          <TopBar>
           <Progress index={1} length={5} />
           <Spacer8 />
           <Button
@@ -92,17 +126,32 @@ const AnimatedMinusIcon = () => {
             variant="tertiary">
             {'Notes'}
           </Button>
-        </Row>
-        <Spacer8 />
-        <Body14>{'NOTES JOE'}</Body14>
-        <NavWrapper>
-          <NavButton Icon={BackwardCircleIcon} />
-          <Body14>{'1/2'}</Body14>
-          <NavButton Icon={ForwardCircleIcon} />
-        </NavWrapper>
+          </TopBar>
         <Spacer8 />
       </Gutters>
     </Wrapper>
+      {!showNotes && (
+        <NotesWrapper>
+          <Gutters>
+            <Spacer28 />
+            <Navigation>
+              <NavButton
+                onPress={onBackward}
+                Icon={BackwardCircleIcon}
+                disabled={activeIndex <= 0}
+              />
+              <Body14>{`${activeIndex + 1} / ${portalNotes.length}`}</Body14>
+              <NavButton
+                onPress={onForward}
+                Icon={ForwardCircleIcon}
+                disabled={activeIndex >= portalNotes.length - 1}
+              />
+            </Navigation>
+          </Gutters>
+          <Spacer8 />
+        </NotesWrapper>
+      )}
+    </BoxShadowWrapper>
   );
 };
 
