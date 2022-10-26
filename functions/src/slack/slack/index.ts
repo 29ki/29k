53a5c45bebe530cab33b22439b334d3e99d9bac2
Emil Rondahl <emil@29k.org>
2022-10-26 14:05:03 +0200

    Propagate slack errors to sentry

--- functions/src/slack/slack/index.ts
+++ functions/src/slack/slack/index.ts
@@ -8,8 +8,8 @@ slackRouter.post('/', async ctx => {
     await slackHandler(ctx.req.body?.payload as string);
     ctx.status = 200;
   } catch (error) {
-    console.error('Error handling slack action', error);
     ctx.status = 500;
+    throw error;
   }
 });
 
