a3b365808d2fd56a793aea02ee9e7ef1901290ee
Kajsa Unge <hej@kajsaunge.se>
2022-10-27 11:54:45 +0200

    Use rn scrollview

--- client/src/common/components/Screen/Screen.library.tsx
+++ client/src/common/components/Screen/Screen.library.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView} from 'react-native-gesture-handler';
+import {ScrollView} from 'react-native';
 import {Display36} from '../Typography/Display/Display';
 import Screen from './Screen';
 
