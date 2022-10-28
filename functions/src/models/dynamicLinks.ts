b6a863d4b005832dd6004dfd5ae0abf2e70db73b
Jakob Öhman <jakob@29k.org>
2022-10-28 13:30:27 +0200

    Remove need for NS

--- functions/src/models/dynamicLinks.ts
+++ functions/src/models/dynamicLinks.ts
@@ -6,7 +6,6 @@ import config from '../lib/config';
 import i18next, {LANGUAGE_TAG} from '../lib/i18n';
 import type {Exercise} from '../../../shared/src/types/generated/Exercise';
 import dayjs from 'dayjs';
-import * as NS from '../../../shared/src/constants/namespaces';
 
 const dynamicLinks = firebasedynamiclinks('v1');
 
@@ -73,7 +72,7 @@ export const createSessionLink = async (
     returnObjects: true,
   }) as Exercise;
 
-  const t = i18next.getFixedT(language, NS.DEEP_LINK.SESSION);
+  const t = i18next.getFixedT(language, 'DeepLink.Session');
 
   const date = dayjs(startTime).locale(language).format('dddd, D MMM HH:mm');
 
