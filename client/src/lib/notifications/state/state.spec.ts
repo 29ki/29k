5203c5f67fe528ee5092a714a926e5735f7bdf90
Jakob Öhman <jakob@29k.org>
2022-11-05 22:32:11 +0100

    notification state tests

--- client/src/lib/notifications/state/state.spec.ts
+++ client/src/lib/notifications/state/state.spec.ts
@@ -1,80 +1,49 @@
 import notifee, {EventType, Event} from '@notifee/react-native';
-import {snapshot_UNSTABLE} from 'recoil';
-import {notificationAtom} from './state';
+import {renderHook} from '@testing-library/react-hooks';
 
 const mockGetTriggerNotifications =
   notifee.getTriggerNotifications as jest.Mock;
 const mockOnForegroundEvent = notifee.onForegroundEvent as jest.Mock;
 
 type EventCallback = (event?: Event) => Promise<void>;
+let eventCallback: EventCallback = () => Promise.resolve();
+mockOnForegroundEvent.mockImplementation(callback => {
+  eventCallback = callback;
+});
 
-beforeEach(() => jest.resetModules());
+/* This needs to be imported after onForegroundEvent mock implementation as been set
+   as the event is subscribed to in the state create function */
+const {default: useNotificationsState} = require('./state');
 
-// Since recoil somehow keeps state between tests for atomFamiliy
-// each test have to create state with uniqe key between tests
 describe('notifications - state', () => {
-  it('loads initial state', async () => {
-    const notification = {id: 'some-id-1'};
-    mockGetTriggerNotifications.mockResolvedValueOnce([{notification}]);
-
-    const initialSnapshot = snapshot_UNSTABLE();
-
-    expect(
-      await initialSnapshot
-        .getLoadable(notificationAtom(notification.id))
-        .promiseOrThrow(), // default is a promise, needs to be awaited
-    ).toMatchObject(notification);
-  });
-
   it('adds notification to state on TRIGGER_NOTIFICATION_CREATED', async () => {
     const notification = {id: 'some-id-1'};
     mockGetTriggerNotifications.mockResolvedValue([{notification}]);
 
-    let eventCallback: EventCallback = () => Promise.resolve();
-    mockOnForegroundEvent.mockImplementationOnce(callback => {
-      eventCallback = callback;
-    });
-
-    const state = notificationAtom(notification.id);
-    const initialSnapshot = snapshot_UNSTABLE(({set}) => {
-      set(state, undefined);
-    });
-
-    expect(initialSnapshot.getLoadable(state).valueOrThrow()).toBe(undefined);
+    const {result} = renderHook(() => useNotificationsState());
 
     await eventCallback({
       type: EventType.TRIGGER_NOTIFICATION_CREATED,
       detail: {notification},
     });
 
-    expect(initialSnapshot.getLoadable(state).valueOrThrow()).toMatchObject(
-      notification,
-    );
+    expect(result.current.notifications).toEqual({'some-id-1': notification});
   });
 
   it('removes notification from state on DELIVERED', async () => {
     const notification = {id: 'some-id-1'};
+    useNotificationsState.setState({
+      notifications: {'some-id-1': notification},
+    });
     mockGetTriggerNotifications.mockResolvedValue([]);
 
-    let eventCallback: EventCallback = () => Promise.resolve();
-    mockOnForegroundEvent.mockImplementationOnce(callback => {
-      eventCallback = callback;
-    });
-
-    const state = notificationAtom(notification.id);
-    const initialSnapshot = snapshot_UNSTABLE(({set}) => {
-      set(state, notification);
-    });
-
-    expect(initialSnapshot.getLoadable(state).valueOrThrow()).toMatchObject(
-      notification,
-    );
+    const {result} = renderHook(() => useNotificationsState());
 
     await eventCallback({
       type: EventType.DELIVERED,
       detail: {notification},
     });
 
-    expect(initialSnapshot.getLoadable(state).valueOrThrow()).toBe(undefined);
+    expect(result.current.notifications).toEqual({});
   });
 });
--- client/src/lib/notifications/state/state.ts
+++ client/src/lib/notifications/state/state.ts
@@ -1,27 +1,6 @@
 import notifee, {EventType, Notification} from '@notifee/react-native';
-import {atomFamily} from 'recoil';
 import {getTriggerNotificationById} from '../utils';
 import create from 'zustand';
-
-const NAMESPACE = 'notification';
-
-export const notificationAtom = atomFamily<Notification | undefined, string>({
-  key: `${NAMESPACE}/notification`,
-  default: async id => await getTriggerNotificationById(id),
-  effects: id => [
-    ({setSelf}) =>
-      notifee.onForegroundEvent(async ({type, detail}) => {
-        if (detail.notification?.id === id) {
-          switch (type) {
-            case EventType.TRIGGER_NOTIFICATION_CREATED:
-            case EventType.DELIVERED:
-              setSelf(await getTriggerNotificationById(id));
-              break;
-          }
-        }
-      }),
-  ],
-});
 
 type State = {
   notifications: {[key: string]: Notification | undefined};
