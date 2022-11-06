c9c9d5cd851c42f7b83a357fd988234457f742e3
Jakob Öhman <jakob@29k.org>
2022-11-06 08:51:12 +0100

    Migrate session to zustand

--- client/src/routes/Session/state/state.ts
+++ client/src/routes/Session/state/state.ts
@@ -1,21 +1,5 @@
-import {atom, selector} from 'recoil';
-import {
-  ExerciseStateData,
-  SessionData,
-} from '../../../../../shared/src/types/Session';
+import {SessionData} from '../../../../../shared/src/types/Session';
 import create from 'zustand';
-
-const SESSION_NAMESPACE = 'Session';
-
-export const sessionAtom = atom<SessionData | null>({
-  key: `${SESSION_NAMESPACE}/session`,
-  default: null,
-});
-
-export const sessionExerciseStateSelector = selector<ExerciseStateData | null>({
-  key: `${SESSION_NAMESPACE}/exerciseStateSelector`,
-  get: ({get}) => get(sessionAtom)?.exerciseState || null,
-});
 
 type State = {
   session: SessionData | null;
