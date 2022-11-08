ee2fa3c0243b97299b8f0798883bcf08eb9f1b34
Jonas <jonasandrade.mp@gmail.com>
2022-11-08 19:16:52 +0100

    Handle non-joinable session on subscribe

--- client/src/routes/Session/hooks/useSubscribeToSession.spec.ts
+++ client/src/routes/Session/hooks/useSubscribeToSession.spec.ts
@@ -1,7 +1,12 @@
 import {renderHook} from '@testing-library/react-hooks';
 import firestore from '@react-native-firebase/firestore';
-
+import {useNavigation} from '@react-navigation/native';
 import useSessionState from '../state/state';
+import useSessions from '../../Sessions/hooks/useSessions';
+jest.mock('../../Sessions/hooks/useSessions');
+
+const mockUseSessions = useSessions as jest.Mock;
+
 import useSubscribeToSession from './useSubscribeToSession';
 
 afterEach(() => {
@@ -9,6 +14,10 @@ afterEach(() => {
 });
 
 describe('useSubscribeToSession', () => {
+  const fetchSessionsMock = jest.fn();
+  mockUseSessions.mockReturnValue({fetchSessions: fetchSessionsMock});
+  const navigation = useNavigation();
+
   const useTestHook = () => {
     useSubscribeToSession('session-id');
     const session = useSessionState(state => state.session);
@@ -33,5 +42,35 @@ describe('useSubscribeToSession', () => {
     const {result} = renderHook(() => useTestHook());
 
     expect(result.current).toEqual({id: 'test-id'});
+  });
+
+  it('should handle when session does not exist', () => {
+    (
+      firestore().collection('sessions').doc().onSnapshot as jest.Mock
+    ).mockImplementationOnce(cb => {
+      cb({exists: false, data: () => undefined});
+    });
+
+    const {result} = renderHook(() => useTestHook());
+    expect(result.current).toBe(null);
+
+    expect(fetchSessionsMock).toHaveBeenCalledTimes(1);
+    expect(navigation.navigate).toHaveBeenCalledWith('Sessions');
+    expect(navigation.navigate).toHaveBeenCalledWith('SessionUnavailableModal');
+  });
+
+  it('should handle when session has ended', () => {
+    (
+      firestore().collection('sessions').doc().onSnapshot as jest.Mock
+    ).mockImplementationOnce(cb => {
+      cb({exists: true, data: () => ({ended: true})});
+    });
+
+    const {result} = renderHook(() => useTestHook());
+    expect(result.current).toBe(null);
+
+    expect(fetchSessionsMock).toHaveBeenCalledTimes(1);
+    expect(navigation.navigate).toHaveBeenCalledWith('Sessions');
+    expect(navigation.navigate).toHaveBeenCalledWith('SessionUnavailableModal');
   });
 });
--- client/src/routes/Session/hooks/useSubscribeToSession.ts
+++ client/src/routes/Session/hooks/useSubscribeToSession.ts
@@ -2,16 +2,40 @@ import {useEffect} from 'react';
 import firestore from '@react-native-firebase/firestore';
 import useSessionState from '../state/state';
 import {Session, SessionData} from '../../../../../shared/src/types/Session';
+import useSessions from '../../Sessions/hooks/useSessions';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {
+  ModalStackProps,
+  TabNavigatorProps,
+} from '../../../lib/navigation/constants/routes';
 
 const useSubscribeToSession = (sessionId: Session['id']) => {
   const setSessionState = useSessionState(state => state.setState);
+
+  const {navigate} =
+    useNavigation<
+      NativeStackNavigationProp<TabNavigatorProps & ModalStackProps>
+    >();
+
+  const {fetchSessions} = useSessions();
 
   useEffect(() => {
     const doc = firestore().collection('sessions').doc(sessionId);
 
     const unsubscribe = doc.onSnapshot(
-      documentSnapshot =>
-        setSessionState(documentSnapshot.data() as SessionData),
+      documentSnapshot => {
+        const session = documentSnapshot.data() as SessionData;
+
+        if (!documentSnapshot.exists || session.ended) {
+          fetchSessions();
+          navigate('Sessions');
+          navigate('SessionUnavailableModal');
+          return;
+        }
+
+        setSessionState(session);
+      },
       error =>
         console.debug(
           `Failed to subscribe to live session ${sessionId}`,
@@ -20,7 +44,7 @@ const useSubscribeToSession = (sessionId: Session['id']) => {
     );
 
     return unsubscribe;
-  }, [setSessionState, sessionId]);
+  }, [setSessionState, sessionId, navigate, fetchSessions]);
 };
 
 export default useSubscribeToSession;
