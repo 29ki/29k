fd9fec2521a7b83de2407679856b629bb2d508d3
Jakob Öhman <jakob@29k.org>
2022-11-10 15:07:47 +0100

    Add SafeAreaProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,15 +6,18 @@ import codePush, {CodePushOverlay} from './lib/codePush';
 import {UiLibProvider} from './lib/uiLib/hooks/useUiLib';
 import Bootstrap from './Bootstrap';
 import ErrorBoundary from './lib/sentry/components/ErrorBoundary';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => (
   <ErrorBoundary>
-    <UiLibProvider>
-      <Bootstrap>
-        <Navigation />
-        <CodePushOverlay />
-      </Bootstrap>
-    </UiLibProvider>
+    <SafeAreaProvider>
+      <UiLibProvider>
+        <Bootstrap>
+          <Navigation />
+          <CodePushOverlay />
+        </Bootstrap>
+      </UiLibProvider>
+    </SafeAreaProvider>
   </ErrorBoundary>
 );
 
