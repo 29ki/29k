cd54e63894c563ca9c7e22bf61ae09588b7f4cd9
Emil Rondahl <emil@29k.org>
2022-11-11 09:29:54 +0100

    Using forked react-native-video as audio player

--- client/src/routes/Session/components/VideoBase/VideoBase.tsx
+++ client/src/routes/Session/components/VideoBase/VideoBase.tsx
@@ -4,12 +4,6 @@ import RNVideo, {VideoProperties} from 'react-native-video';
 import {Platform} from 'react-native';
 import {COLORS} from '../../../../../../shared/src/constants/colors';
 
-const StyledVideo = styled(RNVideo)({
-  flex: 1,
-  shadowOpacity: 1, // Removes ugly frame on iPhone 13 mini (found in thread: https://github.com/react-native-video/react-native-video/issues/1638)
-  shadowColor: COLORS.WHITE_TRANSPARENT_01, // Removes ugly frame on iPhone 13 mini (found in thread: https://github.com/react-native-video/react-native-video/issues/1638)
-});
-
 const getSilentSwitchSetting = () =>
   parseFloat(Platform.Version as string) >= 16 ? 'ignore' : undefined;
 
@@ -19,22 +13,36 @@ type CommonProps =
   | 'playInBackground'
   | 'playWhenInactive';
 
-const VideoBase = React.forwardRef<RNVideo, Omit<VideoProperties, CommonProps>>(
-  (props, ref) => {
-    return (
-      <StyledVideo
-        {...props}
-        mixWithOthers="mix"
-        ignoreSilentSwitch={Platform.select({
-          ios: getSilentSwitchSetting(),
-        })}
-        playInBackground
-        playWhenInactive
-        allowsExternalPlayback={false}
-        ref={ref}
-      />
-    );
-  },
-);
+export const VideoBase = React.forwardRef<
+  RNVideo,
+  Omit<VideoProperties, CommonProps>
+>((props, ref) => {
+  return (
+    <RNVideo
+      {...props}
+      mixWithOthers="mix"
+      ignoreSilentSwitch={Platform.select({
+        ios: getSilentSwitchSetting(),
+      })}
+      playInBackground
+      playWhenInactive
+      allowsExternalPlayback={false}
+      ref={ref}
+    />
+  );
+});
 
-export default VideoBase;
+const StyledVideo = styled(VideoBase)({
+  flex: 1,
+  shadowOpacity: 1, // Removes ugly frame on iPhone 13 mini (found in thread: https://github.com/react-native-video/react-native-video/issues/1638)
+  shadowColor: COLORS.WHITE_TRANSPARENT_01, // Removes ugly frame on iPhone 13 mini (found in thread: https://github.com/react-native-video/react-native-video/issues/1638)
+});
+
+const StyledVideoBase = React.forwardRef<
+  RNVideo,
+  Omit<VideoProperties, CommonProps>
+>((props, ref) => {
+  return <StyledVideo {...props} ref={ref} />;
+});
+
+export default StyledVideoBase;
