c35ae34e550871d9d04f2e55542290762104255d
Kajsa Unge <hej@kajsaunge.se>
2022-11-23 16:17:22 +0100

    Align image

--- client/src/routes/SessionUnavailableModal/SessionUnavailableModal.tsx
+++ client/src/routes/SessionUnavailableModal/SessionUnavailableModal.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
+import styled from 'styled-components/native';
 
 import Gutters from '../../common/components/Gutters/Gutters';
 import Image from '../../common/components/Image/Image';
 import SheetModal from '../../common/components/Modals/SheetModal';
+import {Spacer28} from '../../common/components/Spacers/Spacer';
 import {ModalHeading} from '../../common/components/Typography/Heading/Heading';
+
+const ImageWrapper = styled.View({
+  width: '65%',
+  height: 276,
+  alignSelf: 'center',
+});
 
 const SessionUnavailableModal = () => {
   const {t} = useTranslation('Modal.SessionUnavailable');
@@ -14,7 +22,10 @@ const SessionUnavailableModal = () => {
       <Gutters big>
         <ModalHeading>{t('description')}</ModalHeading>
       </Gutters>
-      <Image resizeMode="contain" source={{uri: t('image')}} />
+      <ImageWrapper>
+        <Spacer28 />
+        <Image resizeMode="contain" source={{uri: t('image')}} />
+      </ImageWrapper>
     </SheetModal>
   );
 };
