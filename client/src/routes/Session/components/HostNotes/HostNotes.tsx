c2097485e2879179306bff740c5fc7c569894434
Jonas <jonasandrade.mp@gmail.com>
2022-11-25 16:04:12 -0300

    Update fix to be reactive towards scroll state

--- client/src/routes/Session/components/HostNotes/HostNotes.tsx
+++ client/src/routes/Session/components/HostNotes/HostNotes.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import styled from 'styled-components/native';
 import {FlatList} from 'react-native-gesture-handler';
 import Animated, {Easing, FadeInUp, SlideOutUp} from 'react-native-reanimated';
@@ -87,14 +87,28 @@ const HostNotes: React.FC<HostNotesProps> = ({
   const [showNotes, setShowNotes] = useState(introPortal ? true : false);
   const [containerWidth, setContainerWidth] = useState(0);
   const listItemWidth = containerWidth - SPACINGS.THIRTYTWO;
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [scroll, setScroll] = useState({index: 0, animated: false});
   const exercise = useSessionExercise();
   const {t} = useTranslation('Component.HostNotes');
 
-  const calculatePageIndex = (e: NativeSyntheticEvent<NativeScrollEvent>) =>
-    setActiveIndex(
-      Math.round(e?.nativeEvent?.contentOffset?.x / containerWidth),
-    );
+  useEffect(() => {
+    console.log('scrolling to - ', scroll.index);
+    listRef.current?.scrollToIndex({
+      animated: scroll.animated,
+      index: scroll.index,
+    });
+  }, [scroll.animated, scroll.index]);
+
+  const calculatePageIndex = useCallback(
+    (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+      console.log('setting scroll - calculatePageIndex');
+      setScroll({
+        index: Math.round(e?.nativeEvent?.contentOffset?.x / containerWidth),
+        animated: true,
+      });
+    },
+    [containerWidth],
+  );
 
   const notes = introPortal
     ? exercise?.introPortal?.hostNotes
@@ -139,15 +153,12 @@ const HostNotes: React.FC<HostNotesProps> = ({
                 ref={listRef}
                 data={notes}
                 onContentSizeChange={() =>
-                  listRef.current?.scrollToIndex({
-                    animated: false,
-                    index: 0,
-                  })
+                  setScroll({index: 0, animated: false})
                 }
                 pagingEnabled
-                onScroll={calculatePageIndex}
+                onMomentumScrollEnd={calculatePageIndex}
                 keyExtractor={(_, i) => `notes-${i}`}
-                initialScrollIndex={activeIndex}
+                initialScrollIndex={scroll.index}
                 snapToInterval={listItemWidth}
                 showsHorizontalScrollIndicator={false}
                 scrollEnabled={Boolean(notes.length)}
@@ -160,33 +171,25 @@ const HostNotes: React.FC<HostNotesProps> = ({
               />
               <Navigation>
                 <NavButton
-                  onPress={() => {
-                    const newIndex = activeIndex - 1;
-                    if (newIndex < 0) {
-                      return;
-                    }
-                    setActiveIndex(newIndex);
-                    listRef.current?.scrollToIndex({
-                      index: newIndex,
-                    });
-                  }}
+                  onPress={() =>
+                    setScroll({
+                      index: scroll.index - 1,
+                      animated: true,
+                    })
+                  }
                   Icon={BackwardCircleIcon}
-                  disabled={activeIndex <= 0}
+                  disabled={scroll.index <= 0}
                 />
-                <Body14>{`${activeIndex + 1} / ${notes.length}`}</Body14>
+                <Body14>{`${scroll.index + 1} / ${notes.length}`}</Body14>
                 <NavButton
-                  onPress={() => {
-                    const newIndex = activeIndex + 1;
-                    if (newIndex >= notes.length) {
-                      return;
-                    }
-                    setActiveIndex(newIndex);
-                    listRef.current?.scrollToIndex({
-                      index: newIndex,
-                    });
-                  }}
+                  onPress={() =>
+                    setScroll({
+                      index: scroll.index + 1,
+                      animated: true,
+                    })
+                  }
                   Icon={ForwardCircleIcon}
-                  disabled={activeIndex >= notes.length - 1}
+                  disabled={scroll.index >= notes.length - 1}
                 />
               </Navigation>
             </Gutters>
