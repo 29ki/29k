1beb4b203c496144ae62b95ce537604c6452240c
Kajsa Unge <hej@kajsaunge.se>
2022-11-29 17:02:38 +0100

    Email link

--- client/src/routes/Onboarding/Welcome.tsx
+++ client/src/routes/Onboarding/Welcome.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
+import {Linking} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import {COLORS} from '../../../../shared/src/constants/colors';
 import Button from '../../common/components/Buttons/Button';
+import TextButton from '../../common/components/Buttons/TextButton/TextButton';
 import Gutters from '../../common/components/Gutters/Gutters';
 import Image from '../../common/components/Image/Image';
 import Screen from '../../common/components/Screen/Screen';
@@ -16,6 +18,7 @@ import {
 } from '../../common/components/Spacers/Spacer';
 import {Display24} from '../../common/components/Typography/Display/Display';
 import Markdown from '../../common/components/Typography/Markdown/Markdown';
+import {SPACINGS} from '../../common/constants/spacings';
 import useAppState from '../../lib/appState/state/state';
 
 const ImageWrapper = styled.View({
@@ -25,13 +28,26 @@ const ImageWrapper = styled.View({
 const TopImage = styled(Image)({
   aspectRatio: '1',
 });
-
+const CenteredHeading = styled(Display24)({
+  textAlign: 'center',
+});
+const EmailWrapper = styled.View({
+  marginTop: -SPACINGS.SIXTEEN,
+});
 const Center = styled.View({
   alignItems: 'center',
 });
 const Welcome = () => {
   const {t} = useTranslation('Screen.Welcome');
   const setIsFirstLaunch = useAppState(state => state.setIsFirstLaunch);
+  const emailURL = t('email.url');
+
+  const onEmail = () => {
+    if (emailURL) {
+      Linking.openURL(emailURL);
+    }
+  };
+
   const onContinue = () => {
     setIsFirstLaunch(false);
   };
@@ -47,11 +63,12 @@ const Welcome = () => {
             </ImageWrapper>
           </Center>
           <Spacer20 />
-          <Center>
-            <Display24>{t('heading')}</Display24>
-          </Center>
+          <CenteredHeading>{t('heading')}</CenteredHeading>
           <Spacer16 />
           <Markdown>{t('text__markdown')}</Markdown>
+          <EmailWrapper>
+            <TextButton onPress={onEmail} title={t('email.text')} />
+          </EmailWrapper>
           <Spacer32 />
           <Center>
             <Button onPress={onContinue}>{t('button')}</Button>
