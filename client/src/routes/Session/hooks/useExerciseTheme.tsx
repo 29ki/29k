103f0834492ce6d7c1a688248f3ec83dc337095d
Jonas <jonasandrade.mp@gmail.com>
2022-11-30 16:43:15 -0300

    Remove unused code

--- client/src/routes/Session/hooks/useExerciseTheme.tsx
+++ client/src/routes/Session/hooks/useExerciseTheme.tsx
@@ -1,19 +1,6 @@
-import {ExerciseSlide} from '../../../../../shared/src/types/Content';
-import {
-  Exercise,
-  ExerciseTheme,
-} from '../../../../../shared/src/types/generated/Exercise';
+import {ExerciseTheme} from '../../../../../shared/src/types/generated/Exercise';
 import useExerciseById from '../../../lib/content/hooks/useExerciseById';
 import useSessionState from '../state/state';
-
-export type SessionExercise = Exercise & {
-  slide: {
-    index: number;
-    previous?: ExerciseSlide;
-    current: ExerciseSlide;
-    next?: ExerciseSlide;
-  };
-};
 
 const useExerciseTheme = (): ExerciseTheme | undefined => {
   const contentId = useSessionState(state => state.session?.contentId);
