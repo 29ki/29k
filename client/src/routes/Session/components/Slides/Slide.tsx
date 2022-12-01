46280cc240d72afe4a343099742943b7afe6ce48
Kajsa Unge <hej@kajsaunge.se>
2022-12-01 11:24:21 +0100

    Remove redundant Host component

--- client/src/routes/Session/components/Slides/Slide.tsx
+++ client/src/routes/Session/components/Slides/Slide.tsx
@@ -7,6 +7,8 @@ import {COLORS} from '../../../../../../shared/src/constants/colors';
 import Content from './Slides/Content';
 
 import useExerciseTheme from '../../hooks/useExerciseTheme';
+import useSessionParticipantSpotlight from '../../hooks/useSessionParticipantSpotlight';
+import Participant from '../Participants/Participant';
 
 type WrapperProps = {backgroundColor?: string};
 const Wrapper = styled.View<WrapperProps>(({backgroundColor}) => ({
@@ -20,11 +22,14 @@ type SlideProps = {
 };
 
 export const Slide = React.memo(({slide, active}: SlideProps) => {
+  const participantSpotlight = useSessionParticipantSpotlight();
   const theme = useExerciseTheme();
 
   return (
     <Wrapper backgroundColor={theme?.backgroundColor}>
-      {slide.type === 'host' && <Host active={active} />}
+      {slide.type === 'host' && (
+        <Participant participant={participantSpotlight} isHostSpotlight />
+      )}
       {slide.type === 'content' && <Content slide={slide} active={active} />}
       {slide.type === 'reflection' && <Content slide={slide} active={active} />}
       {slide.type === 'sharing' && <Content slide={slide} active={active} />}
