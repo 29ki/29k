6d59e48e336ea9788cb901d3da3a1a2da48f0154
Kajsa Unge <hej@kajsaunge.se>
2022-12-02 08:51:22 +0100

    Keep host slide

--- client/src/routes/Session/components/Slides/Slide.tsx
+++ client/src/routes/Session/components/Slides/Slide.tsx
@@ -6,9 +6,8 @@ import {COLORS} from '../../../../../../shared/src/constants/colors';
 import Content from './Slides/Content';
 
 import useExerciseTheme from '../../hooks/useExerciseTheme';
-import useSessionParticipantSpotlight from '../../hooks/useSessionParticipantSpotlight';
-import Participant from '../Participants/Participant';
 import useSessionState from '../../state/state';
+import Host from './Slides/Host';
 
 type WrapperProps = {backgroundColor?: string};
 const Wrapper = styled.View<WrapperProps>(({backgroundColor}) => ({
@@ -22,7 +21,6 @@ type SlideProps = {
 };
 
 export const Slide = React.memo(({slide, active}: SlideProps) => {
-  const participantSpotlight = useSessionParticipantSpotlight();
   const theme = useExerciseTheme();
 
   const setIsLoadingContent = useSessionState(
@@ -37,9 +35,7 @@ export const Slide = React.memo(({slide, active}: SlideProps) => {
 
   return (
     <Wrapper backgroundColor={theme?.backgroundColor}>
-      {slide.type === 'host' && (
-        <Participant participant={participantSpotlight} isHostSpotlight />
-      )}
+      {slide.type === 'host' && <Host active={active} />}
       {slide.type === 'content' && <Content slide={slide} active={active} />}
       {slide.type === 'reflection' && <Content slide={slide} active={active} />}
       {slide.type === 'sharing' && <Content slide={slide} active={active} />}
