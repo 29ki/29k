b053a2299fe96460960eacb3ba707fe626ac2fde
Jakob Öhman <jakob@29k.org>
2022-12-05 21:19:43 +0100

    Re-add react-native-sound

---
+++ client/src/lib/audio/hooks/useAudio.ts
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import Sound from 'react-native-sound';
+
+const useAudio = (source: string): Sound | undefined => {
+  const [audio, setAudio] = useState<Sound | undefined>();
+
+  useEffect(() => {
+    const audioInstance = new Sound(source, '', err => {
+      if (err) {
+        console.error(err);
+      }
+      /*
+      Sound.setCategory('PlayAndRecord');
+      Sound.setMode('VideoChat');
+      */
+
+      setAudio(audioInstance);
+    });
+  }, [source]);
+
+  useEffect(() => {
+    return () => {
+      audio?.release();
+    };
+  }, [audio]);
+
+  return audio;
+};
+
+export default useAudio;
