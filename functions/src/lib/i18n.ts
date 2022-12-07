1702809de16bec2a3ced7b0410e670592c0c49d1
Emil Rondahl <emil@29k.org>
2022-12-07 10:57:42 +0100

    Remove hidden exercises from functions

--- functions/src/lib/i18n.ts
+++ functions/src/lib/i18n.ts
@@ -11,6 +11,7 @@ import {
   DEFAULT_LANGUAGE_TAG,
   LANGUAGE_TAGS,
 } from '../../../shared/src/constants/i18n';
+import {removeHidden} from '../../../shared/src/i18n/utils';
 
 export * from '../../../shared/src/constants/i18n';
 export type {i18n as I18nInterface} from 'i18next';
@@ -21,7 +22,7 @@ init({
   lng: DEFAULT_LANGUAGE_TAG,
   supportedLngs: LANGUAGE_TAGS,
   fallbackLng: DEFAULT_LANGUAGE_TAG,
-  resources: content.i18n,
+  resources: removeHidden(content.i18n),
 });
 
 export default i18next;
