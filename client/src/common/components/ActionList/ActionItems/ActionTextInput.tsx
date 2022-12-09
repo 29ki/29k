139cab2e555a6503bf3ea4872237ef4c411c49cd
Jakob Öhman <jakob@29k.org>
2022-12-10 00:38:03 +0100

    Add pencil icon to ActionTextInput

--- client/src/common/components/ActionList/ActionItems/ActionTextInput.tsx
+++ client/src/common/components/ActionList/ActionItems/ActionTextInput.tsx
@@ -6,10 +6,12 @@ import textStyles from '../../Typography/styles';
 import ActionItem from '../ActionItem';
 import {COLORS} from '../../../../../../shared/src/constants/colors';
 import hexToRgba from 'hex-to-rgba';
+import {PencilIcon} from '../../Icons';
+import {Spacer8} from '../../Spacers/Spacer';
 
 const style = ({hasError = false}) => ({
   flex: 1,
-  paddingHorizontal: SPACINGS.SIXTEEN,
+  paddingLeft: SPACINGS.SIXTEEN,
   ...textStyles.Body16,
   color: hasError ? COLORS.ERROR : undefined,
 });
@@ -22,11 +24,20 @@ const TextInput = styled.TextInput.attrs(attrs)(style);
 
 const BottomSheetTextInput = styled(BSTextInput).attrs(attrs)(style);
 
+const IconWrapper = styled.View({
+  width: 21,
+  height: 21,
+});
+
 const ActionTextInput: React.FC<
   React.ComponentProps<typeof TextInput>
 > = props => (
   <ActionItem>
     <TextInput {...props} />
+    <IconWrapper>
+      <PencilIcon />
+    </IconWrapper>
+    <Spacer8 />
   </ActionItem>
 );
 
@@ -35,6 +46,10 @@ export const BottomSheetActionTextInput: React.FC<
 > = props => (
   <ActionItem>
     <BottomSheetTextInput {...props} />
+    <IconWrapper>
+      <PencilIcon />
+    </IconWrapper>
+    <Spacer8 />
   </ActionItem>
 );
 
