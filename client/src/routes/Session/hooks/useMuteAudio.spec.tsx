cb372638ff3e3cb5ae04fd719d2fe217a3457b22
Emil Rondahl <emil@29k.org>
2023-01-12 11:07:55 +0100

    move notification to SessionNotification

--- client/src/routes/Session/hooks/useMuteAudio.spec.tsx
+++ client/src/routes/Session/hooks/useMuteAudio.spec.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {renderHook} from '@testing-library/react-hooks';
-import {useTranslation} from 'react-i18next';
 
 import {
   DailyContext,
   DailyProviderTypes,
 } from '../../../lib/daily/DailyProvider';
 import useMuteAudio from './useMuteAudio';
-import useSessionNotificationsState from '../state/sessionNotificationsState';
 import {ExerciseSlide} from '../../../../../shared/src/types/Content';
 
 afterEach(() => {
@@ -17,11 +15,6 @@ afterEach(() => {
 const mockMuteAll = jest.fn();
 
 jest.mock('../../../lib/daily/DailyProvider');
-jest.mock('./useSessionSlideState', () => jest.fn());
-
-jest.mock('../../../lib/components/Icons', () => ({
-  MicrophoneOffIcon: 'MicOffIconComponent',
-}));
 
 describe('useMuteAudioListener', () => {
   const wrapper: React.FC<{children: React.ReactNode}> = ({children}) => (
@@ -33,9 +26,6 @@ describe('useMuteAudioListener', () => {
 
   describe('conditionallyMuteParticipants', () => {
     it('should toggle audio when state is playing and current slide is not sharing', async () => {
-      const {t} = useTranslation();
-      (t as unknown as jest.Mock).mockReturnValue('Some translation');
-
       const current = renderHook(() => useMuteAudio(), {
         wrapper,
       });
@@ -45,18 +35,9 @@ describe('useMuteAudioListener', () => {
       } as ExerciseSlide);
 
       expect(mockMuteAll).toHaveBeenCalledTimes(1);
-      expect(
-        useSessionNotificationsState.getState().notifications,
-      ).toContainEqual({
-        text: 'Some translation',
-        Icon: 'MicOffIconComponent',
-      });
     });
 
     it('should not toggle audio when state is not playing', async () => {
-      const {t} = useTranslation();
-      (t as unknown as jest.Mock).mockReturnValue('Some translation');
-
       const current = renderHook(() => useMuteAudio(), {
         wrapper,
       });
@@ -66,13 +47,9 @@ describe('useMuteAudioListener', () => {
       } as ExerciseSlide);
 
       expect(mockMuteAll).toHaveBeenCalledTimes(0);
-      expect(useSessionNotificationsState.getState().notifications).toEqual([]);
     });
 
     it('should not toggle audio when current slide is sharing', async () => {
-      const {t} = useTranslation();
-      (t as unknown as jest.Mock).mockReturnValue('Some translation');
-
       const current = renderHook(() => useMuteAudio(), {
         wrapper,
       });
@@ -82,7 +59,6 @@ describe('useMuteAudioListener', () => {
       } as ExerciseSlide);
 
       expect(mockMuteAll).toHaveBeenCalledTimes(0);
-      expect(useSessionNotificationsState.getState().notifications).toEqual([]);
     });
   });
 });
--- client/src/routes/Session/hooks/useMuteAudio.ts
+++ client/src/routes/Session/hooks/useMuteAudio.ts
@@ -1,29 +1,18 @@
 import {useCallback, useContext} from 'react';
-import {useTranslation} from 'react-i18next';
 import {ExerciseSlide} from '../../../../../shared/src/types/Content';
-import {MicrophoneOffIcon} from '../../../lib/components/Icons';
 
 import {DailyContext} from '../../../lib/daily/DailyProvider';
-import useSessionNotificationsState from '../state/sessionNotificationsState';
 
 const useMuteAudio = () => {
-  const {t} = useTranslation('Screen.Session');
   const {muteAll} = useContext(DailyContext);
-  const addNotification = useSessionNotificationsState(
-    state => state.addNotification,
-  );
 
   const conditionallyMuteParticipants = useCallback(
     (playing: boolean, currentSlide: ExerciseSlide | undefined) => {
       if (playing && currentSlide?.type !== 'sharing') {
         muteAll();
-        addNotification({
-          text: t('notifications.muted'),
-          Icon: MicrophoneOffIcon,
-        });
       }
     },
-    [muteAll, addNotification, t],
+    [muteAll],
   );
 
   return {conditionallyMuteParticipants};
