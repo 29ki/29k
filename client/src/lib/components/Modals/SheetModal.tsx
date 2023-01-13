17fd9a8f46382862d850a51358d2d7cc0b4077d3
Jakob Öhman <jakob@29k.org>
2023-01-13 15:29:56 +0100

    Add support for close button on sheet modals

--- client/src/lib/components/Modals/SheetModal.tsx
+++ client/src/lib/components/Modals/SheetModal.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {TouchableOpacityProps} from 'react-native';
 
 import styled from 'styled-components/native';
 
 import {COLORS} from '../../../../../shared/src/constants/colors';
 import SETTINGS from '../../constants/settings';
+import {SPACINGS} from '../../constants/spacings';
+import {CloseIcon} from '../Icons';
 import {TopSafeArea} from '../Spacers/Spacer';
+import TouchableOpacity from '../TouchableOpacity/TouchableOpacity';
 
 const Container = styled.View<{backgroundColor?: string}>(
   ({backgroundColor}) => ({
@@ -13,15 +17,28 @@ const Container = styled.View<{backgroundColor?: string}>(
     backgroundColor,
     borderTopLeftRadius: SETTINGS.BORDER_RADIUS.MODALS,
     borderTopRightRadius: SETTINGS.BORDER_RADIUS.MODALS,
+    overflow: 'hidden',
   }),
 );
 
+const Close = styled(TouchableOpacity)({
+  position: 'absolute',
+  top: 24,
+  right: 24,
+  width: SPACINGS.TWENTYEIGHT,
+  height: SPACINGS.TWENTYEIGHT,
+  borderRadius: 24,
+  backgroundColor: COLORS.BLACK,
+});
+
 type ModalProps = {
+  onPressClose?: TouchableOpacityProps['onPress'];
   backgroundColor?: string;
   children: React.ReactNode;
 };
 
 export const SheetModal: React.FC<ModalProps> = ({
+  onPressClose,
   children,
   backgroundColor = COLORS.WHITE,
 }) => (
@@ -29,6 +46,11 @@ export const SheetModal: React.FC<ModalProps> = ({
     {children}
     {/* This is to compensate for the marginTop in ModalStack */}
     <TopSafeArea />
+    {onPressClose && (
+      <Close onPress={onPressClose}>
+        <CloseIcon fill={COLORS.WHITE} />
+      </Close>
+    )}
   </Container>
 );
 
