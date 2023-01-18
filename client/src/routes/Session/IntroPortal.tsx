f948a446887720016c24c4d3fa4f73d6d82fcdb6
Jakob Öhman <jakob@29k.org>
2023-01-18 15:17:53 +0100

    Uniused import

--- client/src/routes/Session/IntroPortal.tsx
+++ client/src/routes/Session/IntroPortal.tsx
@@ -8,7 +8,7 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import dayjs from 'dayjs';
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Video from 'react-native-video';
 import styled from 'styled-components/native';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
