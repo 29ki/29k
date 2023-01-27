f68d6c475acfb6fb0045613c82967bb6372c530d
Jakob Öhman <jakob@29k.org>
2023-01-27 15:12:32 +0100

    logEvent end point

--- functions/src/metrics/logEvent/index.ts
+++ functions/src/metrics/logEvent/index.ts
@@ -1,9 +1,29 @@
+import * as yup from 'yup';
+import validator from 'koa-yup-validator';
 import {createMetricsRouter} from '../../lib/routers';
+import * as metricsModel from '../../models/metrics';
+import {properties} from '../lib/validation';
 
 const router = createMetricsRouter();
 
-export const logEventRouter = router.post('/', ({response}) => {
-  response.status = 200;
-  response.body = 'hej';
-  return;
+const logEventBodySchema = yup.object().shape({
+  userId: yup.string().uuid().required(),
+  timestamp: yup.date().required(),
+  event: yup.string().required(),
+  properties: properties(),
 });
+
+type MetricsEvent = yup.InferType<typeof logEventBodySchema>;
+
+export const logEventRouter = router.post(
+  '/',
+  validator({body: logEventBodySchema}),
+  async ({request, response}) => {
+    const {userId, timestamp, event, properties} = request.body as MetricsEvent;
+
+    await metricsModel.logEvent(userId, new Date(timestamp), event, properties);
+
+    response.status = 200;
+    return;
+  },
+);
