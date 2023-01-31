5e732c692b60e05c84aa4e79f9f84f193e9c4699
Jonas <jonasandrade.mp@gmail.com>
2023-01-31 13:44:43 +0100

    Add userState migration

--- client/src/lib/user/state/state.ts
+++ client/src/lib/user/state/state.ts
@@ -4,17 +4,26 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {createJSONStorage, persist} from 'zustand/middleware';
 import {omit} from 'ramda';
 
+import {Session} from '../../../../../shared/src/types/Session';
+import {v0} from './migrateVersion0';
+
+const USER_STATE_VERSION = 1;
+
 type PinnedSession = {
   id: string;
   expires: Date;
 };
 
-type CompletedSession = {
-  id: string;
+export type CompletedSession = {
+  id: Session['id'];
+  hostId: Session['hostId'];
+  contentId: Session['contentId'];
+  language: Session['language'];
+  type: Session['type'];
   completedAt: Date;
 };
 
-type UserState = {
+export type UserState = {
   pinnedSessions?: Array<PinnedSession>;
   completedSessions?: Array<CompletedSession>;
   metricsUid?: string;
@@ -116,8 +125,28 @@ const useUserState = create<State & Actions>()(
       name: 'userState',
       storage: createJSONStorage(() => AsyncStorage),
       partialize: ({userState}) => ({userState}),
+      // In dev I had change this with the app closed (android)
+      // otherwise the "migrate" functions does not run
+      version: USER_STATE_VERSION,
+      migrate: async (persistedState, version) => {
+        const persisted = persistedState as {userState: State['userState']};
+
+        if (version === 0) {
+          const newState: State['userState'] = {};
+          for (const [userId, state] of Object.entries(persisted.userState)) {
+            newState[userId] = await v0.migrateState(state as UserState);
+          }
+          return {userState: newState} as unknown as State & Actions;
+        }
+
+        return {userState: persisted.userState} as unknown as State & Actions;
+      },
     },
   ),
+);
+
+useUserState.subscribe(state =>
+  console.log('user state', JSON.stringify(state.userState, null, 2)),
 );
 
 export default useUserState;
