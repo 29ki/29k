ca12f10184256d65280888cdffb40557fb1fd57b
Jakob Öhman <jakob@29k.org>
2023-02-02 12:03:14 +0100

    Add more properties to feedback

--- functions/src/metrics/feedback/index.ts
+++ functions/src/metrics/feedback/index.ts
@@ -7,6 +7,10 @@ const router = createMetricsRouter();
 
 const feedbackBodySchema = yup.object({
   exerciseId: yup.string().required(),
+  sessionId: yup.string(),
+  completed: yup.boolean().required(),
+  host: yup.boolean(),
+
   question: yup.string().required(),
   answer: yup.boolean().required(),
   comment: yup.string(),
