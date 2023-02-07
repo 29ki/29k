1efc9aceb80307c51e6ff2b3941d14e08014b594
Jonas <jonasandrade.mp@gmail.com>
2023-02-07 15:51:56 +0100

    Update completed sessions card

--- client/src/lib/components/Cards/SessionCard/CompletedSessionCard.tsx
+++ client/src/lib/components/Cards/SessionCard/CompletedSessionCard.tsx
@@ -8,6 +8,13 @@ import useExerciseById from '../../../content/hooks/useExerciseById';
 import {formatExerciseName} from '../../../utils/string';
 
 import WalletCard from '../WalletCard';
+import Badge from '../../Badge/Badge';
+import {Body14} from '../../Typography/Body/Body';
+import styled from 'styled-components/native';
+import {CheckIcon, CommunityIcon} from '../../Icons';
+import {COLORS} from '../../../../../../shared/src/constants/colors';
+import {Spacer4} from '../../Spacers/Spacer';
+import dayjs from 'dayjs';
 
 type CompletedSessionCardProps = {
   session: CompletedSession;
@@ -16,13 +23,24 @@ type CompletedSessionCardProps = {
   hasCardAfter: boolean;
 };
 
+const Row = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+});
+
+const ChekIconWrapper = styled.View({
+  width: 22,
+  height: 22,
+  alignSelf: 'center',
+});
+
 const CompletedSessionCard: React.FC<CompletedSessionCardProps> = ({
   session,
   hostProfile,
   hasCardBefore,
   hasCardAfter,
 }) => {
-  const {contentId} = session;
+  const {contentId, completedAt} = session;
   const exercise = useExerciseById(contentId);
 
   const onContextPress = useCallback(() => {}, []);
@@ -54,7 +72,19 @@ const CompletedSessionCard: React.FC<CompletedSessionCardProps> = ({
       onPress={onContextPress}
       hasCardBefore={hasCardBefore}
       hasCardAfter={hasCardAfter}
-    />
+      completed={true}>
+      <Row>
+        <ChekIconWrapper>
+          <CheckIcon fill={COLORS.PRIMARY} />
+        </ChekIconWrapper>
+        <Body14>{'Completed'}</Body14>
+        <Spacer4 />
+        <Badge
+          text={dayjs(completedAt).format('ddd, D MMM')}
+          Icon={<CommunityIcon />}
+        />
+      </Row>
+    </WalletCard>
   );
 };
 
