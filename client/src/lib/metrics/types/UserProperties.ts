55bc977f8eaebdeee0506dfa15371fb6a9981771
Jakob Öhman <jakob@29k.org>
2023-02-10 14:22:39 +0100

    Add Language to metrics user properties

--- client/src/lib/metrics/types/UserProperties.ts
+++ client/src/lib/metrics/types/UserProperties.ts
@@ -1,6 +1,9 @@
+import {LANGUAGE_TAG} from '../../i18n';
+
 type UserProperties = {
   Anonymous: boolean;
   'Public Host': boolean;
+  Language: LANGUAGE_TAG;
 };
 
 export default UserProperties;
