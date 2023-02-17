32ac07c75c92f22d50512bff1fa337e0188e52ce
Jakob Öhman <jakob@29k.org>
2023-02-17 12:57:05 +0100

    Get exercise from session state to minimize i18next lookup

--- client/src/lib/session/hooks/useSessionExercise.ts
+++ client/src/lib/session/hooks/useSessionExercise.ts
@@ -1,11 +1,7 @@
 import {Exercise} from '../../../../../shared/src/types/generated/Exercise';
-import useExerciseById from '../../../lib/content/hooks/useExerciseById';
 import useSessionState from '../state/state';
 
-const useSessionExercise = (): Exercise | null => {
-  const liveSession = useSessionState(state => state.liveSession);
-  const asyncSession = useSessionState(state => state.asyncSession);
-  return useExerciseById(liveSession?.exerciseId ?? asyncSession?.exerciseId);
-};
+const useSessionExercise = (): Exercise | null =>
+  useSessionState(state => state.exercise);
 
 export default useSessionExercise;
