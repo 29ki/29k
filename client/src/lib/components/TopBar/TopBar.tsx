c17df7cd63356db438d731043104309fc6d67ab7
Jakob Öhman <jakob@29k.org>
2023-02-23 13:07:31 +0100

    TopBar component and UILib

---
+++ client/src/lib/components/TopBar/TopBar.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import {ViewStyle} from 'react-native';
+import hexToRgba from 'hex-to-rgba';
+import LinearGradient, {
+  LinearGradientProps,
+} from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+import {COLORS} from '../../../../../shared/src/constants/colors';
+import IconButton from '../Buttons/IconButton/IconButton';
+import {ArrowLeftIcon, EllipsisIcon} from '../Icons';
+import CloseButton from '../Buttons/CloseButton/CloseButton';
+import TouchableOpacity from '../TouchableOpacity/TouchableOpacity';
+import {Spacer16, Spacer20} from '../Spacers/Spacer';
+
+const Wrapper = styled.View<{backgroundColor?: string}>(
+  ({backgroundColor}) => ({
+    backgroundColor,
+    zIndex: 1,
+  }),
+);
+
+const Row = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+});
+
+const Content = styled.View({
+  flex: 1,
+  justifyContent: 'center',
+});
+
+type FadeProps = {
+  backgroundColor: string;
+};
+const Fade = styled(LinearGradient).attrs<FadeProps, LinearGradientProps>(
+  ({backgroundColor}) => ({
+    colors: [hexToRgba(backgroundColor, 1), hexToRgba(backgroundColor, 0)],
+  }),
+)<FadeProps>({
+  position: 'absolute',
+  height: 36,
+  left: 0,
+  right: 0,
+  top: '100%',
+  backgroundColor: 'none',
+  pointerEvents: 'none',
+});
+
+const Button = styled(TouchableOpacity).attrs({
+  hitSlop: {
+    left: 5,
+    right: 5,
+    top: 5,
+    bottom: 5,
+  },
+})({
+  width: 30,
+  height: 30,
+});
+
+type TopBarProps = {
+  backgroundColor?: string;
+  onPressBack?: () => void;
+  onPressEllipsis?: () => void;
+  onPressClose?: () => void;
+  fade?: boolean;
+  style?: ViewStyle;
+  children?: React.ReactNode;
+};
+
+const TopBar: React.FC<TopBarProps> = ({
+  backgroundColor,
+  onPressBack,
+  onPressEllipsis,
+  onPressClose,
+  fade,
+  style,
+  children,
+}) => (
+  <Wrapper style={style} backgroundColor={backgroundColor}>
+    <Row>
+      {onPressBack && (
+        <>
+          <Spacer16 />
+          <Button onPress={onPressBack}>
+            <ArrowLeftIcon />
+          </Button>
+        </>
+      )}
+      <Content>{children}</Content>
+      {onPressEllipsis && (
+        <>
+          <Button onPress={onPressEllipsis}>
+            <EllipsisIcon />
+          </Button>
+          <Spacer16 />
+        </>
+      )}
+      {onPressClose && (
+        <>
+          <CloseButton onPress={onPressClose} />
+          <Spacer16 />
+        </>
+      )}
+    </Row>
+    {fade && backgroundColor && <Fade backgroundColor={backgroundColor} />}
+  </Wrapper>
+);
+
+export default TopBar;
