3a7fdbc06478bd2e0f8b256300641eb188fde2fb
Jakob Öhman <jakob@29k.org>
2023-03-06 12:51:12 +0100

    Link to host modal

--- client/src/routes/overlays/CommunityEditorialOverlay/components/CommunityActionList.tsx
+++ client/src/routes/overlays/CommunityEditorialOverlay/components/CommunityActionList.tsx
@@ -8,6 +8,8 @@ import {
   CommunityIcon,
   CheckedIcon,
   WandIcon,
+  MeIcon,
+  FriendsIcon,
 } from '../../../../lib/components/Icons';
 import {ModalStackProps} from '../../../../lib/navigation/constants/routes';
 import {Linking} from 'react-native';
@@ -28,6 +30,8 @@ const CommunityActionList = () => {
     [navigate],
   );
 
+  const hostsPress = useCallback(() => navigate('HostsModal'), [navigate]);
+
   const partnersPress = useCallback(
     () => navigate('PartnersModal'),
     [navigate],
@@ -41,6 +45,9 @@ const CommunityActionList = () => {
         </ActionButton>
         <ActionButton Icon={CommunityIcon} onPress={contributorsPress}>
           {t('actions.contributors')}
+        </ActionButton>
+        <ActionButton Icon={FriendsIcon} onPress={hostsPress}>
+          {t('actions.hosts')}
         </ActionButton>
         <ActionButton Icon={CheckedIcon} onPress={partnersPress}>
           {t('actions.partners')}
