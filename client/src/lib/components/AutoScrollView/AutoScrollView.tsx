06673c542c70828a9210f77271e3ccd7e1494e2b
Jakob Öhman <jakob@29k.org>
2023-03-06 14:48:35 +0100

    Forward ScrollView ref

--- client/src/lib/components/AutoScrollView/AutoScrollView.tsx
+++ client/src/lib/components/AutoScrollView/AutoScrollView.tsx
@@ -2,52 +2,51 @@ import React, {useCallback, useState} from 'react';
 import {LayoutChangeEvent, ScrollView, ScrollViewProps} from 'react-native';
 
 // This component only enables scroll when content is larger than the container
-const AutoScrollView: React.FC<ScrollViewProps> = ({
-  onLayout = () => {},
-  onContentSizeChange = () => {},
-  ...props
-}) => {
-  const [containerDimenstions, setContainerDimensions] = useState({
-    width: 0,
-    height: 0,
-  });
-  const [contentDimensions, setContentDimensions] = useState({
-    width: 0,
-    height: 0,
-  });
+const AutoScrollView = React.forwardRef<ScrollView, ScrollViewProps>(
+  ({onLayout = () => {}, onContentSizeChange = () => {}, ...props}, ref) => {
+    const [containerDimenstions, setContainerDimensions] = useState({
+      width: 0,
+      height: 0,
+    });
+    const [contentDimensions, setContentDimensions] = useState({
+      width: 0,
+      height: 0,
+    });
 
-  const handleLayout = useCallback(
-    (event: LayoutChangeEvent) => {
-      const {
-        nativeEvent: {
-          layout: {width, height},
-        },
-      } = event;
-      setContainerDimensions({width, height});
-      onLayout(event);
-    },
-    [onLayout],
-  );
+    const handleLayout = useCallback(
+      (event: LayoutChangeEvent) => {
+        const {
+          nativeEvent: {
+            layout: {width, height},
+          },
+        } = event;
+        setContainerDimensions({width, height});
+        onLayout(event);
+      },
+      [onLayout],
+    );
 
-  const handleContentSizeChange = useCallback(
-    (width: number, height: number) => {
-      setContentDimensions({width, height});
-      onContentSizeChange(width, height);
-    },
-    [onContentSizeChange],
-  );
-  const enabled =
-    contentDimensions.height > containerDimenstions.height ||
-    contentDimensions.width > containerDimenstions.width;
+    const handleContentSizeChange = useCallback(
+      (width: number, height: number) => {
+        setContentDimensions({width, height});
+        onContentSizeChange(width, height);
+      },
+      [onContentSizeChange],
+    );
+    const enabled =
+      contentDimensions.height > containerDimenstions.height ||
+      contentDimensions.width > containerDimenstions.width;
 
-  return (
-    <ScrollView
-      onLayout={handleLayout}
-      onContentSizeChange={handleContentSizeChange}
-      scrollEnabled={enabled}
-      {...props}
-    />
-  );
-};
+    return (
+      <ScrollView
+        onLayout={handleLayout}
+        onContentSizeChange={handleContentSizeChange}
+        scrollEnabled={enabled}
+        {...props}
+        ref={ref}
+      />
+    );
+  },
+);
 
 export default React.memo(AutoScrollView);
