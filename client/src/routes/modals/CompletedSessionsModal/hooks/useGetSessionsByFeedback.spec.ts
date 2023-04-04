60cf1abb3b6c84e7c32b7047c6c53f8fcf136d5f
Jonas Mendes <jonasandrade.mp@gmail.com>
2023-04-04 10:27:00 +0200

    Add specs to useGetSessionsByFeedback

---
+++ client/src/routes/modals/CompletedSessionsModal/hooks/useGetSessionsByFeedback.spec.ts
@@ -0,0 +1,88 @@
+import {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {renderHook} from '@testing-library/react-hooks';
+
+import useUserState from '../../../../lib/user/state/state';
+import useGetSessionsByFeedback from './useGetSessionsByFeedback';
+import {
+  CompletedSessionPayload,
+  FeedbackPayload,
+} from '../../../../../../shared/src/types/Event';
+import {act} from 'react-test-renderer';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('useGetSessionsByFeedback', () => {
+  beforeEach(() =>
+    useUserState.setState({
+      user: {uid: 'user-id'} as FirebaseAuthTypes.User,
+      userState: {
+        'user-id': {
+          userEvents: [
+            {
+              type: 'feedback',
+              payload: {
+                sessionId: 'session-id-1',
+                answer: true,
+              } as FeedbackPayload,
+              timestamp: new Date('2023-01-01').toISOString(),
+            },
+            {
+              type: 'feedback',
+              payload: {
+                sessionId: 'session-id-2',
+                answer: false,
+              } as FeedbackPayload,
+              timestamp: new Date('2023-01-01').toISOString(),
+            },
+            {
+              type: 'completedSession',
+              payload: {id: 'session-id-1'} as CompletedSessionPayload,
+              timestamp: new Date().toISOString(),
+            },
+            {
+              type: 'completedSession',
+              payload: {id: 'session-id-2'} as CompletedSessionPayload,
+              timestamp: new Date().toISOString(),
+            },
+          ],
+        },
+      },
+    }),
+  );
+
+  it('should return all sessions that got a feedback', async () => {
+    const {result} = renderHook(() => useGetSessionsByFeedback());
+    expect(result.current()).toEqual([
+      {
+        payload: {id: 'session-id-1'},
+        timestamp: expect.any(String),
+        type: 'completedSession',
+      },
+      {
+        payload: {id: 'session-id-2'},
+        timestamp: expect.any(String),
+        type: 'completedSession',
+      },
+    ]);
+  });
+
+  it('should return only sessions with respective feedbackAnswer', async () => {
+    const {result} = renderHook(() => useGetSessionsByFeedback());
+    expect(result.current(true)).toEqual([
+      {
+        payload: {id: 'session-id-1'},
+        timestamp: expect.any(String),
+        type: 'completedSession',
+      },
+    ]);
+    expect(result.current(false)).toEqual([
+      {
+        payload: {id: 'session-id-2'},
+        timestamp: expect.any(String),
+        type: 'completedSession',
+      },
+    ]);
+  });
+});
