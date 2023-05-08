f6e5a47c2b3d363a4f289fe7e454d77781a7c948
Emil Rondahl <emil@29k.org>
2023-05-08 12:46:47 +0200

    use get started collection by constat

--- client/src/lib/content/hooks/useGetStartedCollection.ts
+++ client/src/lib/content/hooks/useGetStartedCollection.ts
@@ -1,10 +1,11 @@
 import {useMemo} from 'react';
 import useGetCollectionById from './useGetCollectionById';
+import {GET_STARTED_COLLECTION_ID} from '../constants';
 
 const useGetStartedCollection = () => {
   const getCollectionById = useGetCollectionById();
   const getStartedCollection = useMemo(() => {
-    return getCollectionById('46f653cd-b77f-458d-a257-1b171591c08b');
+    return getCollectionById(GET_STARTED_COLLECTION_ID);
   }, [getCollectionById]);
 
   return {getStartedCollection};
