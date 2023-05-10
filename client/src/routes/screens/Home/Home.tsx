02d4444e0a514c785556fa44cf34fd0a5ff4e511
Jonas Mendes <jonasandrade.mp@gmail.com>
2023-05-10 11:57:36 +0200

    Add coming soon

--- client/src/routes/screens/Home/Home.tsx
+++ client/src/routes/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import {groupBy} from 'ramda';
+import {groupBy, takeLast} from 'ramda';
 import dayjs from 'dayjs';
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {RefreshControl, SectionList} from 'react-native';
@@ -37,11 +37,20 @@ import TopBar from '../../../lib/components/TopBar/TopBar';
 import MiniProfile from '../../../lib/components/MiniProfile/MiniProfile';
 import BottomFade from '../../../lib/components/BottomFade/BottomFade';
 import useGetRelativeDateGroup from '../../../lib/date/hooks/useGetRelativeDateGroup';
+import ComingSoonSlider from './components/ComingSoon';
+
+export type ComingSoonHeading = {heading: string; description: string};
+export type ComingSoonItem = {when: string; what: string};
+export type ComingSoon = ComingSoonHeading & {
+  id: 'coming-soon-section';
+  items: ComingSoonItem[];
+};
 
 type Section = {
   title: string;
-  data: LiveSessionType[];
-  type: 'hostedBy' | 'interested' | 'comming';
+  data: (LiveSessionType | ComingSoon)[];
+  type: 'hostedBy' | 'interested' | 'comming' | 'comingSoon';
+  beyondThisWeek?: boolean;
 };
 
 const AddButton = styled(Button)({
@@ -81,10 +90,14 @@ const renderSectionHeader: (info: {section: Section}) => React.ReactElement = ({
   </StickyHeading>
 );
 
-const renderSession: SectionListRenderItem<LiveSessionType, Section> = ({
+const renderSession = ({
   item,
   section,
   index,
+}: {
+  item: LiveSessionType;
+  section: Section;
+  index: number;
 }) => {
   const standAlone = section.type === 'comming' || section.data.length === 1;
   const hasCardBefore = index > 0;
@@ -102,6 +115,18 @@ const renderSession: SectionListRenderItem<LiveSessionType, Section> = ({
   );
 };
 
+const renderListItem: SectionListRenderItem<
+  ComingSoon | LiveSessionType,
+  Section
+> = ({item, section, index}) => {
+  switch (section.type) {
+    case 'comingSoon':
+      return <ComingSoonSlider comingSoonSection={item as ComingSoon} />;
+    default:
+      return renderSession({item: item as LiveSessionType, section, index});
+  }
+};
+
 const Home = () => {
   const {t} = useTranslation('Screen.Home');
   const {navigate} =
@@ -109,13 +134,17 @@ const Home = () => {
   const {fetchSessions, sessions, pinnedSessions, hostedSessions} =
     useSessions();
   const getRelativeDateGroup = useGetRelativeDateGroup();
-  const listRef = useRef<SectionList<LiveSessionType, Section>>(null);
+  const listRef =
+    useRef<SectionList<LiveSessionType | ComingSoon, Section>>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const comingSoon: ComingSoon = t('comingSoon', {returnObjects: true});
 
   useScrollToTop(listRef);
 
   const sections = useMemo(() => {
     let sectionsList: Section[] = [];
+    let beyondThisWeek: Section[] = [];
+
     if (hostedSessions.length > 0) {
       sectionsList.push({
         title: t('sections.hostedBy'),
@@ -137,15 +166,45 @@ const Home = () => {
           sessions,
         ),
       ).forEach(([group, items]) => {
-        sectionsList.push({
-          title: group,
-          data: items,
-          type: 'comming',
-        });
+        const isBeyondThisWeek =
+          dayjs(takeLast(1, items)[0].startTime).isoWeek() !==
+          dayjs().isoWeek();
+
+        if (isBeyondThisWeek) {
+          beyondThisWeek.push({
+            title: group,
+            data: items,
+            type: 'comming',
+          });
+        } else {
+          sectionsList.push({
+            title: group,
+            data: items,
+            type: 'comming',
+          });
+        }
       });
     }
+
+    if (comingSoon.items.length > 0) {
+      sectionsList.push({
+        title: t('sections.comingSoon'),
+        data: [comingSoon],
+        type: 'comingSoon',
+      });
+    }
+
+    sectionsList.push(...beyondThisWeek);
+
     return sectionsList;
-  }, [sessions, pinnedSessions, hostedSessions, t, getRelativeDateGroup]);
+  }, [
+    sessions,
+    pinnedSessions,
+    hostedSessions,
+    t,
+    getRelativeDateGroup,
+    comingSoon,
+  ]);
 
   useEffect(() => {
     fetchSessions();
@@ -182,7 +241,7 @@ const Home = () => {
         ListFooterComponent={Spacer48}
         stickySectionHeadersEnabled
         renderSectionHeader={renderSectionHeader}
-        renderItem={renderSession}
+        renderItem={renderListItem}
         refreshControl={
           <RefreshControl refreshing={isLoading} onRefresh={refreshPull} />
         }
---
+++ client/src/routes/screens/Home/components/ComingSoon.tsx
@@ -0,0 +1,101 @@
+import {pick} from 'ramda';
+import React from 'react';
+import Gutters from '../../../../lib/components/Gutters/Gutters';
+import {Spacer16, Spacer24} from '../../../../lib/components/Spacers/Spacer';
+import {SPACINGS} from '../../../../lib/constants/spacings';
+import {ListRenderItem} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import {Body14, Body12} from '../../../../lib/components/Typography/Body/Body';
+import {Display14} from '../../../../lib/components/Typography/Display/Display';
+import hexToRgba from 'hex-to-rgba';
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components';
+import {ComingSoonHeading, ComingSoonItem, ComingSoon} from '../Home';
+import {COLORS} from '../../../../../../shared/src/constants/colors';
+
+const ComingSoonList = styled(FlatList)({
+  flexGrow: 0,
+  width: '100%',
+  overflow: 'visible',
+}) as unknown as FlatList;
+
+const CardContainer = styled.view({width: 136, marginTop: -5, minHeight: 92});
+const CardTag = styled.view({
+  position: 'absolute',
+  top: -8,
+  backgroundColor: 'black',
+  borderRadius: 4,
+  paddingVertical: 2,
+  paddingHorizontal: 4,
+  left: -12,
+});
+const Tag = styled(Body12)({
+  color: COLORS.PURE_WHITE,
+});
+const Description = styled.view({width: 143, marginRight: SPACINGS.SIXTEEN});
+
+const ComingSoonGradient = styled(LinearGradient).attrs({
+  colors: [
+    hexToRgba('#E3D8C1', 0.6),
+    hexToRgba('#FACBA0', 0.6),
+    hexToRgba('#F1B09960', 0.6),
+  ],
+})({
+  borderRadius: 10,
+  flex: 1,
+  justifyContent: 'center',
+  padding: 15,
+});
+
+const renderComingSoonItem: ListRenderItem<
+  ComingSoonHeading | ComingSoonItem
+> = ({item}) => {
+  console.log(item);
+  if ((item as ComingSoonHeading).heading) {
+    return (
+      <Description>
+        <Body14>{(item as ComingSoonHeading).description}</Body14>
+      </Description>
+    );
+  } else {
+    return (
+      <CardContainer>
+        <ComingSoonGradient>
+          <Display14>{(item as ComingSoonItem).what}</Display14>
+        </ComingSoonGradient>
+        <CardTag>
+          <Tag>{(item as ComingSoonItem).when}</Tag>
+        </CardTag>
+      </CardContainer>
+    );
+  }
+};
+
+const ComingSoonSlider: React.FC<{comingSoonSection: ComingSoon}> = ({
+  comingSoonSection,
+}) => {
+  console.log([
+    pick(['heading', 'description'], comingSoonSection),
+    ...comingSoonSection.items,
+  ]);
+  return (
+    <Gutters>
+      <ComingSoonList
+        horizontal
+        snapToAlignment="center"
+        decelerationRate="fast"
+        snapToInterval={200 + SPACINGS.SIXTEEN}
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={Spacer16}
+        data={[
+          pick(['heading', 'description'], comingSoonSection),
+          ...comingSoonSection.items,
+        ]}
+        renderItem={renderComingSoonItem}
+      />
+      <Spacer24 />
+    </Gutters>
+  );
+};
+
+export default ComingSoonSlider;
