01333819a4a7d10d05a46ec03a5770ab1a6af58b
Jakob Öhman <jakob@29k.org>
2023-05-13 10:06:39 +0200

    Implement improved ProfilePicture

--- client/src/lib/components/Bylines/BylineUser.tsx
+++ client/src/lib/components/Bylines/BylineUser.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components/native';
-import {COLORS} from '../../../../../shared/src/constants/colors';
 import {UserProfileType} from '../../../../../shared/src/schemas/User';
 import {SPACINGS} from '../../constants/spacings';
-import Image from '../Image/Image';
 import {Spacer4} from '../Spacers/Spacer';
 import {Body14} from '../Typography/Body/Body';
+import ProfilePicture from '../User/ProfilePicture';
 
 const Container = styled.View({
   flexDirection: 'row',
   justifyContent: 'flex-start',
   alignItems: 'center',
-});
-
-const ImageContainer = styled.View({
-  backgroundColor: COLORS.GREYMEDIUM,
-  width: SPACINGS.TWENTYFOUR,
-  height: SPACINGS.TWENTYFOUR,
-  borderRadius: SPACINGS.TWELVE,
-  overflow: 'hidden',
-  shadowColor: COLORS.GREYDARK,
 });
 
 type BylineUserProps = {user?: UserProfileType | null};
@@ -29,9 +19,11 @@ const BylineUser: React.FC<BylineUserProps> = React.memo(({user}) => {
   const {t} = useTranslation('Component.BylineUser');
   return (
     <Container>
-      <ImageContainer>
-        {user?.photoURL && <Image source={{uri: user?.photoURL}} />}
-      </ImageContainer>
+      <ProfilePicture
+        size={SPACINGS.TWENTYFOUR}
+        pictureURL={user?.photoURL}
+        letter={user?.displayName?.[0]}
+      />
       <Spacer4 />
       <Body14 numberOfLines={1}>
         {user?.displayName ? user.displayName : t('anonymous')}
