a84d72ed34b0dc83901852b8c980371e30b2d9ea
Jakob Öhman <jakob@29k.org>
2023-05-31 23:07:24 +0200

    Add usePracticeReminderNotificationsSetting tests

---
+++ client/src/lib/notifications/hooks/usePracticeReminderNotificationsSetting.spec.ts
@@ -0,0 +1,228 @@
+import {act, renderHook} from '@testing-library/react-hooks';
+
+import useUserState from '../../user/state/state';
+import {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import usePracticeReminderNotificationsSetting from './usePracticeReminderNotificationsSetting';
+import {IntervalEnum} from '../../user/types/Interval';
+
+const mockRequestPermission = jest.fn();
+const mockCheckPermission = jest.fn();
+jest.mock('./useNotificationPermissions', () =>
+  jest.fn(() => ({
+    requestPermission: mockRequestPermission,
+    checkPermission: mockCheckPermission,
+  })),
+);
+
+const mockRemoveTriggerNotifications = jest.fn();
+jest.mock('./useTriggerNotifications', () =>
+  jest.fn(() => ({
+    removeTriggerNotifications: mockRemoveTriggerNotifications,
+  })),
+);
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('usePracticeReminderNotificationsSetting', () => {
+  describe('practiceRemindersEnabled', () => {
+    it('is enabled if having permission and practiceReminderConfig is set', async () => {
+      mockCheckPermission.mockResolvedValueOnce(true);
+
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {
+          'some-uid': {
+            practiceReminderConfig: {
+              hour: 0,
+              minute: 0,
+              interval: IntervalEnum.everyDay,
+            },
+          },
+        },
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      expect(mockCheckPermission).toHaveBeenCalledTimes(1);
+      expect(result.current.practiceRemindersEnabled).toBe(true);
+    });
+
+    it('is disabled if practiceReminderConfig == null', async () => {
+      mockCheckPermission.mockResolvedValueOnce(true);
+
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {'some-uid': {practiceReminderConfig: null}},
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      expect(mockCheckPermission).toHaveBeenCalledTimes(1);
+      expect(result.current.practiceRemindersEnabled).toBe(false);
+    });
+
+    it('is disabled if not having permission', async () => {
+      mockCheckPermission.mockResolvedValueOnce(false);
+
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {
+          'some-uid': {
+            practiceReminderConfig: {
+              hour: 0,
+              minute: 0,
+              interval: IntervalEnum.everyDay,
+            },
+          },
+        },
+      });
+
+      const {result, waitForNextUpdate} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      await waitForNextUpdate();
+
+      expect(mockCheckPermission).toHaveBeenCalledTimes(1);
+      expect(result.current.practiceRemindersEnabled).toBe(false);
+    });
+
+    it('is undefined if practiceReminderConfig == undefined', async () => {
+      mockCheckPermission.mockResolvedValueOnce(true);
+
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {'some-uid': {}},
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      expect(mockCheckPermission).toHaveBeenCalledTimes(1);
+      expect(result.current.practiceRemindersEnabled).toBe(undefined);
+    });
+  });
+
+  describe('practiceReminderConfig', () => {
+    it('can be set', async () => {
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {
+          'some-uid': {
+            practiceReminderConfig: {
+              hour: 0,
+              minute: 0,
+              interval: IntervalEnum.everyDay,
+            },
+          },
+        },
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      expect(result.current.practiceReminderConfig).toEqual({
+        hour: 0,
+        minute: 0,
+        interval: IntervalEnum.everyDay,
+      });
+    });
+
+    it('can be null', async () => {
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {'some-uid': {practiceReminderConfig: null}},
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      expect(result.current.practiceReminderConfig).toBe(null);
+    });
+
+    it('can be undefined', async () => {
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+        userState: {'some-uid': {}},
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      expect(result.current.practiceReminderConfig).toBe(undefined);
+    });
+  });
+
+  describe('setPracticeRemindersConfig', () => {
+    it('requests permission and sets practiceReminderConfig is set', async () => {
+      mockRequestPermission.mockResolvedValueOnce(undefined);
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      await act(async () => {
+        await result.current.setPracticeRemindersConfig({
+          hour: 0,
+          minute: 0,
+          interval: IntervalEnum.everyDay,
+        });
+      });
+
+      expect(mockRequestPermission).toHaveBeenCalledTimes(1);
+      expect(mockRemoveTriggerNotifications).toHaveBeenCalledTimes(0);
+      expect(useUserState.getState()).toEqual(
+        expect.objectContaining({
+          userState: {
+            'some-uid': {
+              practiceReminderConfig: {
+                hour: 0,
+                minute: 0,
+                interval: IntervalEnum.everyDay,
+              },
+            },
+          },
+        }),
+      );
+    });
+
+    it('clears all notifications and set practiceReminderConfig is set to null', async () => {
+      mockRequestPermission.mockResolvedValueOnce(undefined);
+      useUserState.setState({
+        user: {uid: 'some-uid'} as FirebaseAuthTypes.User,
+      });
+
+      const {result} = renderHook(() =>
+        usePracticeReminderNotificationsSetting(),
+      );
+
+      await act(async () => {
+        await result.current.setPracticeRemindersConfig(null);
+      });
+
+      expect(mockRequestPermission).toHaveBeenCalledTimes(0);
+      expect(mockRemoveTriggerNotifications).toHaveBeenCalledTimes(1);
+      expect(mockRemoveTriggerNotifications).toHaveBeenCalledWith(
+        'practice-reminders',
+      );
+      expect(useUserState.getState()).toEqual(
+        expect.objectContaining({
+          userState: {'some-uid': {practiceReminderConfig: null}},
+        }),
+      );
+    });
+  });
+});
