32e327f5526bcefcf330001898e98185aa463306
Emil Rondahl <emil@29k.org>
2023-06-13 08:33:32 +0200

    send onProgress from android

--- client/android/app/src/main/java/org/twentyninek/app/cupcake/newarchitecture/videoLooper/ReactEvents.java
+++ client/android/app/src/main/java/org/twentyninek/app/cupcake/newarchitecture/videoLooper/ReactEvents.java
@@ -3,9 +3,9 @@ package org.twentyninek.app.cupcake.newarchitecture.videoLooper;
 public enum ReactEvents {
   EVENT_ON_END("onEnd"),
   EVENT_ON_LOAD("onLoad"),
+  EVENT_ON_PROGRESS("onProgress"),
   EVENT_ON_TRANSITION("onTransition"),
   EVENT_ON_ERROR("onError");
-
   private final String mName;
 
   ReactEvents(final String name) {
