273b90c6035dc4eed93cb7b07626670c63ffc014
Emil Rondahl <emil@29k.org>
2023-06-21 11:43:42 +0200

    show only others and users current

--- client/src/lib/session/components/Slides/Slides/Sharing.tsx
+++ client/src/lib/session/components/Slides/Slides/Sharing.tsx
@@ -3,13 +3,12 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import hexToRgba from 'hex-to-rgba';
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {LayoutChangeEvent, ListRenderItem, ScrollView} from 'react-native';
+import {ListRenderItem, ScrollView} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 import {COLORS} from '../../../../../../../shared/src/constants/colors';
 import {ExerciseSlideSharingSlide} from '../../../../../../../shared/src/types/generated/Exercise';
-import {PostEvent} from '../../../../../../../shared/src/types/Event';
 import {PostType} from '../../../../../../../shared/src/schemas/Post';
 import Button from '../../../../components/Buttons/Button';
 import Gutters from '../../../../components/Gutters/Gutters';
@@ -31,6 +30,7 @@ import useSharingPosts from '../../../../posts/hooks/useSharingPosts';
 import useUser from '../../../../user/hooks/useUser';
 import useSessionState from '../../../state/state';
 import ListPostCard, {CARD_WIDTH} from '../../Posts/ListPostCard';
+import MyPostCard from '../../Posts/MyPostCard';
 
 const Wrapper = styled.View({
   flex: 1,
@@ -89,15 +89,11 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
   const {navigate} =
     useNavigation<NativeStackNavigationProp<AppStackProps & ModalStackProps>>();
   const user = useUser();
-  const [otherPostListHeight, setOhterPostListHeight] = useState(0);
-  const [myPostListHeight, setMyPostListHeight] = useState(0);
   const session = useSessionState(state => state.asyncSession);
   const theme = useSessionState(state => state.exercise?.theme);
-  const {
-    getSharingPosts,
-    getSharingPostForSession,
-    getSharingPostsForExercise,
-  } = useSharingPosts(session?.exerciseId);
+  const {getSharingPosts, getSharingPostForSession} = useSharingPosts(
+    session?.exerciseId,
+  );
 
   const background = theme?.backgroundColor ?? COLORS.WHITE;
   const topGradientColors = useMemo(
@@ -128,36 +124,6 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
     }
   }, [session?.id, slide.id, getSharingPostForSession]);
 
-  const previousPosts = useMemo(() => {
-    if (session?.id) {
-      return getSharingPostsForExercise(slide.id);
-    }
-    return [];
-  }, [session?.id, slide.id, getSharingPostsForExercise]);
-
-  const onOhterPostListLayout = useCallback(
-    (event: LayoutChangeEvent) => {
-      setOhterPostListHeight(event.nativeEvent.layout.height);
-    },
-    [setOhterPostListHeight],
-  );
-
-  const onMyPostListLayout = useCallback(
-    (event: LayoutChangeEvent) => {
-      setMyPostListHeight(event.nativeEvent.layout.height);
-    },
-    [setMyPostListHeight],
-  );
-
-  useEffect(() => {
-    requestAnimationFrame(() => {
-      scrollRef.current?.scrollTo({
-        y: otherPostListHeight + myPostListHeight,
-        animated: true,
-      });
-    });
-  }, [otherPostListHeight, myPostListHeight]);
-
   const userProfile = useMemo(() => {
     if (user?.displayName) {
       return {
@@ -169,7 +135,7 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
     return null;
   }, [user]);
 
-  const renderOtherItem = useCallback<ListRenderItem<PostType>>(
+  const renderItem = useCallback<ListRenderItem<PostType>>(
     ({item, index}) => {
       return (
         <ItemWrapper isLast={index === posts.length - 1}>
@@ -180,73 +146,25 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
     [posts],
   );
 
-  const otherKeyExtractor = useCallback((item: PostType) => item.id, []);
-
-  const renderMyItem = useCallback<ListRenderItem<PostEvent>>(
-    ({item, index}) => {
-      return (
-        <ItemWrapper isLast={index === previousPosts.length - 1}>
-          <ListPostCard
-            text={item.payload.text}
-            userProfile={!item.payload.isAnonymous ? userProfile : null}
-            isPublic={item.payload.isPublic}
-            sharingAt={item.timestamp}
-          />
-        </ItemWrapper>
-      );
-    },
-    [previousPosts, userProfile],
-  );
-
-  const myKeyExtractor = useCallback(
-    (item: PostEvent) => item.payload.sessionId,
-    [],
-  );
+  const keyExtractor = useCallback((item: PostType) => item.id, []);
 
   return (
     <Wrapper>
       <ScrollView stickyHeaderIndices={[0]} ref={scrollRef}>
         <TopGradient colors={topGradientColors}>
           <Gutters>
-            {previousPosts.length > 0 ? (
-              <StickyHeader textColor={theme?.textColor}>
-                {t('reflectionLabel')}
-              </StickyHeader>
-            ) : posts.length > 0 ? (
+            {posts.length > 0 && (
               <StickyHeader textColor={theme?.textColor}>
                 {t('othersHeading')}
               </StickyHeader>
-            ) : null}
+            )}
           </Gutters>
         </TopGradient>
         <Content>
-          {previousPosts.length > 0 && (
-            <PostsList
-              onLayout={onMyPostListLayout}
-              renderItem={renderMyItem}
-              keyExtractor={myKeyExtractor}
-              horizontal
-              data={previousPosts}
-              snapToAlignment="center"
-              decelerationRate="fast"
-              showsHorizontalScrollIndicator={false}
-              snapToInterval={CARD_WIDTH + SPACINGS.SIXTEEN}
-            />
-          )}
-
-          {posts.length > 0 && previousPosts.length > 0 && (
-            <Gutters>
-              <StyledHeader textColor={theme?.textColor}>
-                {t('othersHeading')}
-              </StyledHeader>
-            </Gutters>
-          )}
-
           {posts.length > 0 && (
             <PostsList
-              onLayout={onOhterPostListLayout}
-              renderItem={renderOtherItem}
-              keyExtractor={otherKeyExtractor}
+              renderItem={renderItem}
+              keyExtractor={keyExtractor}
               horizontal
               data={posts}
               snapToAlignment="center"
@@ -256,23 +174,38 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
             />
           )}
 
-          {!currentPostEvent && (
-            <Gutters>
-              <StyledSubHeader textColor={theme?.textColor}>
-                {slide.content?.heading}
-              </StyledSubHeader>
-              <Spacer16 />
-              <ButtonWrapper>
-                <Button
-                  variant="secondary"
-                  onPress={onAddSharing}
-                  LeftIcon={PlusIcon}>
-                  {t('addReflectionCta')}
-                </Button>
-              </ButtonWrapper>
-            </Gutters>
-          )}
-
+          <Gutters>
+            {currentPostEvent ? (
+              <>
+                <StyledHeader textColor={theme?.textColor}>
+                  {t('reflectionLabel')}
+                </StyledHeader>
+                <Spacer16 />
+                <MyPostCard
+                  text={currentPostEvent.payload.text}
+                  isPublic={currentPostEvent.payload.isPublic}
+                  userProfile={
+                    !currentPostEvent.payload.isAnonymous ? userProfile : null
+                  }
+                />
+              </>
+            ) : (
+              <>
+                <StyledSubHeader textColor={theme?.textColor}>
+                  {slide.content?.heading}
+                </StyledSubHeader>
+                <Spacer16 />
+                <ButtonWrapper>
+                  <Button
+                    variant="secondary"
+                    onPress={onAddSharing}
+                    LeftIcon={PlusIcon}>
+                    {t('addReflectionCta')}
+                  </Button>
+                </ButtonWrapper>
+              </>
+            )}
+          </Gutters>
           <Spacer60 />
           <Spacer8 />
         </Content>
