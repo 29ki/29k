ca01eb8303f047f337fecdc4fb5b77a04f23b056
Jakob Öhman <jakob@29k.org>
2023-07-19 13:11:48 +0200

    Translate and classify text in posts controller

--- functions/src/controllers/posts.spec.ts
+++ functions/src/controllers/posts.spec.ts
@@ -17,12 +17,19 @@ import {
   ExerciseSlideSharingSlide,
 } from '../../../shared/src/types/generated/Exercise';
 import {CreatePostType} from '../../../shared/src/schemas/Post';
+import {classifyText} from '../models/openAi';
+import {translate} from '../lib/translation';
 
 jest.mock('../models/post');
 jest.mock('../models/auth');
-jest.mock('../lib/exercise');
 jest.mock('../models/slack');
+jest.mock('../models/openAi');
+jest.mock('../lib/exercise');
+jest.mock('../lib/translation');
 jest.mock('../../../shared/src/content/exercise');
+
+const mockTranslate = jest.mocked(translate);
+const mockClassifyText = jest.mocked(classifyText);
 
 const mockAddPost = jest.mocked(addPost);
 const mockGetPostsByExerciseAndSharingId = jest.mocked(
@@ -41,10 +48,15 @@ beforeEach(async () => {
 
 describe('posts - controller', () => {
   describe('addPost', () => {
-    it('saves post with user set', async () => {
+    beforeEach(() => {
       mockGetExerciseById.mockReturnValueOnce({
         id: 'some-exercise-id',
         name: 'some exercise name',
+        card: {
+          image: {
+            source: 'some-image-url',
+          },
+        },
       } as Exercise);
       mockGetSharingSlideById.mockReturnValueOnce({
         content: {heading: 'some question'},
@@ -58,7 +70,9 @@ describe('posts - controller', () => {
         approved: true,
         userId: 'some-user-id',
       } as PostRecord);
+    });
 
+    it('saves post with user set', async () => {
       await postsController.createPost(
         {
           exerciseId: 'some-exercise-id',
@@ -78,33 +92,24 @@ describe('posts - controller', () => {
         sharingId: 'some-sharing-id',
         language: 'en',
         text: 'some text',
+        classifications: undefined,
+        translatedText: undefined,
       });
       expect(mockSendPostMessage).toHaveBeenCalledTimes(1);
       expect(mockSendPostMessage).toHaveBeenCalledWith(
         'some-post-id',
+        true,
+        'some-image-url',
         'some exercise name',
         'some question',
         'some text',
+        undefined,
+        undefined,
         'en',
       );
     });
 
     it('saves post without user set', async () => {
-      mockGetExerciseById.mockReturnValueOnce({
-        id: 'some-exercise-id',
-        name: 'some exercise name',
-      } as Exercise);
-      mockGetSharingSlideById.mockReturnValueOnce({
-        content: {heading: 'some question'},
-      } as ExerciseSlideSharingSlide);
-      mockAddPost.mockResolvedValueOnce({
-        id: 'some-post-id',
-        exerciseId: 'some-exercise-id',
-        sharingId: 'some-sharing-id',
-        text: 'some text',
-        language: 'en',
-        approved: true,
-      } as PostRecord);
       await postsController.createPost(
         {
           exerciseId: 'some-exercise-id',
@@ -124,13 +129,181 @@ describe('posts - controller', () => {
         sharingId: 'some-sharing-id',
         language: 'en',
         text: 'some text',
+        classifications: undefined,
+        translatedText: undefined,
       });
       expect(mockSendPostMessage).toHaveBeenCalledTimes(1);
       expect(mockSendPostMessage).toHaveBeenCalledWith(
         'some-post-id',
+        true,
+        'some-image-url',
         'some exercise name',
         'some question',
         'some text',
+        undefined,
+        undefined,
+        'en',
+      );
+    });
+
+    it('translates the text if language is not default', async () => {
+      mockTranslate.mockResolvedValueOnce('some translated text');
+
+      await postsController.createPost(
+        {
+          exerciseId: 'some-exercise-id',
+          sharingId: 'some-sharing-id',
+          language: 'sv',
+          text: 'some text',
+          anonymous: true,
+        } as CreatePostType,
+        'some-user-id',
+      );
+
+      expect(mockAddPost).toHaveBeenCalledTimes(1);
+      expect(mockAddPost).toHaveBeenCalledWith({
+        userId: null,
+        approved: true,
+        exerciseId: 'some-exercise-id',
+        sharingId: 'some-sharing-id',
+        language: 'sv',
+        text: 'some text',
+        classifications: undefined,
+        translatedText: 'some translated text',
+      });
+      expect(mockSendPostMessage).toHaveBeenCalledTimes(1);
+      expect(mockSendPostMessage).toHaveBeenCalledWith(
+        'some-post-id',
+        true,
+        'some-image-url',
+        'some exercise name',
+        'some question',
+        'some text',
+        'some translated text',
+        undefined,
+        'en',
+      );
+    });
+
+    it('classifies the text and disapproves', async () => {
+      mockClassifyText.mockResolvedValueOnce([
+        'Some classification',
+        'Some other classification',
+      ]);
+
+      await postsController.createPost(
+        {
+          exerciseId: 'some-exercise-id',
+          sharingId: 'some-sharing-id',
+          language: 'en',
+          text: 'some text',
+          anonymous: true,
+        } as CreatePostType,
+        'some-user-id',
+      );
+
+      expect(mockAddPost).toHaveBeenCalledTimes(1);
+      expect(mockAddPost).toHaveBeenCalledWith({
+        userId: null,
+        approved: false,
+        exerciseId: 'some-exercise-id',
+        sharingId: 'some-sharing-id',
+        language: 'en',
+        text: 'some text',
+        classifications: ['Some classification', 'Some other classification'],
+        translatedText: undefined,
+      });
+      expect(mockSendPostMessage).toHaveBeenCalledTimes(1);
+      expect(mockSendPostMessage).toHaveBeenCalledWith(
+        'some-post-id',
+        false,
+        'some-image-url',
+        'some exercise name',
+        'some question',
+        'some text',
+        undefined,
+        ['Some classification', 'Some other classification'],
+        'en',
+      );
+    });
+
+    it('approves the post if classification is null', async () => {
+      mockClassifyText.mockResolvedValueOnce(null);
+
+      await postsController.createPost(
+        {
+          exerciseId: 'some-exercise-id',
+          sharingId: 'some-sharing-id',
+          language: 'en',
+          text: 'some text',
+          anonymous: true,
+        } as CreatePostType,
+        'some-user-id',
+      );
+
+      expect(mockAddPost).toHaveBeenCalledTimes(1);
+      expect(mockAddPost).toHaveBeenCalledWith({
+        userId: null,
+        approved: true,
+        exerciseId: 'some-exercise-id',
+        sharingId: 'some-sharing-id',
+        language: 'en',
+        text: 'some text',
+        classifications: null,
+        translatedText: undefined,
+      });
+      expect(mockSendPostMessage).toHaveBeenCalledTimes(1);
+      expect(mockSendPostMessage).toHaveBeenCalledWith(
+        'some-post-id',
+        true,
+        'some-image-url',
+        'some exercise name',
+        'some question',
+        'some text',
+        undefined,
+        null,
+        'en',
+      );
+    });
+
+    it('continues even if translation and classification fails', async () => {
+      mockTranslate.mockRejectedValueOnce(new Error('some translation error'));
+      mockClassifyText.mockRejectedValueOnce(
+        new Error('some classification error'),
+      );
+
+      await postsController.createPost(
+        {
+          exerciseId: 'some-exercise-id',
+          sharingId: 'some-sharing-id',
+          language: 'sv',
+          text: 'some text',
+          anonymous: true,
+        } as CreatePostType,
+        'some-user-id',
+      );
+
+      expect(mockAddPost).toHaveBeenCalledTimes(1);
+      expect(mockAddPost).toHaveBeenCalledWith({
+        userId: null,
+        approved: true,
+        exerciseId: 'some-exercise-id',
+        sharingId: 'some-sharing-id',
+        language: 'sv',
+        text: 'some text',
+        classifications: undefined,
+        translatedText: undefined,
+      });
+      expect(mockSendPostMessage).toHaveBeenCalledTimes(1);
+      expect(mockSendPostMessage).toHaveBeenCalledWith(
+        'some-post-id',
+        true,
+        'some-image-url',
+        'some exercise name',
+        'some question',
+        'some text',
+        undefined,
+        undefined,
         'en',
       );
     });
