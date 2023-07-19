010e901b932c0e1cf18721c75877766bbbce5908
Jakob Öhman <jakob@29k.org>
2023-07-19 11:10:01 +0200

    Ignore undefined firestore properties

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,13 @@ import * as firestore from 'firebase-admin/firestore';
 
 /* This should improve cold starts, no emulator support yet though
    https://github.com/firebase/firebase-admin-node/pull/1901 */
-firestore.initializeFirestore(admin.initializeApp(), {
-  preferRest: !process.env.FUNCTIONS_EMULATOR,
-});
+firestore
+  .initializeFirestore(admin.initializeApp(), {
+    preferRest: !process.env.FUNCTIONS_EMULATOR,
+  })
+  .settings({
+    ignoreUndefinedProperties: true,
+  });
 
 export * from './api';
 export * from './slack';
