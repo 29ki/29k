2418d8939fdf014b26eb1db544c4d6ddd1b50949
Emil Rondahl <emil@29k.org>
2023-08-23 14:06:11 +0200

    show error banner when state subscription fails

--- client/src/lib/session/hooks/useSubscribeToSession.ts
+++ client/src/lib/session/hooks/useSubscribeToSession.ts
@@ -1,12 +1,18 @@
-import {useCallback} from 'react';
+import {useCallback, useContext} from 'react';
 import firestore from '@react-native-firebase/firestore';
 import {
   SessionStateType,
   LiveSessionType,
   SessionStateSchema,
 } from '../../../../../shared/src/schemas/Session';
+import {ErrorBannerContext} from '../../contexts/ErrorBannerContext';
+import {useTranslation} from 'react-i18next';
+import Sentry from '../../sentry';
 
 const useSubscribeToSession = (sessionId: LiveSessionType['id']) => {
+  const errorBannerContext = useContext(ErrorBannerContext);
+  const {t} = useTranslation('Component.NetworkError');
+
   return useCallback(
     (onSnapshot: (sessionState: SessionStateType | undefined) => any) => {
       const stateDoc = firestore()
@@ -22,16 +28,18 @@ const useSubscribeToSession = (sessionId: LiveSessionType['id']) => {
           }
           onSnapshot(SessionStateSchema.validateSync(snapshot.data()));
         },
-        error =>
-          console.debug(
-            `Failed to subscribe to live session ${sessionId}`,
-            error,
-          ),
+        error => {
+          errorBannerContext?.showError(
+            t('subscriptionTitle'),
+            t('subscriptionMessage'),
+          );
+          Sentry.captureException(error, {extra: {sessionId}});
+        },
       );
 
       return unsubscribe;
     },
-    [sessionId],
+    [sessionId, t, errorBannerContext],
   );
 };
 
