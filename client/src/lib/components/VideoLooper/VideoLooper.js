09489c467410520ecf5195cc782b6ba2278c7dd3
Emil Rondahl <emil@29k.org>
2023-08-23 09:56:15 +0200

    send whole event as cause

--- client/src/lib/components/VideoLooper/VideoLooper.js
+++ client/src/lib/components/VideoLooper/VideoLooper.js
@@ -46,7 +46,7 @@ class VideoLooper extends React.Component {
 
   onError(event) {
     Sentry.captureException(
-      new Error('Error in video library', {cause: event.nativeEvent.cause}),
+      new Error('Error in video library', {cause: event.nativeEvent}),
       {
         extra: {sources: this.props.sources},
       },
