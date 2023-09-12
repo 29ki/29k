8aac629dce06419b2bc31111746e04ccbce669fb
gewfy <jakob@29k.org>
2023-09-12 21:31:48 +0200

    Add logMindfulMinutes.ios unit tests

--- client/src/lib/mindfulMinutes/lib/logMindfulMinutes.ios.ts
+++ client/src/lib/mindfulMinutes/lib/logMindfulMinutes.ios.ts
@@ -9,14 +9,11 @@ export const isAvailable = () => HealthKit.isHealthDataAvailable();
 export const requestAuthorization = () =>
   HealthKit.requestAuthorization([], [HKCategoryTypeIdentifier.mindfulSession]);
 
-export const getAuthorizationStatus = async () => {
-  return (
-    (await isAvailable()) &&
-    (await HealthKit.authorizationStatusFor(
-      HKCategoryTypeIdentifier.mindfulSession,
-    )) === HKAuthorizationStatus.sharingAuthorized
-  );
-};
+export const getAuthorizationStatus = async () =>
+  (await isAvailable()) &&
+  (await HealthKit.authorizationStatusFor(
+    HKCategoryTypeIdentifier.mindfulSession,
+  )) === HKAuthorizationStatus.sharingAuthorized;
 
 export const log = (start: Date, end?: Date) =>
   HealthKit.saveCategorySample(
