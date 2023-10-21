2e81adf5c617714119a26426fda458090b3a3fc5
gewfy <jakob@29k.org>
2023-10-21 12:40:32 +0200

    Do not classify if no api key

--- functions/src/models/openAi.ts
+++ functions/src/models/openAi.ts
@@ -3,10 +3,8 @@ import config from '../lib/config';
 
 const {OPENAI_API_KEY} = config;
 
-
-
 const openAi = new OpenAI({
-  apiKey: OPENAI_API_KEY
+  apiKey: OPENAI_API_KEY,
 });
 
 const classifications = [
@@ -28,16 +26,20 @@ const prompt = `Classify if text contains ${classifications.join(
 )}. Answer with unique lower case comma separated, without space between classifications. Answer with none when nothing matches.`;
 
 export const classifyText = async (text: string) => {
-  const completion = await openAi.chat.completions.create({
-    model: 'gpt-4-0613', //'gpt-3.5-turbo-0613',
-    temperature: 0, // Lower = less random, more deterministic
-    messages: [
-      {role: 'system', content: prompt},
-      {role: 'user', content: text},
-    ],
-  });
+  if (OPENAI_API_KEY) {
+    const completion = await openAi.chat.completions.create({
+      model: 'gpt-4-0613', //'gpt-3.5-turbo-0613',
+      temperature: 0, // Lower = less random, more deterministic
+      messages: [
+        {role: 'system', content: prompt},
+        {role: 'user', content: text},
+      ],
+    });
 
-  const content = completion.choices?.[0]?.message?.content;
+    const content = completion.choices?.[0]?.message?.content;
 
-  return content && content !== 'none' ? content?.split(',') : null;
+    return content && content !== 'none' ? content?.split(',') : null;
+  }
+
+  return null;
 };
