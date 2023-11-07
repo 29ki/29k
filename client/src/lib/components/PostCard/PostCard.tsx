49f75f1b1878e243ce705a1cfa4c9567c4a94ba9
gewfy <jakob@29k.org>
2023-11-07 19:40:30 +0100

    Add PostCard component

---
+++ client/src/lib/components/PostCard/PostCard.tsx
@@ -0,0 +1,59 @@
+import {TouchableOpacity} from 'react-native';
+import {SPACINGS} from '../../constants/spacings';
+import {COLORS} from '../../../../../shared/src/constants/colors';
+import styled from 'styled-components/native';
+import {TouchableProps} from 'react-native-svg';
+import React from 'react';
+import hexToRgba from 'hex-to-rgba';
+import LinearGradient, {
+  LinearGradientProps,
+} from 'react-native-linear-gradient';
+
+const Card = styled(TouchableOpacity).attrs(({onPress}) => ({
+  disabled: !onPress,
+}))<{backgroundColor: string}>(({backgroundColor}) => ({
+  flex: 1,
+  backgroundColor,
+  borderRadius: 24,
+  padding: SPACINGS.SIXTEEN,
+  overflow: 'hidden',
+}));
+
+type BottomGradientProps = {
+  color: string;
+};
+
+const BottomGradient = styled(LinearGradient).attrs<
+  BottomGradientProps,
+  LinearGradientProps
+>(({color}) => ({
+  colors: [hexToRgba(color, 0), hexToRgba(color, 1)],
+  locations: [0, 0.84],
+}))<BottomGradientProps>({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  bottom: 0,
+  height: 100,
+});
+
+type Props = {
+  onPress?: TouchableProps['onPress'];
+  backgroundColor?: string;
+  clip?: boolean;
+  children?: React.ReactNode;
+};
+
+const PostCard: React.FC<Props> = ({
+  onPress,
+  clip = false,
+  backgroundColor = COLORS.WHITE,
+  children,
+}) => (
+  <Card onPress={onPress} backgroundColor={backgroundColor}>
+    {children}
+    {clip && <BottomGradient color={backgroundColor} />}
+  </Card>
+);
+
+export default PostCard;
