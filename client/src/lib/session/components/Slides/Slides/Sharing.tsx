5d77cd6a813af74b6c1250fd7c0804b70d941518
gewfy <jakob@29k.org>
2023-11-07 20:50:51 +0100

    Refactor async Sharing slide with new SharingPostCard

--- client/src/lib/session/components/Slides/Slides/Sharing.tsx
+++ client/src/lib/session/components/Slides/Slides/Sharing.tsx
@@ -1,11 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import hexToRgba from 'hex-to-rgba';
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {ListRenderItem, ScrollView} from 'react-native';
+import {Dimensions, ListRenderItem, ScrollView} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 import {COLORS} from '../../../../../../../shared/src/constants/colors';
 import {ExerciseSlideSharingSlide} from '../../../../../../../shared/src/types/generated/Exercise';
@@ -13,55 +11,25 @@ import Button from '../../../../components/Buttons/Button';
 import Gutters from '../../../../components/Gutters/Gutters';
 import {PlusIcon} from '../../../../components/Icons';
 import {
+  BottomSafeArea,
   Spacer16,
-  Spacer60,
-  Spacer8,
+  Spacer28,
 } from '../../../../components/Spacers/Spacer';
-import {Body16} from '../../../../components/Typography/Body/Body';
-import {Display24} from '../../../../components/Typography/Display/Display';
-import {HKGroteskBold} from '../../../../constants/fonts';
-import {SPACINGS} from '../../../../constants/spacings';
 import {
   AppStackProps,
   ModalStackProps,
 } from '../../../../navigation/constants/routes';
 import useSharingPosts from '../../../../posts/hooks/useSharingPosts';
-import useUser from '../../../../user/hooks/useUser';
 import useSessionState from '../../../state/state';
-import ListPostCard, {CARD_WIDTH} from '../../Posts/ListPostCard';
-import MyPostCard from '../../Posts/MyPostCard';
 import {PostItem} from '../../../../posts/types/PostItem';
+import SharingPostCard from '../../../../components/PostCard/SharingPostCard';
+import AutoScrollView from '../../../../components/AutoScrollView/AutoScrollView';
+import StickyHeading from '../../../../components/StickyHeading/StickyHeading';
+import {Heading16} from '../../../../components/Typography/Heading/Heading';
+import {SPACINGS} from '../../../../constants/spacings';
 
-const Wrapper = styled.View({
-  flex: 1,
-});
-
-const TopGradient = styled(LinearGradient)({
-  height: 40,
-  justifyContent: 'flex-end',
-});
-
-const Content = styled.View({
-  flex: 1,
-  marginTop: -SPACINGS.TWELVE,
-});
-
-const StickyHeader = styled(Body16)<{textColor?: string}>(({textColor}) => ({
-  textAlign: 'left',
-  color: textColor ?? COLORS.BLACK,
-  fontFamily: HKGroteskBold,
-  marginTop: -35,
-}));
-
-const StyledHeader = styled(Body16)<{textColor?: string}>(({textColor}) => ({
-  textAlign: 'left',
-  color: textColor ?? COLORS.BLACK,
-  fontFamily: HKGroteskBold,
-}));
-
-const StyledSubHeader = styled(Display24)<{textColor?: string}>(
+const StyledHeading = styled(Heading16)<{textColor?: string}>(
   ({textColor}) => ({
-    textAlign: 'left',
     color: textColor ?? COLORS.BLACK,
   }),
 );
@@ -71,11 +39,14 @@ const PostsList = styled(FlatList)({
   width: '100%',
 }) as unknown as FlatList;
 
-const ItemWrapper = styled.View<{isLast: boolean}>(({isLast}) => ({
-  paddingLeft: SPACINGS.SIXTEEN,
-  paddingRight: isLast ? SPACINGS.SIXTEEN : undefined,
-  paddingTop: SPACINGS.EIGHT,
-}));
+const SCREEN_DIMENSIONS = Dimensions.get('screen');
+const CARD_WIDTH = SCREEN_DIMENSIONS.width / 2;
+const CARD_HEIGHT = SCREEN_DIMENSIONS.height / 3;
+
+const CardWrapper = styled.View({
+  width: CARD_WIDTH,
+  height: CARD_HEIGHT,
+});
 
 const ButtonWrapper = styled.View({flexDirection: 'row'});
 
@@ -88,22 +59,10 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
   const {t} = useTranslation('Component.Sharing');
   const {navigate} =
     useNavigation<NativeStackNavigationProp<AppStackProps & ModalStackProps>>();
-  const user = useUser();
   const session = useSessionState(state => state.asyncSession);
   const theme = useSessionState(state => state.exercise?.theme);
   const {getSharingPosts, getSharingPostForSession} = useSharingPosts(
     session?.exerciseId,
-  );
-
-  const background = theme?.backgroundColor ?? COLORS.WHITE;
-  const topGradientColors = useMemo(
-    () => [
-      hexToRgba(background, 1),
-      hexToRgba(background, 1),
-      hexToRgba(background, 1),
-      hexToRgba(background, 0),
-    ],
-    [background],
   );
 
   const [posts, setPosts] = useState<PostItem[]>([]);
@@ -124,39 +83,13 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
     }
   }, [session?.id, slide.id, getSharingPostForSession]);
 
-  const userProfile = useMemo(() => {
-    if (user?.displayName) {
-      return {
-        displayName: user.displayName,
-        photoURL: user.photoURL ? user.photoURL : undefined,
-        uid: user.uid,
-      };
-    }
-    return null;
-  }, [user]);
-
   const renderItem = useCallback<ListRenderItem<PostItem>>(
-    ({item, index}) => {
-      if (item.type === 'text') {
-        return (
-          <ItemWrapper isLast={index === posts.length - 1}>
-            <ListPostCard
-              text={item.item.text}
-              userProfile={item.item.userProfile}
-            />
-          </ItemWrapper>
-        );
-      }
-      return (
-        <ItemWrapper isLast={index === posts.length - 1}>
-          <ListPostCard
-            video={item.item.video}
-            userProfile={item.item.profile ?? null}
-          />
-        </ItemWrapper>
-      );
-    },
-    [posts],
+    ({item}) => (
+      <CardWrapper>
+        <SharingPostCard sharingPost={item} clip />
+      </CardWrapper>
+    ),
+    [],
   );
 
   const keyExtractor = useCallback(
@@ -166,68 +99,52 @@ const Sharing: React.FC<SharingProps> = ({slide}) => {
   );
 
   return (
-    <Wrapper>
-      <ScrollView stickyHeaderIndices={[0]} ref={scrollRef}>
-        <TopGradient colors={topGradientColors}>
-          <Gutters>
-            {posts.length > 0 && (
-              <StickyHeader textColor={theme?.textColor}>
-                {t('othersHeading')}
-              </StickyHeader>
-            )}
-          </Gutters>
-        </TopGradient>
-        <Content>
-          {posts.length > 0 && (
-            <PostsList
-              renderItem={renderItem}
-              keyExtractor={keyExtractor}
-              horizontal
-              data={posts}
-              snapToAlignment="center"
-              decelerationRate="fast"
-              snapToInterval={CARD_WIDTH + SPACINGS.SIXTEEN}
-              showsHorizontalScrollIndicator={false}
-            />
-          )}
-
-          <Gutters>
-            {currentPostEvent ? (
-              <>
-                <StyledHeader textColor={theme?.textColor}>
-                  {t('reflectionLabel')}
-                </StyledHeader>
-                <Spacer16 />
-                <MyPostCard
-                  text={currentPostEvent.payload.text}
-                  isPublic={currentPostEvent.payload.isPublic}
-                  userProfile={
-                    !currentPostEvent.payload.isAnonymous ? userProfile : null
-                  }
-                />
-              </>
-            ) : (
-              <>
-                <StyledSubHeader textColor={theme?.textColor}>
-                  {slide.content?.heading}
-                </StyledSubHeader>
-                <Spacer16 />
-                <ButtonWrapper>
-                  <Button
-                    variant="secondary"
-                    onPress={onAddSharing}
-                    LeftIcon={PlusIcon}>
-                    {t('addReflectionCta')}
-                  </Button>
-                </ButtonWrapper>
-              </>
-            )}
-          </Gutters>
-          <Spacer60 />
-          <Spacer8 />
-        </Content>
-      </ScrollView>
-    </Wrapper>
+    <AutoScrollView stickyHeaderIndices={[0, 3]} ref={scrollRef}>
+      {posts.length > 0 && (
+        <StickyHeading backgroundColor={theme?.backgroundColor}>
+          <StyledHeading textColor={theme?.textColor}>
+            {t('othersHeading')}
+          </StyledHeading>
+        </StickyHeading>
+      )}
+      {posts.length > 0 && (
+        <PostsList
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+          horizontal
+          data={posts}
+          ListHeaderComponent={Spacer16}
+          ItemSeparatorComponent={Spacer16}
+          ListFooterComponent={Spacer16}
+          snapToAlignment="center"
+          decelerationRate="fast"
+          snapToInterval={CARD_WIDTH + SPACINGS.SIXTEEN}
+          showsHorizontalScrollIndicator={false}
+        />
+      )}
+      <Spacer16 />
+      <StickyHeading backgroundColor={theme?.backgroundColor}>
+        <StyledHeading textColor={theme?.textColor}>
+          {currentPostEvent ? t('reflectionLabel') : slide.content?.heading}
+        </StyledHeading>
+      </StickyHeading>
+      <Gutters>
+        {currentPostEvent ? (
+          <SharingPostCard sharingPost={currentPostEvent} />
+        ) : (
+          <ButtonWrapper>
+            <Button
+              variant="secondary"
+              onPress={onAddSharing}
+              LeftIcon={PlusIcon}>
+              {t('addReflectionCta')}
+            </Button>
+          </ButtonWrapper>
+        )}
+      </Gutters>
+      <Spacer28 />
+      <BottomSafeArea />
+    </AutoScrollView>
   );
 };
 
