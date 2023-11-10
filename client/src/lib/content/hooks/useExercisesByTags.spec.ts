2a2a1f2a1791668f3fbf470b11f18df3806934d5
gewfy <jakob@29k.org>
2023-11-10 10:11:46 +0100

    Add support for limiting

--- client/src/lib/content/hooks/useExercisesByTags.spec.ts
+++ client/src/lib/content/hooks/useExercisesByTags.spec.ts
@@ -25,16 +25,14 @@ describe('useExercisesByTags', () => {
       ]),
     );
 
-    act(() => {
-      expect(result.current).toEqual([
-        {
-          id: 'exercise-id-1',
-          name: 'exercise-1',
-          tags: [{tag: 'tag-1'}, {tag: 'tag-2'}, {tag: 'tag-3'}],
-        },
-        {id: 'exercise-id-2', name: 'exercise-2', tags: [{tag: 'tag-2'}]},
-      ]);
-    });
+    expect(result.current).toEqual([
+      {
+        id: 'exercise-id-1',
+        name: 'exercise-1',
+        tags: [{tag: 'tag-1'}, {tag: 'tag-2'}, {tag: 'tag-3'}],
+      },
+      {id: 'exercise-id-2', name: 'exercise-2', tags: [{tag: 'tag-2'}]},
+    ]);
   });
 
   it('excludes exercise id', () => {
@@ -49,15 +47,36 @@ describe('useExercisesByTags', () => {
       ),
     );
 
-    act(() => {
-      expect(result.current).toEqual([
-        {
-          id: 'exercise-id-1',
-          name: 'exercise-1',
-          tags: [{tag: 'tag-1'}, {tag: 'tag-2'}, {tag: 'tag-3'}],
-        },
-        {id: 'exercise-id-3', name: 'exercise-3', tags: [{tag: 'tag-3'}]},
-      ]);
-    });
+    expect(result.current).toEqual([
+      {
+        id: 'exercise-id-1',
+        name: 'exercise-1',
+        tags: [{tag: 'tag-1'}, {tag: 'tag-2'}, {tag: 'tag-3'}],
+      },
+      {id: 'exercise-id-3', name: 'exercise-3', tags: [{tag: 'tag-3'}]},
+    ]);
+  });
+
+  it('limits the result', () => {
+    const {result} = renderHook(() =>
+      useExercisesByTags(
+        [
+          {id: 'tag-1', tag: 'tag-1'},
+          {id: 'tag-2', tag: 'tag-2'},
+          {id: 'tag-3', tag: 'tag-3'},
+        ],
+        undefined,
+        2,
+      ),
+    );
+
+    expect(result.current).toEqual([
+      {
+        id: 'exercise-id-1',
+        name: 'exercise-1',
+        tags: [{tag: 'tag-1'}, {tag: 'tag-2'}, {tag: 'tag-3'}],
+      },
+      {id: 'exercise-id-2', name: 'exercise-2', tags: [{tag: 'tag-2'}]},
+    ]);
   });
 });
