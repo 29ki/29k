aaccd147b6bfb44dd039212a1ca4d1d8283af735
gewfy <jakob@29k.org>
2023-12-07 11:15:56 +0100

    Enable for all but production

--- client/src/lib/navigation/ExploreStack.tsx
+++ client/src/lib/navigation/ExploreStack.tsx
@@ -9,6 +9,7 @@ import NewExplore from '../../routes/screens/Explore/NewExplore';
 import Collection from '../../routes/screens/Collection/Collection';
 import ExploreTag from '../../routes/screens/Explore/ExploreTag';
 import ExploreCategory from '../../routes/screens/Explore/ExploreCategory';
+import {ENVIRONMENT} from 'config';
 
 const {Navigator, Screen} = createNativeStackNavigator<ExploreStackProps>();
 
@@ -19,7 +20,10 @@ const screenOptions: NativeStackNavigationOptions = {
 
 const ExploreStack = () => (
   <Navigator screenOptions={screenOptions}>
-    <Screen name="Explore" component={__DEV__ ? NewExplore : Explore} />
+    <Screen
+      name="Explore"
+      component={ENVIRONMENT !== 'production' ? NewExplore : Explore}
+    />
     <Screen name="ExploreCategory" component={ExploreCategory} />
     <Screen name="ExploreTag" component={ExploreTag} />
     <Screen name="Collection" component={Collection} />
