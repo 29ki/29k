093db2388670d8f5f892ee6d0e15c6ff43515ea5
gewfy <jakob@29k.org>
2023-12-12 10:49:37 +0100

    Support filtering of content

--- client/src/lib/content/hooks/useFeaturedCollections.ts
+++ client/src/lib/content/hooks/useFeaturedCollections.ts
@@ -1,6 +1,13 @@
+import {intersection} from 'ramda';
 import useCollections from './useCollections';
 import {useFeaturedCollectionIds} from './useFeaturedContent';
 
-const useFeaturedCollections = () => useCollections(useFeaturedCollectionIds());
+const useFeaturedCollections = (collectionIds?: string[]) => {
+  const featuredIds = useFeaturedCollectionIds();
+  const ids = collectionIds
+    ? intersection(featuredIds, collectionIds)
+    : featuredIds;
+  return useCollections(ids);
+};
 
 export default useFeaturedCollections;
--- client/src/lib/content/hooks/useFeaturedExercises.ts
+++ client/src/lib/content/hooks/useFeaturedExercises.ts
@@ -1,6 +1,11 @@
+import {intersection} from 'ramda';
 import useExercises from './useExercises';
 import {useFeaturedExerciseIds} from './useFeaturedContent';
 
-const useFeaturedExercises = () => useExercises(useFeaturedExerciseIds());
+const useFeaturedExercises = (exericeIds?: string[]) => {
+  const featuredIds = useFeaturedExerciseIds();
+  const ids = exericeIds ? intersection(featuredIds, exericeIds) : featuredIds;
+  return useExercises(ids);
+};
 
 export default useFeaturedExercises;
