55405d1ea1e58e385409f8c8ff07955082ba9c6b
gewfy <jakob@29k.org>
2023-12-12 14:33:46 +0100

    Simplify filtering content by tag

--- client/src/lib/content/hooks/useFeaturedExercisesByTags.tsx
+++ client/src/lib/content/hooks/useFeaturedExercisesByTags.tsx
@@ -1,21 +1,11 @@
-import {intersection} from 'ramda';
-import {useMemo} from 'react';
 import useFeaturedExercises from './useFeaturedExercises';
+import useFilterContentByTags from './useFilterContentByTags';
 
 const useFeaturedExercisesByTags = (
   filterTags: string[] = [],
   excludeId?: string,
   limit?: number,
-) => {
-  const exercises = useFeaturedExercises();
-
-  return useMemo(() => {
-    const filteredExercises = exercises.filter(
-      ({id, tags = []}) =>
-        id !== excludeId && intersection(tags, filterTags).length > 0,
-    );
-    return limit ? filteredExercises.slice(0, limit) : filteredExercises;
-  }, [filterTags, exercises, excludeId, limit]);
-};
+) =>
+  useFilterContentByTags(useFeaturedExercises(), filterTags, excludeId, limit);
 
 export default useFeaturedExercisesByTags;
---
+++ client/src/lib/content/hooks/useFilterContentByTags.spec.ts
@@ -0,0 +1,106 @@
+import {renderHook} from '@testing-library/react-hooks';
+import useFilterContentByTags from './useFilterContentByTags';
+import {Exercise} from '../../../../../shared/src/types/generated/Exercise';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('useFilterContentByTags', () => {
+  it('return content containing tags', () => {
+    const {result} = renderHook(() =>
+      useFilterContentByTags(
+        [
+          {
+            id: 'content-id-1',
+            tags: ['tag-1', 'tag-2', 'tag-3'],
+          },
+          {id: 'content-id-2', tags: ['tag-2']},
+          {id: 'content-id-3', tags: ['tag-3']},
+        ] as Exercise[],
+        ['tag-1', 'tag-2'],
+      ),
+    );
+
+    expect(result.current).toEqual([
+      {
+        id: 'content-id-1',
+        tags: ['tag-1', 'tag-2', 'tag-3'],
+      },
+      {id: 'content-id-2', tags: ['tag-2']},
+    ]);
+  });
+
+  it('does not filter when tags are empty', () => {
+    const {result} = renderHook(() =>
+      useFilterContentByTags([
+        {
+          id: 'content-id-1',
+          tags: ['tag-1', 'tag-2', 'tag-3'],
+        },
+        {id: 'content-id-2', tags: ['tag-2']},
+        {id: 'content-id-3', tags: ['tag-3']},
+      ] as Exercise[]),
+    );
+
+    expect(result.current).toEqual([
+      {
+        id: 'content-id-1',
+        tags: ['tag-1', 'tag-2', 'tag-3'],
+      },
+      {id: 'content-id-2', tags: ['tag-2']},
+      {id: 'content-id-3', tags: ['tag-3']},
+    ]);
+  });
+
+  it('excludes content id', () => {
+    const {result} = renderHook(() =>
+      useFilterContentByTags(
+        [
+          {
+            id: 'content-id-1',
+            tags: ['tag-1', 'tag-2', 'tag-3'],
+          },
+          {id: 'content-id-2', tags: ['tag-2']},
+          {id: 'content-id-3', tags: ['tag-3']},
+        ] as Exercise[],
+        ['tag-1', 'tag-2', 'tag-3'],
+        'content-id-2',
+      ),
+    );
+
+    expect(result.current).toEqual([
+      {
+        id: 'content-id-1',
+        tags: ['tag-1', 'tag-2', 'tag-3'],
+      },
+      {id: 'content-id-3', tags: ['tag-3']},
+    ]);
+  });
+
+  it('limits the result', () => {
+    const {result} = renderHook(() =>
+      useFilterContentByTags(
+        [
+          {
+            id: 'content-id-1',
+            tags: ['tag-1', 'tag-2', 'tag-3'],
+          },
+          {id: 'content-id-2', tags: ['tag-2']},
+          {id: 'content-id-3', tags: ['tag-3']},
+        ] as Exercise[],
+        ['tag-1', 'tag-2', 'tag-3'],
+        undefined,
+        2,
+      ),
+    );
+
+    expect(result.current).toEqual([
+      {
+        id: 'content-id-1',
+        tags: ['tag-1', 'tag-2', 'tag-3'],
+      },
+      {id: 'content-id-2', tags: ['tag-2']},
+    ]);
+  });
+});
