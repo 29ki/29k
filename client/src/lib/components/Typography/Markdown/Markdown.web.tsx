b556ce7da7106b36ff649a516142bf47f8e12ab1
gewfy <jakob@29k.org>
2023-12-19 15:25:08 +0100

    Add WIP markdown support for web

--- client/src/lib/components/Typography/Markdown/Markdown.web.tsx
+++ client/src/lib/components/Typography/Markdown/Markdown.web.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import RMarkdown, {Components} from 'react-markdown';
+import {Spacer16} from '../../Spacers/Spacer';
+import styles from './styles';
+import {View, ViewStyle} from 'react-native';
 
-const Markdown = () => null;
+const components: Components = {
+  p: ({children}) => (
+    <>
+      <View style={styles.paragraph}>{children}</View>
+      <Spacer16 />
+    </>
+  ),
+};
+
+type Props = {
+  children: string;
+  style: ViewStyle;
+};
+const Markdown = ({children}: Props) => (
+  <RMarkdown components={components}>{children}</RMarkdown>
+);
 
 export default Markdown;
