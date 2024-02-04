f36f4b231ff1d152642080408660b42eb413aa85
gewfy <jakob@29k.org>
2024-02-04 14:43:23 +0100

    Add P5Animation web component

---
+++ client/src/lib/session/components/P5Animation/P5Animation.web.tsx
@@ -0,0 +1,26 @@
+import React, {forwardRef} from 'react';
+import styled from 'styled-components';
+import p5HtmlTemplate from './p5HtmlTemplate';
+
+const Iframe = styled.iframe({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  border: 0,
+});
+
+export type P5AnimationType = HTMLIFrameElement & {
+  contentWindow: {
+    noLoop: () => void;
+    loop: () => void;
+  };
+};
+
+type Props = {
+  script: string;
+};
+const P5Animation = forwardRef<P5AnimationType, Props>(({script}, ref) => (
+  <Iframe srcDoc={p5HtmlTemplate(script)} ref={ref} />
+));
+
+export default React.memo(P5Animation);
