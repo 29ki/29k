df14cc8e6bf26e2e1cd4a3180e6266363d5a6587
gewfy <jakob@29k.org>
2024-02-22 14:12:33 +0100

    Fix text rendering in exercises

--- client/src/lib/components/Typography/Markdown/rules.tsx
+++ client/src/lib/components/Typography/Markdown/rules.tsx
@@ -6,54 +6,52 @@ import {Spacer16, Spacer8} from '../../Spacers/Spacer';
 
 const rules: RenderRules = {
   heading1: (node, children) => (
-    <Fragment key={node.key}>
+    <View key={node.key}>
       {children}
       <Spacer16 />
-    </Fragment>
+    </View>
   ),
   heading2: (node, children) => (
-    <Fragment key={node.key}>
+    <View key={node.key}>
       {children}
       <Spacer16 />
-    </Fragment>
+    </View>
   ),
   heading3: (node, children) => (
-    <Fragment key={node.key}>
+    <View key={node.key}>
       {children}
       <Spacer16 />
-    </Fragment>
+    </View>
   ),
   heading4: (node, children) => (
-    <Fragment key={node.key}>
+    <View key={node.key}>
       {children}
       <Spacer16 />
-    </Fragment>
+    </View>
   ),
   paragraph: (node, children, parent, styles) => (
-    <Fragment key={node.key}>
+    <View key={node.key}>
       <View style={styles.paragraph}>{children}</View>
       <Spacer16 />
-    </Fragment>
+    </View>
   ),
-  bullet_list: (node, children) => (
-    <Fragment key={node.key}>
+  bullet_list: (node, children, parent, styles) => (
+    <View key={node.key} style={styles.bullet_list}>
       {children}
       <Spacer8 />
-    </Fragment>
+    </View>
   ),
-  ordered_list: (node, children) => (
-    <Fragment key={node.key}>
+  ordered_list: (node, children, parent, styles) => (
+    <View key={node.key} style={styles.ordered_list}>
       {children}
       <Spacer8 />
-    </Fragment>
+    </View>
   ),
   blockquote: (node, children, parent, styles) => (
-    <Fragment key={node.key}>
-      <View style={styles.blockquote}>
-        <View style={styles.blockquote_background} />
-        {children}
-      </View>
-    </Fragment>
+    <View key={node.key} style={styles.blockquote}>
+      <View style={styles.blockquote_background} />
+      {children}
+    </View>
   ),
 };
 
