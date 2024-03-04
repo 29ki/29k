c98d48517937bf1c4b8705ef37b5565ffa23eec0
gewfy <jakob@29k.org>
2024-03-04 13:56:59 +0100

    Filter sharing posts by available exercises

--- client/src/lib/posts/hooks/useSharingPosts.ts
+++ client/src/lib/posts/hooks/useSharingPosts.ts
@@ -1,19 +1,29 @@
 import {useCallback, useState} from 'react';
 import {fetchPosts} from '../api/posts';
 import {PostItem} from '../types/PostItem';
+import useGetExerciseById from '../../content/hooks/useGetExerciseById';
 
 const useSharingPosts = () => {
+  const getExerciseById = useGetExerciseById();
   const [sharingPosts, setSharingPosts] = useState<PostItem[]>([]);
+
+  const filterPosts = useCallback(
+    (posts: PostItem[]) =>
+      posts.filter(({item: {exerciseId}}) => getExerciseById(exerciseId)),
+    [getExerciseById],
+  );
 
   const fetchSharingPosts = useCallback(async () => {
     const posts = await fetchPosts(20);
     setSharingPosts(
-      posts.map<PostItem>(post => ({
-        type: 'text',
-        item: post,
-      })),
+      filterPosts(
+        posts.map<PostItem>(post => ({
+          type: 'text',
+          item: post,
+        })),
+      ),
     );
-  }, []);
+  }, [filterPosts]);
 
   return {
     sharingPosts,
