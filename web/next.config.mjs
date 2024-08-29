ed914ab4e784b9e00ffca87ca0f203a99fee8f96
gewfy <jakob@29k.org>
2024-08-29 21:16:37 +0200

    More web setup

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,10 +3,11 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   compiler: {
     styledComponents: true,
   },
-  webpack: config => {
+  webpack: (config, context) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
@@ -16,6 +17,10 @@ const nextConfig = {
       'styled-components': path.resolve(
         '../client/node_modules/styled-components',
       ),
+      'react-native-safe-area-context': path.resolve(
+        '../client/node_modules/react-native-safe-area-context',
+      ),
+      'react-i18next': path.resolve('../client/node_modules/react-i18next'),
       'react-native-linear-gradient': 'react-native-web-linear-gradient',
     };
     config.resolve.extensions = [
@@ -25,6 +30,11 @@ const nextConfig = {
       '.web.tsx',
       ...config.resolve.extensions,
     ];
+    config.plugins.push(
+      new context.webpack.DefinePlugin({
+        __DEV__: process.env.NODE_ENV === 'development',
+      }),
+    );
     return config;
   },
 };
