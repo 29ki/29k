9d10757b918db5d03f5e7ec1b381267429841aa2
Jakob Öhman <jakob@29k.org>
2024-10-27 20:23:27 +0100

    Fix web deploy

--- web/src/app/[language]/exercises/[exerciseId]/page.tsx
+++ web/src/app/[language]/exercises/[exerciseId]/page.tsx
@@ -52,7 +52,7 @@ const OutroPortal = dynamic(
     ),
 );
 
-export const initialWindowMetrics: Metrics | null = {
+const initialWindowMetrics: Metrics | null = {
   frame: {x: 0, y: 0, width: 0, height: 0},
   insets: {top: 0, left: 0, right: 0, bottom: 0},
 };
