c9216767e20bd1f879f6fba188ce4f3f63de40be
gewfy <jakob@29k.org>
2024-10-29 15:42:09 +0100

    Add support for duration based on session type

--- client/src/lib/components/Cards/SessionCard/hooks/useGetSessionCardTags.ts
+++ client/src/lib/components/Cards/SessionCard/hooks/useGetSessionCardTags.ts
@@ -2,20 +2,28 @@ import {useMemo} from 'react';
 import {useTranslation} from 'react-i18next';
 import useTags from '../../../../content/hooks/useTags';
 import {ExerciseWithLanguage} from '../../../../content/types';
+import {SessionMode} from '../../../../../../../shared/src/schemas/Session';
 
-const useGetSessionCardTags = (exercise?: ExerciseWithLanguage | null) => {
+const useGetSessionCardTags = (
+  exercise?: ExerciseWithLanguage | null,
+  sessionMode: SessionMode = SessionMode.async,
+) => {
   const {t} = useTranslation('Component.SessionCard');
   const tagObjs = useTags(exercise?.tags);
 
   return useMemo(() => {
     let tags = tagObjs.map(tag => tag.name);
 
-    if (exercise?.duration) {
-      tags = [`${exercise.duration} ${t('minutesAbbreviation')}`, ...tags];
+    if (sessionMode === SessionMode.async && exercise?.asyncDuration) {
+      tags = [`${exercise.asyncDuration} ${t('minutesAbbreviation')}`, ...tags];
+    }
+
+    if (sessionMode === SessionMode.live && exercise?.liveDuration) {
+      tags = [`${exercise.liveDuration} ${t('minutesAbbreviation')}`, ...tags];
     }
 
     return tags;
-  }, [t, tagObjs, exercise]);
+  }, [t, tagObjs, exercise, sessionMode]);
 };
 
 export default useGetSessionCardTags;
