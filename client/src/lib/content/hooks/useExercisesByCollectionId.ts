e9307ac52fbae4e179d37cdfc21838e3b214ebc8
gewfy <jakob@29k.org>
2024-11-04 16:58:59 +0100

    Add support for language and ignoreLocked

--- client/src/lib/content/hooks/useExercisesByCollectionId.ts
+++ client/src/lib/content/hooks/useExercisesByCollectionId.ts
@@ -1,12 +1,20 @@
 import {useMemo} from 'react';
 import useGetExercisesByCollectionId from './useGetExercisesByCollectionId';
+import {LANGUAGE_TAG} from '../../i18n';
 
-const useExercisesByCollectionId = (collectionId?: string) => {
+const useExercisesByCollectionId = (
+  collectionId?: string,
+  language?: LANGUAGE_TAG,
+  ignoreLocked?: boolean,
+) => {
   const getExercisesByCollectionId = useGetExercisesByCollectionId();
 
   return useMemo(
-    () => (collectionId ? getExercisesByCollectionId(collectionId) : []),
-    [collectionId, getExercisesByCollectionId],
+    () =>
+      collectionId
+        ? getExercisesByCollectionId(collectionId, language, ignoreLocked)
+        : [],
+    [collectionId, language, ignoreLocked, getExercisesByCollectionId],
   );
 };
 
