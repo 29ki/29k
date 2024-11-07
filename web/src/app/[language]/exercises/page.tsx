34d25e6e32a9140231556bc19733a36cb6182707
gewfy <jakob@29k.org>
2024-11-07 20:10:34 +0100

    Add exercise modal

--- web/src/app/[language]/exercises/page.tsx
+++ web/src/app/[language]/exercises/page.tsx
@@ -16,6 +16,9 @@ import Logo from '@/lib/components/Logo';
 import LanguageSelect from '@/lib/components/LanguageSelect';
 import Columns from '@/lib/components/Columns';
 import Gutters from '@/lib/components/Gutters';
+import ExerciseModal from '@/lib/components/ExerciseModal';
+import {MouseEventHandler, useCallback, useState} from 'react';
+import {ExerciseWithLanguage} from '../../../../../client/src/lib/content/types';
 
 const StyledLogo = styled(Logo)({
   height: 46,
@@ -36,30 +39,51 @@ const StyledLink = styled(Link)({
 export default function ExercisesPage() {
   const {t} = useTranslation('Screen.Explore');
   const exercises = useExercises();
+  const [exerciseModal, setExerciseModal] =
+    useState<ExerciseWithLanguage | null>(null);
+
+  const onExerciseClick = useCallback(
+    (exercise: ExerciseWithLanguage): MouseEventHandler<HTMLAnchorElement> =>
+      e => {
+        e.preventDefault();
+        setExerciseModal(exercise);
+      },
+    [],
+  );
+
+  const onCloseModal = useCallback(() => {
+    setExerciseModal(null);
+  }, []);
 
   return (
-    <Gutters>
-      <Spacer32 />
-      <Header>
-        <StyledLogo />
-        <LanguageSelect />
-      </Header>
-      <Spacer24 />
-      <Heading18>{t('sessionsHeading')}</Heading18>
-      <Spacer8 />
-      <Columns>
-        {exercises
-          .filter(({hidden, locked}) => !hidden && !locked)
-          .map(exercise => (
-            <div key={exercise.id}>
-              <StyledLink
-                href={`/${exercise.language}/exercises/${exercise.id}`}>
-                <ExerciseCard exercise={exercise} />
-              </StyledLink>
-              <Spacer16 />
-            </div>
-          ))}
-      </Columns>
-    </Gutters>
+    <>
+      <Gutters>
+        <Spacer32 />
+        <Header>
+          <StyledLogo />
+          <LanguageSelect />
+        </Header>
+        <Spacer24 />
+        <Heading18>{t('sessionsHeading')}</Heading18>
+        <Spacer8 />
+        <Columns>
+          {exercises
+            .filter(({hidden, locked}) => !hidden && !locked)
+            .map(exercise => (
+              <div key={exercise.id}>
+                <StyledLink
+                  href={`/${exercise.language}/exercises/${exercise.id}`}
+                  onClick={onExerciseClick(exercise)}>
+                  <ExerciseCard exercise={exercise} />
+                </StyledLink>
+                <Spacer16 />
+              </div>
+            ))}
+        </Columns>
+      </Gutters>
+      {exerciseModal && (
+        <ExerciseModal exercise={exerciseModal} onClose={onCloseModal} />
+      )}
+    </>
   );
 }
