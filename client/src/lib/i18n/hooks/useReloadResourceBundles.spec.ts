9eba63b58d80d63af1fb2e8b71669ba21d1c9106
gewfy <jakob@29k.org>
2024-11-25 21:42:41 +0100

    Fix tests and types

--- client/src/lib/i18n/hooks/useReloadResourceBundles.spec.ts
+++ client/src/lib/i18n/hooks/useReloadResourceBundles.spec.ts
@@ -15,7 +15,7 @@ describe('useReloadResourceBundles', () => {
       await result.current();
     });
 
-    expect(i18n.removeResourceBundle).toHaveBeenCalledTimes(28);
+    expect(i18n.removeResourceBundle).toHaveBeenCalledTimes(32);
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('en', 'categories');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('pt', 'categories');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('sv', 'categories');
@@ -23,6 +23,7 @@ describe('useReloadResourceBundles', () => {
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('da', 'categories');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('cs', 'categories');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('nl', 'categories');
+    expect(i18n.removeResourceBundle).toHaveBeenCalledWith('es', 'categories');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('en', 'collections');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('pt', 'collections');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('sv', 'collections');
@@ -30,6 +31,7 @@ describe('useReloadResourceBundles', () => {
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('da', 'collections');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('cs', 'collections');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('nl', 'collections');
+    expect(i18n.removeResourceBundle).toHaveBeenCalledWith('es', 'collections');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('en', 'tags');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('pt', 'tags');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('sv', 'tags');
@@ -37,6 +39,7 @@ describe('useReloadResourceBundles', () => {
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('da', 'tags');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('cs', 'tags');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('nl', 'tags');
+    expect(i18n.removeResourceBundle).toHaveBeenCalledWith('es', 'tags');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('en', 'exercises');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('pt', 'exercises');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('sv', 'exercises');
@@ -44,6 +47,7 @@ describe('useReloadResourceBundles', () => {
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('da', 'exercises');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('cs', 'exercises');
     expect(i18n.removeResourceBundle).toHaveBeenCalledWith('nl', 'exercises');
+    expect(i18n.removeResourceBundle).toHaveBeenCalledWith('es', 'exercises');
     expect(i18n.reloadResources).toHaveBeenCalledTimes(1);
     expect(i18n.reloadResources).toHaveBeenCalledWith(undefined, [
       'categories',
