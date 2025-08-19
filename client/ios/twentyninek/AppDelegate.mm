cfaba8c83d0f3db6d8a445d6f1449d9364f50271
gewfy <jakob@29k.org>
2025-08-19 19:19:21 +0200

    Add airbridge and rnconfig

--- client/ios/twentyninek/AppDelegate.mm
+++ client/ios/twentyninek/AppDelegate.mm
@@ -1,16 +1,23 @@
 #import "AppDelegate.h"
 
+#import "RNCConfig.h"
 #import <CodePush/CodePush.h>
 #import <Firebase.h>
 #import <AVFoundation/AVFoundation.h>
+#import <AirbridgeReactNative/AirbridgeReactNative.h>
 
 #import <React/RCTBundleURLProvider.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
+
+  NSString *appName = [RNCConfig envFor:@"AIRBRIDGE_APP_NAME"];
+  NSString *sdkToken = [RNCConfig envFor:@"AIRBRIDGE_SDK_TOKEN"];
+  [AirbridgeReactNative initializeSDKWithName:appName token:sdkToken];
 
   // allowing sounds from several sources
   [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
@@ -38,6 +45,22 @@
 #else
   return [CodePush bundleURL];
 #endif
+}
+
+// Airbridge when app is opened with scheme deeplink
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    // track deeplink
+    [AirbridgeReactNative trackDeeplinkWithUrl:url];
+
+    return YES;
+}
+
+// Airbridge when app is opened with universal links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+    // track deeplink
+    [AirbridgeReactNative trackDeeplinkWithUserActivity:userActivity];
+
+    return YES;
 }
 
 @end
